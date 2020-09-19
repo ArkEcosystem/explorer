338418b22474b835d889c65715ace250a5a4673d
Brian Faust <faustbrian@users.noreply.github.com>
2020-09-19 06:15:09 +0300

    chore: add routing boilerplate

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,23 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+// Route::get("/wallets/{address}", "");
+// Route::get("/wallets/{address}/voters", "");
+// Route::get("/wallets/{address}/voters/{page}", "");
+// Route::get("/wallets/{address}/blocks", "");
+// Route::get("/wallets/{address}/blocks/{page}", "");
+// Route::get("/wallets/{address}/transactions", "");
+// Route::get("/wallets/{address}/transactions/{type}", "");
+// Route::get("/wallets/{address}/transactions/{type}/{page}", "");
+// Route::get("/block/{id}", "");
+// Route::get("/block/{id}/transactions", "");
+// Route::get("/block/{id}/transactions/{page}", "");
+// Route::get("/blocks", "");
+// Route::get("/blocks/{page}", "");
+// Route::get("/transaction/{id}", "");
+// Route::get("/transactions", "");
+// Route::get("/transactions/{page}", "");
+// Route::get("/delegate-monitor", "");
+// Route::get("/top-wallets", "");
+// Route::get("/top-wallets/{page}", "");
