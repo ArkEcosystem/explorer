ffafde6601f5a5831ae17904d06e327e20993bcd
alexbarnsley <alexbarnsley@users.noreply.github.com>
2020-10-15 11:53:21 +0000

    style: resolve style guide violations

--- app/Http/Livewire/NetworkStatusBlock.php
+++ app/Http/Livewire/NetworkStatusBlock.php
@@ -9,9 +9,9 @@ class NetworkStatusBlock extends Component
     public function render()
     {
         return view('livewire.network-status-block', [
-            'height' => '12,198,189',
-            'network' => 'ARK Public Network',
-            'supply' => 'Ѧ 149,245,180',
+            'height'    => '12,198,189',
+            'network'   => 'ARK Public Network',
+            'supply'    => 'Ѧ 149,245,180',
             'marketCap' => '$ 28,774,470.70',
         ]);
     }
