37f31d17a06ee272e2d9764d69255c6ac3f890a0
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-15 11:39:06 +0100

    chore: remove social urls from lang

--- resources/lang/en/urls.php
+++ resources/lang/en/urls.php
@@ -1,13 +1,5 @@
 <?php
 
 return [
-    // Socials
-    'slack' => 'https://slack.ark.io/',
-    'discord' => 'https://discord.ark.io/',
-    'twitter' => 'https://twitter.ark.io/',
-    'linkedin' => 'https://www.linkedin.com/company/ark-ecosystem',
-    'facebook' => 'https://facebook.ark.io/',
-    'reddit' => 'https://reddit.ark.io/',
-    'youtube' => 'https://youtube.ark.io/',
-    'github' => 'https://github.com/ArkEcosystem',
+
 ];
