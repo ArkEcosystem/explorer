67804d67b8bdde9a8b78a6e94e202dfeac6e2d6d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-15 10:33:53 +0100

    refactor: update app template

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -3,7 +3,6 @@
     @push('metatags')
         <meta property="og:title" content="@lang('metatags.home.title')" />
         <meta property="og:description" content="@lang('metatags.home.description')">
-        <meta property="og:image" content="@lang('metatags.home.image')" />
     @endpush
 
     @section('breadcrumbs')
