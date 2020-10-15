f118fc6cd023877303e9025a35b696e3e8e0af58
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-15 12:41:27 +0100

    feat: network status block component

---
+++ app/Http/Livewire/NetworkStatusBlock.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class NetworkStatusBlock extends Component
+{
+    public function render()
+    {
+        return view('livewire.network-status-block');
+    }
+}
---
+++ resources/views/livewire/network-status-block.blade.php
@@ -0,0 +1,8 @@
+<div class="inline-block bg-theme-secondary-200 rounded-lg">
+    <div class="flex space-x-5 text-xs font-medium px-4 py-2">
+        <div>Height: 12,198,189</div>
+        <div>Network: ARK Public Network</div>
+        <div>Supply: Ѧ 149,245,180</div>
+        <div>Market Cap:  $ 28,774,470.70</div>
+    </div>
+</div>
