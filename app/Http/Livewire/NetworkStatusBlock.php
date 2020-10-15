aa93e088e15c8bf0731d9bf0eaced711ad833560
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-15 12:50:07 +0100

    refactor: use lang & vars for component

--- app/Http/Livewire/NetworkStatusBlock.php
+++ app/Http/Livewire/NetworkStatusBlock.php
@@ -8,6 +8,11 @@ class NetworkStatusBlock extends Component
 {
     public function render()
     {
-        return view('livewire.network-status-block');
+        return view('livewire.network-status-block', [
+            'height' => '12,198,189',
+            'network' => 'ARK Public Network',
+            'supply' => 'Ѧ 149,245,180',
+            'marketCap' => '$ 28,774,470.70',
+        ]);
     }
 }
