49a2e40e09179bc35639f2c29912efcb3011ff12
Brian Faust <hello@basecode.sh>
2020-10-19 07:36:26 +0300

    wip

--- database/factories/WalletFactory.php
+++ database/factories/WalletFactory.php
@@ -6,7 +6,6 @@ namespace Database\Factories;
 
 use App\Wallet;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 final class WalletFactory extends Factory
 {
@@ -14,12 +13,6 @@ final class WalletFactory extends Factory
 
     public function definition()
     {
-        return [
-            'name'              => $this->faker->name,
-            'email'             => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token'    => Str::random(10),
-        ];
+        return [];
     }
 }
