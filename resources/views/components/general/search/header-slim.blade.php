de20fd506102e122360d372097eb3246ef1245c1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-19 14:10:04 +0100

    feat: header slim search component

---
+++ resources/views/components/general/search/header-slim.blade.php
@@ -0,0 +1,27 @@
+<div class="flex flex-col w-full space-y-5">
+    <div class="flex items-center justify-between relative">
+        <h1 class="header-2 font-bold whitespace-no-wrap text-theme-secondary-900 dark:text-theme-secondary-200">
+            {{ $title }}
+        </h1>
+
+        <div
+            x-data="{ searchFocused: false }"
+            @search-slim-expand="searchFocused = true"
+            @search-slim-close="searchFocused = false"
+            class="hidden md:block"
+            :class="{
+                'w-full absolute top-0 right-0': searchFocused,
+                'w-1/2 lg:w-5/12 xl:w-8/12': ! searchFocused,
+            }"
+            x-transition:enter="transition ease-out duration-100"
+            x-transition:enter-start="width-auto transform"
+            x-transition:enter-end="width-full transform"
+            x-transition:leave="transition ease-in duration-100"
+            x-transition:leave-start="width-full transform"
+            x-transition:leave-end="width-auto transform"
+            x-cloak
+        >
+            <livewire:search-module :is-slim="true" />
+        </div>
+    </div>
+</div>
