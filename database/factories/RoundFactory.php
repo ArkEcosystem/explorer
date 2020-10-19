0adaf82aac8b3fc5129c08257b71b305ea2688dc
Brian Faust <hello@basecode.sh>
2020-10-19 07:35:26 +0300

    wip

---
+++ database/factories/RoundFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Round;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+final class RoundFactory extends Factory
+{
+    protected $model = Round::class;
+
+    public function definition()
+    {
+        return [
+            'name'              => $this->faker->name,
+            'email'             => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+        ];
+    }
+}
