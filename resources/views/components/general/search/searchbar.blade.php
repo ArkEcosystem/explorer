4f61fc1f83c2b30b1388521700a3693c9125e5e1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-19 16:11:18 +0100

    fix: dark mode button text colour

--- resources/views/components/general/search/searchbar.blade.php
+++ resources/views/components/general/search/searchbar.blade.php
@@ -59,7 +59,7 @@
 
             <button
                 type="button"
-                class="hidden text-theme-secondary-900 mr-8 rounded text-center transition-default font-normal hover:bg-theme-primary-100 dark:hover:bg-theme-primary-800 md:block {{ ($slim ?? false) ? 'px-2 py-1 -my-2' : 'px-4 py-2' }}"
+                class="hidden text-theme-secondary-900 mr-8 rounded text-center transition-default font-normal hover:bg-theme-primary-100 dark:hover:bg-theme-secondary-800 dark:text-theme-secondary-600 md:block {{ ($slim ?? false) ? 'px-2 py-1 -my-2' : 'px-4 py-2' }}"
                 @click="showAdvanced = !showAdvanced; isFocused = true; $dispatch('search-slim-expand')"
             >
                 <span x-show="!showAdvanced">@lang('actions.advanced_search')</span>
