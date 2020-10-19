5eb7d5a57de3a112341c2cb4efa656d762aa9c50
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-19 16:43:56 +0100

    refactor: hide heading on focus, no absolute

--- resources/views/components/general/search/header-slim.blade.php
+++ resources/views/components/general/search/header-slim.blade.php
@@ -1,14 +1,19 @@
 <div class="flex flex-col w-full space-y-5">
-    <div class="flex items-center justify-between relative">
-        <h1 class="header-2 font-bold whitespace-no-wrap text-theme-secondary-900 dark:text-theme-secondary-200">
+    <div
+        x-data="{ searchFocused: false }"
+        @search-slim-expand="searchFocused = true"
+        @search-slim-close="searchFocused = false"
+        class="flex items-center justify-between relative"
+    >
+        <h1
+            class="header-2 font-bold whitespace-no-wrap text-theme-secondary-900 dark:text-theme-secondary-200"
+            :class="{ hidden: searchFocused }"
+        >
             {{ $title }}
         </h1>
 
         <div
-            x-data="{ searchFocused: false }"
-            @search-slim-expand="searchFocused = true"
-            @search-slim-close="searchFocused = false"
-            class="hidden absolute top-0 right-0 md:block"
+            class="hidden md:block"
             :class="{
                 'w-full': searchFocused,
                 'w-1/2 lg:w-5/12 xl:w-7/12': ! searchFocused,
