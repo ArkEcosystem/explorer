cae6788cf1591075a78d6555cdecda85970c3813
Brian Faust <hello@basecode.sh>
2020-10-20 06:22:12 +0300

    wip

--- tests/Unit/ViewModels/WalletViewModelTest.php
+++ tests/Unit/ViewModels/WalletViewModelTest.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
-use App\Models\Wallet;
+use App\Models\Block;
 
+use App\Models\Wallet;
 use App\ViewModels\WalletViewModel;
 use Illuminate\Support\Facades\Http;
 use function Tests\configureExplorerDatabase;
@@ -11,10 +12,19 @@ use function Tests\configureExplorerDatabase;
 beforeEach(function () {
     configureExplorerDatabase();
 
-    $this->subject = new WalletViewModel(Wallet::factory()->create([
+    $wallet = Wallet::factory()->create([
         'balance'      => 1000 * 1e8,
         'vote_balance' => 1000 * 1e8,
-    ]));
+    ]);
+
+    $this->subject = new WalletViewModel($wallet);
+
+    Block::factory()->create([
+        'total_amount'         => 10 * 1e8,
+        'total_fee'            => 8 * 1e8,
+        'reward'               => 2 * 1e8,
+        'generator_public_key' => $wallet->public_key,
+    ]);
 });
 
 it('should get the balance', function () {
@@ -52,4 +62,19 @@ it('should get the votes as percentage from supply', function () {
 it('should generate a QR Code', function () {
     expect($this->subject->qrCode())->toBeString();
     expect($this->subject->qrCode())->toContain('<svg');
+});
+
+it('should sum up the amount forged', function () {
+    expect($this->subject->amountForged())->toBeString();
+    expect($this->subject->amountForged())->toBe('ARK 10.00');
+});
+
+it('should sum up the fees forged', function () {
+    expect($this->subject->feesForged())->toBeString();
+    expect($this->subject->feesForged())->toBe('ARK 8.00');
+});
+
+it('should sum up the rewards forged', function () {
+    expect($this->subject->rewardsForged())->toBeString();
+    expect($this->subject->rewardsForged())->toBe('ARK 2.00');
 });
