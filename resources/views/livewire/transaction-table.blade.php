1039d6154d591ee0bc7af7a093f8ee452940d775
alexbarnsley <alexbarnsley@users.noreply.github.com>
2020-10-20 10:06:08 +0000

    style: resolve style guide violations

--- resources/views/livewire/transaction-table.blade.php
+++ resources/views/livewire/transaction-table.blade.php
@@ -10,7 +10,7 @@
         </script>
     @else
         <div class="pt-4 mt-8 border-t border-theme-secondary-300 dark:border-theme-secondary-800 md:mt-0 md:border-dashed">
-            <a href="{{ route('transactions') }}" class="button-secondary w-full">@lang('actions.view_all')</a>
+            <a href="{{ route('transactions') }}" class="w-full button-secondary">@lang('actions.view_all')</a>
         </div>
     @endunless
 </div>
