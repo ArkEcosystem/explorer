b150da88cc673332ebd2b13ac5786dd76f6b3f96
faustbrian <faustbrian@users.noreply.github.com>
2020-10-21 15:44:21 +0000

    style: resolve style guide violations

--- app/Http/Controllers/ShowWalletsByBalanceController.php
+++ app/Http/Controllers/ShowWalletsByBalanceController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
 use Illuminate\View\View;
 
 final class ShowWalletsByBalanceController
