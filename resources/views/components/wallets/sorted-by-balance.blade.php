3e6e56650b493793bbc604cd09ea154429c70272
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-21 14:07:17 +0100

    refactor: remove top page border

--- resources/views/components/wallets/sorted-by-balance.blade.php
+++ resources/views/components/wallets/sorted-by-balance.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white border-t-20 border-theme-secondary-100 dark:border-black dark:bg-theme-secondary-900">
+<div class="bg-white dark:bg-theme-secondary-900">
     <div class="py-16 content-container flex-col md:px-8">
         <h1 class="header-2">@lang('pages.wallets.title')</h1>
 
