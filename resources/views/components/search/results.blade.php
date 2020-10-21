3d87085f87012427d8c4a3d4cba60798689ef159
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-21 21:40:38 +0100

    feat: initial search results page

---
+++ resources/views/components/search/results.blade.php
@@ -0,0 +1,9 @@
+<div class="bg-white border-t-20 border-theme-secondary-100 dark:border-black dark:bg-theme-secondary-900">
+    <div class="flex-col py-16 content-container md:px-8">
+        <h1 class="header-2">@lang('pages.search_results.title')</h1>
+
+        <div class="mt-4">
+            <livewire:transaction-table />
+        </div>
+    </div>
+</div>
