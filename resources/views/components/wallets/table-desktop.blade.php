081032ce2dc9ea35f614c32f54622557b2ab9fc1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-21 14:07:52 +0100

    refactor: hide info column if no data

--- resources/views/components/wallets/table-desktop.blade.php
+++ resources/views/components/wallets/table-desktop.blade.php
@@ -1,9 +1,20 @@
+@php ($hasInfo = false)
+
+@foreach ($wallets as $wallet)
+    @if ($wallet->isKnown() || $wallet->isOwnedByExchange())
+        @php ($hasInfo = true)
+        @break
+    @endif
+@endforeach
+
 <div class="hidden table-container w-full md:block">
     <table>
         <thead>
             <tr>
                 <th><span class="pl-14">@lang('general.wallet.address')</span></th>
-                <th class="text-center">@lang('general.wallet.info')</th>
+                @if ($hasInfo)
+                    <th class="text-center">@lang('general.wallet.info')</th>
+                @endif
                 <th class="text-right">@lang('general.wallet.balance')</th>
                 <th class="hidden text-right lg:table-cell">@lang('general.wallet.supply')</th>
             </tr>
@@ -12,21 +23,23 @@
             @foreach($wallets as $wallet)
                 <tr>
                     <td><x-general.address :address="$wallet->address()" /></td>
-                    <td class="text-center">
-                        <div class="flex items-center justify-center text-theme-secondary-500 space-x-2">
-                            @if ($wallet->isKnown())
-                                <div data-tippy-content="@lang('general.verified_address')">
-                                    @svg('app-verified', 'w-5 h-5')
-                                </div>
-                            @endif
+                    @if ($hasInfo)
+                        <td class="text-center">
+                            <div class="flex items-center justify-center text-theme-secondary-500 space-x-2">
+                                @if ($wallet->isKnown())
+                                    <div data-tippy-content="@lang('general.verified_address')">
+                                        @svg('app-verified', 'w-5 h-5')
+                                    </div>
+                                @endif
 
-                            @if ($wallet->isOwnedByExchange())
-                                <div data-tippy-content="@lang('general.exchange')">
-                                    @svg('app-exchange', 'w-5 h-5')
-                                </div>
-                            @endif
-                        </div>
-                    </td>
+                                @if ($wallet->isOwnedByExchange())
+                                    <div data-tippy-content="@lang('general.exchange')">
+                                        @svg('app-exchange', 'w-5 h-5')
+                                    </div>
+                                @endif
+                            </div>
+                        </td>
+                    @endif
                     <td class="text-right">{{ $wallet->balance() }}</td>
                     <td class="hidden text-right lg:table-cell">{{ number_format($wallet->balancePercentage(), 2) }} %</td>
                 </tr>
