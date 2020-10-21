78012dd776031e04c6f8f4b0e18ffe767e663d8c
Highjhacker <jolanbeer@gmail.com>
2020-10-21 15:36:23 +0200

    feat: added chart component to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix
     .js('resources/js/app.js', 'public/js')
+    .copy('resources/js/chart.js', 'public/js/chart.js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
