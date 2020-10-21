af0c9bd2f2043adf07ba145590de4edaeb4cba61
Highjhacker <Highjhacker@users.noreply.github.com>
2020-10-21 13:46:41 +0000

    style: resolve style guide violations

--- resources/views/components/charts/price.blade.php
+++ resources/views/components/charts/price.blade.php
@@ -4,14 +4,14 @@
 @endpush
 
 <div x-data="makeChart()" x-init="renderChart()" class="bg-white border-t-20 border-theme-secondary-100 dark:border-black dark:bg-theme-secondary-900">
-    <div class="content-container-full-width md:py-16 md:px-8 flex flex-col">
-        <div class="flex w-full flex-col">
-            <div class="flex relative w-full justify-between items-center">
+    <div class="flex flex-col content-container-full-width md:py-16 md:px-8">
+        <div class="flex flex-col w-full">
+            <div class="relative flex items-center justify-between w-full">
                 <h2>Price</h2>
 
-                <x-ark-dropdown dropdown-classes="left-0 w-32 mt-3" button-class="h-10 w-32 dropdown-button">
+                <x-ark-dropdown dropdown-classes="left-0 w-32 mt-3" button-class="w-32 h-10 dropdown-button">
                     @slot('button')
-                        <div class="flex flex-inline items-center w-full justify-end font-semibold text-theme-secondary-700 space-x-2">
+                        <div class="flex items-center justify-end w-full space-x-2 font-semibold flex-inline text-theme-secondary-700">
                             <span>1 Week</span>
                             <span :class="{ 'rotate-180': open }" class="transition duration-150 ease-in-out">
                                 @svg('chevron-up', 'h-3 w-3')
@@ -30,7 +30,7 @@
                     </div>
                 </x-ark-dropdown>
             </div>
-            <div class="flex w-full justify-between mt-5">
+            <div class="flex justify-between w-full mt-5">
                 <div class="flex items-center pr-5 mr-5 border-r border-theme-secondary-200">
                    <div class="flex flex-col">
                         <span class="font-semibold text-theme-secondary-500">Min Price</span>
