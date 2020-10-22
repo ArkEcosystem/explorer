ffaf9d2fee9a9422263a2337743621f12527e189
Brian Faust <hello@basecode.sh>
2020-10-22 09:46:03 +0300

    wip

--- tests/Unit/Services/Search/BlockSearchTest.php
+++ tests/Unit/Services/Search/BlockSearchTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use App\Models\Block;
 use App\Services\Search\BlockSearch;
 
+use App\Services\Timestamp;
+use Carbon\Carbon;
 use function Tests\configureExplorerDatabase;
 
 beforeEach(fn () => configureExplorerDatabase());
@@ -17,4 +19,56 @@ it('should search for a block by id', function () {
     ]);
 
     expect($result->get())->toHaveCount(1);
+});
+
+it('should search for blocks by timestamp minimum', function () {
+    $today = Carbon::now();
+    $todayGenesis = Timestamp::fromUnix($today->unix())->unix();
+
+    $yesterday = Carbon::now()->subDay();
+    $yesterdayGenesis = Timestamp::fromUnix($yesterday->unix())->unix();
+
+    Block::factory(10)->create(['timestamp' => $todayGenesis]);
+    Block::factory(10)->create(['timestamp' => $yesterdayGenesis]);
+
+    $result = (new BlockSearch())->search([
+        'dateFrom' => $today->toString(),
+    ]);
+
+    expect($result->get())->toHaveCount(10);
+});
+
+it('should search for blocks by timestamp maximum', function () {
+    $today = Carbon::now();
+    $todayGenesis = Timestamp::fromUnix($today->unix())->unix();
+
+    $yesterday = Carbon::now()->subDay();
+    $yesterdayGenesis = Timestamp::fromUnix($yesterday->unix())->unix();
+
+    Block::factory(10)->create(['timestamp' => $todayGenesis]);
+    Block::factory(10)->create(['timestamp' => $yesterdayGenesis]);
+
+    $result = (new BlockSearch())->search([
+        'dateTo' => $yesterday->toString(),
+    ]);
+
+    expect($result->get())->toHaveCount(10);
+});
+
+it('should search for blocks by timestamp range', function () {
+    $today = Carbon::now();
+    $todayGenesis = Timestamp::fromUnix($today->unix())->unix();
+
+    $yesterday = Carbon::now()->subDay();
+    $yesterdayGenesis = Timestamp::fromUnix($yesterday->unix())->unix();
+
+    Block::factory(10)->create(['timestamp' => $todayGenesis]);
+    Block::factory(10)->create(['timestamp' => $yesterdayGenesis]);
+
+    $result = (new BlockSearch())->search([
+        'dateFrom' => $yesterday->toString(),
+        'dateTo'   => $yesterday->toString(),
+    ]);
+
+    expect($result->get())->toHaveCount(10);
 });
--- tests/Unit/Services/Search/TransactionSearchTest.php
+++ tests/Unit/Services/Search/TransactionSearchTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use App\Models\Transaction;
 use App\Services\Search\TransactionSearch;
 
+use App\Services\Timestamp;
+use Carbon\Carbon;
 use function Tests\configureExplorerDatabase;
 
 beforeEach(fn () => configureExplorerDatabase());
@@ -17,4 +19,56 @@ it('should search for a transaction by id', function () {
     ]);
 
     expect($result->get())->toHaveCount(1);
+});
+
+it('should search for transactions by timestamp minimum', function () {
+    $today = Carbon::now();
+    $todayGenesis = Timestamp::fromUnix($today->unix())->unix();
+
+    $yesterday = Carbon::now()->subDay();
+    $yesterdayGenesis = Timestamp::fromUnix($yesterday->unix())->unix();
+
+    Transaction::factory(10)->create(['timestamp' => $todayGenesis]);
+    Transaction::factory(10)->create(['timestamp' => $yesterdayGenesis]);
+
+    $result = (new TransactionSearch())->search([
+        'dateFrom' => $today->toString(),
+    ]);
+
+    expect($result->get())->toHaveCount(10);
+});
+
+it('should search for transactions by timestamp maximum', function () {
+    $today = Carbon::now();
+    $todayGenesis = Timestamp::fromUnix($today->unix())->unix();
+
+    $yesterday = Carbon::now()->subDay();
+    $yesterdayGenesis = Timestamp::fromUnix($yesterday->unix())->unix();
+
+    Transaction::factory(10)->create(['timestamp' => $todayGenesis]);
+    Transaction::factory(10)->create(['timestamp' => $yesterdayGenesis]);
+
+    $result = (new TransactionSearch())->search([
+        'dateTo' => $yesterday->toString(),
+    ]);
+
+    expect($result->get())->toHaveCount(10);
+});
+
+it('should search for transactions by timestamp range', function () {
+    $today = Carbon::now();
+    $todayGenesis = Timestamp::fromUnix($today->unix())->unix();
+
+    $yesterday = Carbon::now()->subDay();
+    $yesterdayGenesis = Timestamp::fromUnix($yesterday->unix())->unix();
+
+    Transaction::factory(10)->create(['timestamp' => $todayGenesis]);
+    Transaction::factory(10)->create(['timestamp' => $yesterdayGenesis]);
+
+    $result = (new TransactionSearch())->search([
+        'dateFrom' => $yesterday->toString(),
+        'dateTo'   => $yesterday->toString(),
+    ]);
+
+    expect($result->get())->toHaveCount(10);
 });
