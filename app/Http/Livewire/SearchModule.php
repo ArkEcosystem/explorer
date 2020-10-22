53e5fe4fa36ab69228a4dbad9b9a8864d287dc34
Brian Faust <hello@basecode.sh>
2020-10-22 11:17:05 +0300

    wip

--- app/Http/Livewire/SearchModule.php
+++ app/Http/Livewire/SearchModule.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Http\Livewire;
 
 use App\Http\Livewire\Concerns\ManagesSearch;
+use App\Services\Search\BlockSearch;
+use App\Services\Search\TransactionSearch;
+use App\Services\Search\WalletSearch;
 use Livewire\Component;
 
 final class SearchModule extends Component
@@ -30,7 +33,58 @@ final class SearchModule extends Component
         if ($this->isAdvanced) {
             $this->emit('searchTriggered', $data);
         } else {
+            if ($this->searchWallet()) {
+                return;
+            }
+
+            if ($this->searchTransaction()) {
+                return;
+            }
+
+            if ($this->searchBlock()) {
+                return;
+            }
+
             $this->redirectRoute('search', ['state' => $data]);
         }
+    }
+
+    private function searchWallet(): bool
+    {
+        $wallet = (new WalletSearch())->search(['term' => $this->state['term']])->first();
+
+        if ($wallet) {
+            $this->redirectRoute('wallet', $wallet->address);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function searchTransaction(): bool
+    {
+        $transaction = (new TransactionSearch())->search(['term' => $this->state['term']])->first();
+
+        if ($transaction) {
+            $this->redirectRoute('transaction', $transaction->id);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function searchBlock(): bool
+    {
+        $block = (new BlockSearch())->search(['term' => $this->state['term']])->first();
+
+        if ($block) {
+            $this->redirectRoute('block', $block->id);
+
+            return true;
+        }
+
+        return false;
     }
 }
