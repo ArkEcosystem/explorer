04902f56cb3e3e640f0eccf8184fcb792c43a14c
Brian Faust <hello@basecode.sh>
2020-10-23 07:25:36 +0300

    wip

--- app/Services/ExchangeRate.php
+++ app/Services/ExchangeRate.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Carbon\Carbon;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cache;
 
 final class ExchangeRate
@@ -12,7 +13,7 @@ final class ExchangeRate
     public static function convert(float $amount, int $timestamp): string
     {
         $prices       = Cache::get('prices.'.Settings::currency());
-        $exchangeRate = $prices[Carbon::parse(static::timestamp($timestamp))->format('Y-m-d')];
+        $exchangeRate = Arr::get($prices, Carbon::parse(static::timestamp($timestamp))->format('Y-m-d'), 0);
 
         return NumberFormatter::currency($amount * $exchangeRate, Settings::currency());
     }
