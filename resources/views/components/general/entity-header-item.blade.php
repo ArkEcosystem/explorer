829968c1077966e33214f3baec217f4c2c0351fb
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-23 18:24:58 +0100

    feat: entity header component

---
+++ resources/views/components/general/entity-header-item.blade.php
@@ -0,0 +1,23 @@
+<div class="entity-header-item">
+    <div class="flex items-center">
+        <div class="circled-icon text-theme-secondary-900 border-theme-secondary-900">
+            @if ($icon ?? false)
+                @svg($icon, 'w-5 h-5')
+            @elseif ($avatar ?? false)
+                <x-general.avatar :identifier="$avatar" size="w-8 h-8" />
+            @endif
+        </div>
+    </div>
+
+    <div class="flex flex-col flex-1 justify-between font-semibold ml-4">
+        <div class="text-sm leading-tight text-theme-secondary-600">{{ $title }}</div>
+
+        @if ($url ?? false)
+            <a href="{{ $url }}" class="link flex leading-tight text-theme-secondary-400">
+                <span class="truncate">{{ $text }}</span>
+            </a>
+        @else
+            <span class="truncate leading-tight">{{ $text }}</span>
+        @endif
+    </div>
+</div>
---
+++ resources/views/components/general/entity-header.blade.php
@@ -0,0 +1,9 @@
+<div class="flex flex-col rounded-lg overflow-hidden border border-theme-secondary-300">
+    <div class="bg-black p-8">
+        {{ $top }}
+    </div>
+
+    <div class="bg-theme-secondary-100 p-8">
+        {{ $bottom }}
+    </div>
+</div>
