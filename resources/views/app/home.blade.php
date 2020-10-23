71d2d7c13d387ef2f19a5baeabeec2cc7bbe905f
ItsANameToo <itsanametoo@protonmail.com>
2020-10-23 15:57:59 +0200

    wip

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -9,9 +9,9 @@
         <x-general.search.header />
 
         @if(Settings::usesCharts())
-            <div class="content-container">
+            <div class="hidden sm:flex content-container">
                 <div class="flex flex-col w-full divide-y divide-dashed divide-theme-secondary-300 dark:divide-theme-secondary-700">
-                    <div class="flex-col hidden w-full py-16 space-x-0 sm:flex lg:flex-row lg:space-x-10">
+                    <div class="flex-col w-full pt-16 space-x-0 flex lg:flex-row lg:space-x-10">
                         @if(Settings::usesPriceChart())
                             <x-charts.price :data="$prices" identifier="price" colours-scheme="#339A51" />
                         @endif
@@ -21,22 +21,11 @@
                         @endif
                     </div>
 
-                    <div class="flex-row justify-between hidden w-full mb-12 xl:flex">
+                    <div class="w-full mt-5 pt-8 mb-16 grid grid-cols-2 grid-flow-row xl:grid-cols-4 gap-6 gap-y-12 xl:gap-y-4">
                         <x-details-box :title="trans('pages.home.network-details.price')" :value="$aggregates['price'] . ' ' . 'BTC'" icon="app-price" />
                         <x-details-box :title="trans('pages.home.network-details.lifetime_transactions_volume')" :value="$aggregates['volume'] . ' ' . Network::currencySymbol()" icon="app-volume" />
-
                         <x-details-box :title="trans('pages.home.network-details.lifetime_transactions')" :value="$aggregates['transactionsCount']" icon="app-transactions-amount" />
                         <x-details-box :title="trans('pages.home.network-details.total_votes')" :value="$aggregates['votesCount'] . ' ' . Network::currencySymbol()" extra-value="74.08%" icon="app-votes" />
-                    </div>
-                    <div class="flex flex-row justify-between w-full mb-12 xl:hidden space-x-18">
-                        <div class="space-y-12">
-                            <x-details-box :title="trans('pages.home.network-details.price')" :value="$aggregates['price'] . ' ' . 'BTC'" icon="app-price" />
-                            <x-details-box :title="trans('pages.home.network-details.lifetime_transactions')" :value="$aggregates['transactionsCount']" icon="app-transactions-amount" />
-                        </div>
-                        <div class="space-y-12">
-                            <x-details-box :title="trans('pages.home.network-details.lifetime_transactions_volume')" :value="$aggregates['volume'] . ' ' . Network::currencySymbol()" icon="app-volume" />
-                            <x-details-box :title="trans('pages.home.network-details.total_votes')" :value="$aggregates['votesCount'] . ' ' . Network::currencySymbol()" extra-value="74.08%" icon="app-votes" />
-                        </div>
                     </div>
                 </div>
             </div>
