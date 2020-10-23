3bac4ce195f0af7116a5111ca4c1f2cc2acd6949
ItsANameToo <ItsANameToo@users.noreply.github.com>
2020-10-23 13:58:44 +0000

    style: resolve style guide violations

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -11,7 +11,7 @@
         @if(Settings::usesCharts())
             <div class="hidden sm:flex content-container">
                 <div class="flex flex-col w-full divide-y divide-dashed divide-theme-secondary-300 dark:divide-theme-secondary-700">
-                    <div class="flex-col w-full pt-16 space-x-0 flex lg:flex-row lg:space-x-10">
+                    <div class="flex flex-col w-full pt-16 space-x-0 lg:flex-row lg:space-x-10">
                         @if(Settings::usesPriceChart())
                             <x-charts.price :data="$prices" identifier="price" colours-scheme="#339A51" />
                         @endif
@@ -21,7 +21,7 @@
                         @endif
                     </div>
 
-                    <div class="w-full mt-5 pt-8 mb-16 grid grid-cols-2 grid-flow-row xl:grid-cols-4 gap-6 gap-y-12 xl:gap-y-4">
+                    <div class="grid w-full grid-flow-row grid-cols-2 gap-6 pt-8 mt-5 mb-16 xl:grid-cols-4 gap-y-12 xl:gap-y-4">
                         <x-details-box :title="trans('pages.home.network-details.price')" :value="$aggregates['price'] . ' ' . 'BTC'" icon="app-price" />
                         <x-details-box :title="trans('pages.home.network-details.lifetime_transactions_volume')" :value="$aggregates['volume'] . ' ' . Network::currencySymbol()" icon="app-volume" />
                         <x-details-box :title="trans('pages.home.network-details.lifetime_transactions')" :value="$aggregates['transactionsCount']" icon="app-transactions-amount" />
