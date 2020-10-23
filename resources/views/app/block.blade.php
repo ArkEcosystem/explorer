dc316508b89f22a58c7ffcd019491ea34183b525
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-23 22:44:28 +0100

    chore: add missing clipboard include

--- resources/views/app/block.blade.php
+++ resources/views/app/block.blade.php
@@ -5,6 +5,10 @@
         <meta property="og:description" content="@lang('metatags.block.description')">
     @endpush
 
+    @push('scripts')
+        <script src="{{ mix('js/clipboard.js')}}"></script>
+    @endpush
+
     @section('breadcrumbs')
         <x-general.breadcrumbs :crumbs="[
             ['route' => 'home', 'label' => trans('menus.home')],
