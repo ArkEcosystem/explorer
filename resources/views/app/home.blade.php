f873a2e48404714a2a2d2779c5d453eb3f258e62
Highjhacker <jolanbeer@gmail.com>
2020-10-23 16:40:58 +0200

    chore: style tweaks

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -9,23 +9,25 @@
         <x-general.search.header />
 
         @if(Settings::usesCharts())
-            <div class="hidden sm:flex content-container">
-                <div class="flex flex-col w-full divide-y divide-dashed divide-theme-secondary-300 dark:divide-theme-secondary-700">
-                    <div class="flex flex-col w-full pt-16 space-x-0 lg:flex-row lg:space-x-10">
-                        @if(Settings::usesPriceChart())
-                            <x-charts.price :data="$prices" identifier="price" colours-scheme="#339A51" />
-                        @endif
+            <div class="hidden sm:flex">
+                <div class="content-container">
+                    <div class="flex flex-col w-full divide-y divide-dashed divide-theme-secondary-300 dark:divide-theme-secondary-700">
+                        <div class="flex flex-col w-full pt-16 space-x-0 lg:flex-row lg:space-x-10">
+                            @if(Settings::usesPriceChart())
+                                <x-charts.price :data="$prices" identifier="price" colours-scheme="#339A51" />
+                            @endif
 
-                        @if(Settings::usesFeeChart())
-                            <x-charts.price :data="$fees" identifier="fees" colours-scheme="#FFAE10" />
-                        @endif
-                    </div>
+                            @if(Settings::usesFeeChart())
+                                <x-charts.price :data="$fees" identifier="fees" colours-scheme="#FFAE10" />
+                            @endif
+                        </div>
 
-                    <div class="grid w-full grid-flow-row grid-cols-2 gap-6 pt-8 mt-5 mb-16 xl:grid-cols-4 gap-y-12 xl:gap-y-4">
-                        <x-details-box :title="trans('pages.home.network-details.price')" :value="$aggregates['price'] . ' ' . 'BTC'" icon="app-price" />
-                        <x-details-box :title="trans('pages.home.network-details.lifetime_transactions_volume')" :value="$aggregates['volume'] . ' ' . Network::currencySymbol()" icon="app-volume" />
-                        <x-details-box :title="trans('pages.home.network-details.lifetime_transactions')" :value="$aggregates['transactionsCount']" icon="app-transactions-amount" />
-                        <x-details-box :title="trans('pages.home.network-details.total_votes')" :value="$aggregates['votesCount'] . ' ' . Network::currencySymbol()" extra-value="74.08%" icon="app-votes" />
+                        <div class="grid w-full grid-flow-row grid-cols-2 gap-6 pt-8 mt-5 mb-16 xl:grid-cols-4 gap-y-12 xl:gap-y-4">
+                            <x-details-box :title="trans('pages.home.network-details.price')" :value="$aggregates['price'] . ' ' . 'BTC'" icon="app-price" />
+                            <x-details-box :title="trans('pages.home.network-details.lifetime_transactions_volume')" :value="$aggregates['volume'] . ' ' . Network::currencySymbol()" icon="app-volume" />
+                            <x-details-box :title="trans('pages.home.network-details.lifetime_transactions')" :value="$aggregates['transactionsCount']" icon="app-transactions-amount" />
+                            <x-details-box :title="trans('pages.home.network-details.total_votes')" :value="$aggregates['votesCount'] . ' ' . Network::currencySymbol()" :extra-value="$aggregates['votesPercentage'] . ' %'" icon="app-votes" />
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/components/details-box.blade.php
+++ resources/views/components/details-box.blade.php
@@ -6,7 +6,7 @@
             @svg($icon, 'h-5 w-5 text-theme-secondary-900'.($iconClass ?? ''))
         </div>
     @endif
-    <div class="flex flex-col">
+    <div class="flex flex-col space-y-2">
         <span class="text-sm font-semibold text-theme-secondary-500">{{ $title }}</span>
         @if($value || is_numeric($value) ?? false)
             <span class="text-lg font-semibold text-theme-secondary-700">{{ $value }} @if($extraValue ?? false) <span class="text-theme-secondary-500">{{ $extraValue }}</span> @endif</span>
