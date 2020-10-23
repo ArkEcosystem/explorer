93de2ff447e451985a192762bf62fca5dbf54864
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-23 23:21:26 +0100

    chore: move text to lang files

--- resources/views/components/transaction/header.blade.php
+++ resources/views/components/transaction/header.blade.php
@@ -3,17 +3,33 @@
         <x-general.search.header-slim :title="trans('pages.transaction.title')" />
 
         <x-general.entity-header
-            title="Transaction ID"
+            :title="trans('pages.transaction.transaction_id')"
             value="58dc75dfd97709acd4a268e318ee7f53651fdc54294325c4a7a4bca08777558a"
         >
             <x-slot name="logo"><span class="text-lg font-medium">ID</span></x-slot>
 
             <x-slot name="bottom">
                 <div class="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-4">
-                    <x-general.entity-header-item title="Transaction Type" icon="app-transactions.transfer" text="Transfer" />
-                    <x-general.entity-header-item title="Fee" icon="app-fee" text="0.09447035 ARK" />
-                    <x-general.entity-header-item title="Amount" icon="app-votes" text="23,097.37265146 ARK" />
-                    <x-general.entity-header-item title="Confirmations" icon="app-confirmations" text="512,189" />
+                    <x-general.entity-header-item
+                        :title="trans('pages.transaction.transaction_type')"
+                        icon="app-transactions.transfer"
+                        text="Transfer"
+                    />
+                    <x-general.entity-header-item
+                        :title="trans('pages.transaction.fee')"
+                        icon="app-fee"
+                        text="0.09447035 ARK"
+                    />
+                    <x-general.entity-header-item
+                        :title="trans('pages.transaction.amount')"
+                        icon="app-votes"
+                        text="23,097.37265146 ARK"
+                    />
+                    <x-general.entity-header-item
+                        :title="trans('pages.transaction.confirmations')"
+                        icon="app-confirmations"
+                        text="512,189"
+                    />
                 </div>
             </x-slot>
         </x-general.entity-header>
