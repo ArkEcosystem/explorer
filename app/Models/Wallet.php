38d7c0d98c6e9561809d76acdc2792c4f2930abe
faustbrian <faustbrian@users.noreply.github.com>
2020-10-24 09:20:08 +0000

    style: resolve style guide violations

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use GeneaLabs\LaravelModelCaching\Traits\Cachable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
