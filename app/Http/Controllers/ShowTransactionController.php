63a84c59a5d9ce02125d69f80996ee1c22b50d7a
Brian Faust <hello@basecode.sh>
2020-10-24 05:02:13 +0300

    wip

--- app/Http/Controllers/ShowTransactionController.php
+++ app/Http/Controllers/ShowTransactionController.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Transaction;
+use App\ViewModels\ViewModelFactory;
 use Illuminate\View\View;
 
 final class ShowTransactionController
 {
     public function __invoke(Transaction $transaction): View
     {
-        return view('app.transaction', ['transaction' => $transaction]);
+        return view('app.transaction', [
+            'transaction' => ViewModelFactory::make($transaction),
+        ]);
     }
 }
