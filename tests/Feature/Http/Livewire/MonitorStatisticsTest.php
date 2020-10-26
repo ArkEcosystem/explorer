e20895cb4606fbe6d6229c3ed93774be8ad77e49
Brian Faust <hello@basecode.sh>
2020-10-26 05:35:10 +0200

    wip

--- tests/Feature/Http/Livewire/MonitorStatisticsTest.php
+++ tests/Feature/Http/Livewire/MonitorStatisticsTest.php
@@ -3,10 +3,14 @@
 declare(strict_types=1);
 
 use App\Http\Livewire\MonitorStatistics;
+
 use Livewire\Livewire;
+use function Tests\configureExplorerDatabase;
 
 // @TODO: make assertions about the visibility of the data
 it('should render the component', function () {
+    configureExplorerDatabase();
+
     Livewire::test(MonitorStatistics::class)
         ->assertSee(trans('pages.monitor.statistics.delegate_registrations'))
         ->assertSee(trans('pages.monitor.statistics.block_reward'))
