75f243cda0678487a8834babbccacd1abab3c746
faustbrian <faustbrian@users.noreply.github.com>
2020-10-27 03:05:04 +0000

    style: resolve style guide violations

--- app/Models/Round.php
+++ app/Models/Round.php
@@ -7,7 +7,6 @@ namespace App\Models;
 use App\Models\Casts\BigInteger;
 use App\Services\BigNumber;
 use GeneaLabs\LaravelModelCaching\Traits\Cachable;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
