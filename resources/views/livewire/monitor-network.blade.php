201ba0064cacea64903a9a3ff9c2d6c38f6a64de
Brian Faust <hello@basecode.sh>
2020-10-27 04:29:48 +0200

    wip

--- resources/views/livewire/monitor-network.blade.php
+++ resources/views/livewire/monitor-network.blade.php
@@ -1,5 +1,4 @@
-{{-- <div id="network-list" class="w-full" wire:poll.8s> --}}
-<div id="network-list" class="w-full">
+<div id="network-list" class="w-full" wire:poll.8s>
     <x-delegates.table-desktop-monitor :delegates="$delegates" />
 
     {{-- <x-delegates.list-mobile-monitor :delegates="$delegates" /> --}}
