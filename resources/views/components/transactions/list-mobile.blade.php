fb8771c880e4ca10a1edd76f2668d0bc43c4c6bf
Highjhacker <jolanbeer@gmail.com>
2020-10-27 12:38:27 +0100

    refactor: apply design changes on mobile versions of blocks transactions and wallets tables

--- resources/views/components/transactions/list-mobile.blade.php
+++ resources/views/components/transactions/list-mobile.blade.php
@@ -1,121 +1,56 @@
-<div class="space-y-8 divide-y">
+<div class="space-y-8 divide-y md:hidden">
     @foreach ($transactions as $transaction)
-        <div class="flex flex-col space-y-3 w-full pt-8 {{ $loop->first ? '' : 'border-t'}} border-theme-secondary-300">
-            <div class="flex w-full justify-between">
-                @lang('general.transaction.id')
+    <div class="flex flex-col space-y-3 w-full pt-8 {{ $loop->first ? '' : 'border-t'}} border-theme-secondary-300">
+        <div class="flex w-full justify-between">
+            @lang('general.transaction.id')
 
-                <x-general.loading-state.text class="font-semibold">
-                    <x-slot name="text">
-                        <x-truncate-middle :value="$transaction->id()" />
-                    </x-slot>
-                </x-general.loading-state.text>
-
-                <a href="{{ $transaction->url() }}" class="font-semibold link" wire:loading.class="hidden">
+            <x-general.loading-state.text class="font-semibold">
+                <x-slot name="text">
                     <x-truncate-middle :value="$transaction->id()" />
-                </a>
-            </div>
+                </x-slot>
+            </x-general.loading-state.text>
 
-            <div class="flex w-full justify-between">
-                @lang('general.transaction.timestamp')
+            <a href="{{ $transaction->url() }}" class="font-semibold link" wire:loading.class="hidden">
+                <x-truncate-middle :value="$transaction->id()" />
+            </a>
+        </div>
 
-                <x-general.loading-state.text :text="$transaction->timestamp()" />
-                <span wire:loading.class="hidden">{{ $transaction->timestamp() }}</span>
-            </div>
+        <div class="flex w-full justify-between">
+            @lang('general.transaction.timestamp')
 
-            <div class="flex w-full justify-between">
-                @lang('general.transaction.sender')
+            <x-general.loading-state.text :text="$transaction->timestamp()" />
 
-                <x-general.address :address="$transaction->sender()" with-loading />
-            </div>
+            <span wire:loading.class="hidden">{{ $transaction->timestamp() }}</span>
+        </div>
 
-            <div class="flex w-full justify-between">
-                @lang('general.transaction.recipient')
+        <div class="flex w-full justify-between">
+            @lang('general.transaction.sender')
+            <x-general.address :address="$transaction->sender()" with-loading />
+        </div>
 
-                <x-general.address :address="$transaction->recipient() ?? $transaction->sender()" with-loading />
-            </div>
+        <div class="flex w-full justify-between">
+            @lang('general.transaction.recipient')
+            <x-general.address :address="$transaction->recipient() ?? $transaction->sender()" with-loading />
+        </div>
 
-            <div class="flex w-full justify-between">
-                @lang('general.transaction.amount')
+        <div class="flex w-full justify-between">
+            @lang('general.transaction.amount')
 
-                <x-general.loading-state.text :text="$transaction->amount()" />
+            <x-general.loading-state.text :text="$transaction->amount()" />
 
-                <div wire:loading.class="hidden">
-                    <x-general.amount-fiat-tooltip :amount="$transaction->amount()" :fiat="$transaction->amountFiat()" />
-                </div>
-            </div>
-
-            <div class="flex w-full justify-between">
-                @lang('general.transaction.fee')
-
-                <x-general.loading-state.text :text="$transaction->fee()" />
-
-                <div wire:loading.class="hidden">
-                    <x-general.amount-fiat-tooltip :amount="$transaction->fee()" :fiat="$transaction->feeFiat()" />
-                </div>
+            <div wire:loading.class="hidden">
+                <x-general.amount-fiat-tooltip :amount="$transaction->amount()" :fiat="$transaction->amountFiat()" />
             </div>
         </div>
+
+        <div class="flex w-full justify-between">
+            @lang('general.transaction.fee')
+            <x-general.loading-state.text :text="$transaction->fee()" />
+
+            <div wire:loading.class="hidden">
+                <x-general.amount-fiat-tooltip :amount="$transaction->fee()" :fiat="$transaction->feeFiat()" />
+            </div>
+        </div>
+    </div>
     @endforeach
 </div>
-
-
-
-{{--<div class="table-list-mobile-row">
-            <table>
-                <tbody>
-                    <tr>
-                        <td width="150">@lang('general.transaction.id')</td>
-                        <td class="text-right w-full">
-                            <x-general.loading-state.text class="font-semibold">
-                                <x-slot name="text">
-                                    <x-truncate-middle :value="$transaction->id()" />
-                                </x-slot>
-                            </x-general.loading-state.text>
-
-                            <a href="{{ $transaction->url() }}" class="font-semibold link" wire:loading.class="hidden">
-<x-truncate-middle :value="$transaction->id()" />
-</a>
-</td>
-</tr>
-<tr>
-    <td>@lang('general.transaction.timestamp')</td>
-    <td class="text-right w-full">
-        <x-general.loading-state.text :text="$transaction->timestamp()" />
-
-        <span wire:loading.class="hidden">{{ $transaction->timestamp() }}</span>
-    </td>
-</tr>
-<tr>
-    <td>@lang('general.transaction.sender')</td>
-    <td class="text-right w-full">
-        <x-general.address :address="$transaction->sender()" with-loading />
-    </td>
-</tr>
-<tr>
-    <td>@lang('general.transaction.recipient')</td>
-    <td class="text-right w-full">
-        <x-general.address :address="$transaction->recipient() ?? $transaction->sender()" with-loading />
-    </td>
-</tr>
-<tr>
-    <td>@lang('general.transaction.amount')</td>
-    <td class="text-right w-full">
-        <x-general.loading-state.text :text="$transaction->amount()" />
-
-        <div wire:loading.class="hidden">
-            <x-general.amount-fiat-tooltip :amount="$transaction->amount()" :fiat="$transaction->amountFiat()" />
-        </div>
-    </td>
-</tr>
-<tr>
-    <td>@lang('general.transaction.fee')</td>
-    <td class="text-right w-full">
-        <x-general.loading-state.text :text="$transaction->fee()" />
-
-        <div wire:loading.class="hidden">
-            <x-general.amount-fiat-tooltip :amount="$transaction->fee()" :fiat="$transaction->feeFiat()" />
-        </div>
-    </td>
-</tr>
-</tbody>
-</table>
-</div>--}}
