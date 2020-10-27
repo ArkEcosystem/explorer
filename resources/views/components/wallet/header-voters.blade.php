fc387955f10a5d5bac32fd23337447a32877ceaf
Brian Faust <hello@basecode.sh>
2020-10-27 13:10:11 +0200

    wip

--- resources/views/components/wallet/header-voters.blade.php
+++ resources/views/components/wallet/header-voters.blade.php
@@ -11,15 +11,23 @@
             </x-slot>
 
             <x-slot name="extra">
-                <div class="flex flex-col justify-between flex-1 pl-4 font-semibold border-l md:ml-8 border-theme-secondary-800">
-                    <div class="text-sm leading-tight text-theme-secondary-600 dark:text-theme-secondary-700">
-                        @lang('pages.wallet.delegate.voters')
+                <div class="flex justify-between flex-1 pl-4 font-semibold border-l md:ml-8 border-theme-secondary-800">
+                    <div class="items-center hidden md:flex">
+                        <div class="circled-icon text-theme-secondary-700 border-theme-secondary-800">
+                            @svg('app-transactions.vote', 'w-5 h-5')
+                        </div>
                     </div>
 
-                    <div class="flex items-center space-x-2 leading-tight">
-                        <span class="truncate text-theme-secondary-400 dark:text-theme-secondary-200">
-                            {{ $wallet->voterCount() }}
-                        </span>
+                    <div class="flex flex-col justify-between flex-1 pl-4">
+                        <div class="text-sm leading-tight text-theme-secondary-600 dark:text-theme-secondary-700">
+                            @lang('pages.wallet.delegate.voters')
+                        </div>
+
+                        <div class="flex items-center space-x-2 leading-tight">
+                            <span class="truncate text-theme-secondary-400 dark:text-theme-secondary-200">
+                                {{ $wallet->voterCount() }}
+                            </span>
+                        </div>
                     </div>
                 </div>
 
