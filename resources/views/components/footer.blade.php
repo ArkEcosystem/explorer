9eeecc4a7137f7dfb9c79e80aa7ba62736ea2bc3
faustbrian <faustbrian@users.noreply.github.com>
2020-10-27 13:45:15 +0000

    style: resolve style guide violations

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
-<div class="bg-theme-secondary-900 border-t border-theme-secondary-800">
-    <div class="hidden lg:flex flex-col px-8 mx-auto max-w-7xl">
+<div class="border-t bg-theme-secondary-900 border-theme-secondary-800">
+    <div class="flex-col hidden px-8 mx-auto lg:flex max-w-7xl">
         <x-ark-footer-bar-desktop class="" />
     </div>
 
-    <x-ark-footer-bar-mobile class="lg:hidden px-8 pb-8" />
+    <x-ark-footer-bar-mobile class="px-8 pb-8 lg:hidden" />
 </div>
