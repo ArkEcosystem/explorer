e73dedb9d0b7e535dcd49ed232df097a51e0a359
Highjhacker <jolanbeer@gmail.com>
2020-10-27 12:37:35 +0100

    fix: put back inline

--- resources/views/components/general/amount-fiat-tooltip.blade.php
+++ resources/views/components/general/amount-fiat-tooltip.blade.php
@@ -8,7 +8,7 @@
     <span>
 @endif
     @if(Network::canBeExchanged())
-        <div class="" @if ($amount ?? false) data-tippy-content="{{ $fiat }}" @endif>
+        <div class="inline" @if ($amount ?? false) data-tippy-content="{{ $fiat }}" @endif>
             {{ $amount }}
         </div>
     @else
