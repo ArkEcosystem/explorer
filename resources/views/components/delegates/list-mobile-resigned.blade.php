dda75c1eb4cedba18e7296b7328d81bd99e81858
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-10-27 23:00:54 +0000

    refactor: tidy table list components

--- resources/views/components/delegates/list-mobile-resigned.blade.php
+++ resources/views/components/delegates/list-mobile-resigned.blade.php
@@ -1,7 +1,7 @@
 <div class="space-y-8 divide-y table-list-mobile">
     @foreach ($delegates as $delegate)
-        <div class="flex flex-col space-y-3 w-full pt-8 {{ $loop->first ? '' : 'border-t'}} border-theme-secondary-300">
-            <div class="flex justify-between w-full">
+        <div class="space-y-3 table-list-mobile-row">
+            <div>
                 @lang('general.delegates.name')
 
                 <div class="flex flex-row items-center space-x-3">
@@ -12,7 +12,7 @@
                 <x-general.address :address="$delegate->username()" />
             </div>
 
-            <div class="flex justify-between w-full">
+            <div>
                 @lang('general.delegates.votes')
 
                 <div wire:loading.class="w-full h-5 rounded-full bg-theme-secondary-300 animate-pulse"></div>
