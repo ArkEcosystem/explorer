82529e41f509559f38e6e0eafd9c53fab4ddd74b
Brian Faust <hello@basecode.sh>
2020-10-28 12:59:05 +0200

    wip

---
+++ app/ViewModels/Concerns/Wallet/HasType.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ViewModels\Concerns\Wallet;
+
+trait HasType
+{
+    public function isKnown(): bool
+    {
+        return ! is_null($this->findWalletByKnown());
+    }
+
+    public function isOwnedByTeam(): bool
+    {
+        if (! $this->isKnown()) {
+            return false;
+        }
+
+        return optional($this->findWalletByKnown())['type'] === 'team';
+    }
+
+    public function isOwnedByExchange(): bool
+    {
+        if (! $this->isKnown()) {
+            return false;
+        }
+
+        return optional($this->findWalletByKnown())['type'] === 'exchange';
+    }
+
+    private function findWalletByKnown(): ?array
+    {
+        return collect(Network::knownWallets())->firstWhere('address', $this->wallet->address);
+    }
+}
