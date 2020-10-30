bef5a2d6d979bc69465e28e64f63800ec4efd2ab
Brian Faust <hello@basecode.sh>
2020-10-30 04:07:07 +0200

    wip

--- resources/views/components/tables/rows/mobile/amount.blade.php
+++ resources/views/components/tables/rows/mobile/amount.blade.php
@@ -3,11 +3,11 @@
 
     <x-general.amount-fiat-tooltip>
         <x-slot name="amount">
-            <x-currency>{{ $model->balance() }}</x-currency>
+            <x-currency>{{ $model->amount() }}</x-currency>
         </x-slot>
 
         <x-slot name="fiat">
-            {{ $model->balanceFiat() }}
+            {{ $model->amountFiat() }}
         </x-slot>
     </x-general.amount-fiat-tooltip>
 </div>
