74b4be49fdaf394889d16670de756f7b4915b2e1
Brian Faust <hello@basecode.sh>
2020-10-30 05:12:08 +0200

    wip

--- tests/Unit/DTO/PaymentTest.php
+++ tests/Unit/DTO/PaymentTest.php
@@ -12,12 +12,12 @@ it('should make an instance that has all properties', function () {
 
     $subject = new Payment(123, [
         'amount'      => 1e8,
-        'recipientId' => Wallet::factory()->create()->address,
+        'recipientId' => Wallet::factory()->create(['address' => 'DGs7JcNRXVAWqKMwceWkVwNWfg5pfth2Pm'])->address,
     ]);
 
     expect($subject->amount())->toBe(1.0);
     expect($subject->amountFiat())->toBe('0 USD');
-    expect($subject->address())->toBe('address');
+    expect($subject->address())->toBe('DGs7JcNRXVAWqKMwceWkVwNWfg5pfth2Pm');
     expect($subject->username())->toBe('username');
     expect($subject->recipient())->toBeInstanceOf(Payment::class);
 });
