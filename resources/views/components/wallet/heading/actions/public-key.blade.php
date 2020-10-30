4f69983be4d7c570d2f5afb1444820569a9ba9af
Highjhacker <jolanbeer@gmail.com>
2020-10-30 14:49:39 +0100

    refactor: requested changes

--- resources/views/components/wallet/heading/actions/public-key.blade.php
+++ resources/views/components/wallet/heading/actions/public-key.blade.php
@@ -1,10 +1,10 @@
-<div x-data="{ publicKeyModalVisible: false }">
+<div x-data="{ publicKeyModalVisible: false }" class="relative">
     <button type="button" @click="publicKeyModalVisible = !publicKeyModalVisible"
-        class="relative flex items-center justify-center flex-1 w-16 px-3 rounded cursor-pointer bg-theme-secondary-800 hover:bg-theme-primary-600 transition-default lg:flex-none h-11">
+        class="flex items-center justify-center flex-1 w-16 px-3 rounded cursor-pointer bg-theme-secondary-800 hover:bg-theme-primary-600 transition-default lg:flex-none h-11">
         @svg('key', 'w-6 h-6')
     </button>
 
-    <div x-show="publicKeyModalVisible" class="absolute right-0 z-10 mt-4 bg-white rounded-lg shadow-lg w-128" x-cloak>
+    <div @click.away="publicKeyModalVisible = false" x-show="publicKeyModalVisible" class="absolute right-0 z-40 mt-4 bg-white rounded-lg shadow-lg w-128" x-cloak>
         <div class="flex flex-col p-8 space-y-2 leading-tight">
             <span class="text-sm font-semibold text-theme-secondary-400">@lang('pages.wallet.public_key.title')</span>
             <span class="flex font-semibold text-theme-secondary-400 dark:text-theme-secondary-200 link">
