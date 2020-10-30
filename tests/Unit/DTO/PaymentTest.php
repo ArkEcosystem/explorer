f594be0d710040656392aecc2a18c1bf6877daa0
Brian Faust <hello@basecode.sh>
2020-10-30 05:13:35 +0200

    wip

--- tests/Unit/DTO/PaymentTest.php
+++ tests/Unit/DTO/PaymentTest.php
@@ -10,14 +10,16 @@ use function Tests\configureExplorerDatabase;
 it('should make an instance that has all properties', function () {
     configureExplorerDatabase();
 
+    $wallet = Wallet::factory()->create();
+
     $subject = new Payment(123, [
         'amount'      => 1e8,
-        'recipientId' => Wallet::factory()->create(['address' => 'DGs7JcNRXVAWqKMwceWkVwNWfg5pfth2Pm'])->address,
+        'recipientId' => $wallet->address,
     ]);
 
     expect($subject->amount())->toBe(1.0);
     expect($subject->amountFiat())->toBe('0 USD');
-    expect($subject->address())->toBe('DGs7JcNRXVAWqKMwceWkVwNWfg5pfth2Pm');
-    expect($subject->username())->toBe('username');
+    expect($subject->address())->toBe($wallet->address);
+    expect($subject->username())->toBe($wallet->attributes['delegate']['username']);
     expect($subject->recipient())->toBeInstanceOf(Payment::class);
 });
