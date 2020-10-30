b15a9c3ac8a636a92ba35321ed2cb2d3b0e40070
Brian Faust <hello@basecode.sh>
2020-10-30 12:51:10 +0200

    wip

--- resources/views/components/wallet/delegate.blade.php
+++ resources/views/components/wallet/delegate.blade.php
@@ -10,7 +10,7 @@
                     @if ($wallet->rank() > Network::delegateCount())
                         {{ $wallet->rank() }}
                     @else
-                        {{ $wallet->rank() }} <span class="text-theme-secondary-400">/{{ Network::delegateCount() }}</span>
+                        {{ $wallet->rank() }} <span class="text-theme-secondary-500">/{{ Network::delegateCount() }}</span>
                     @endif
                 </x-details-box>
 
