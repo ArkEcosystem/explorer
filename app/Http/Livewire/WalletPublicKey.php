d7cc7f2beb9757a8129cfb039b6a3cc28f68dc75
Highjhacker <jolanbeer@gmail.com>
2020-10-30 13:17:03 +0100

    feat: wallet public key modal component

---
+++ app/Http/Livewire/WalletPublicKey.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+final class WalletPublicKey extends Component
+{
+    public bool $isOpen = false;
+
+    public string $publicKey;
+
+    public int $amount = 10;
+
+    /** @phpstan-ignore-next-line */
+    protected $listeners = ['togglePublicKey'];
+
+    public function togglePublicKey(): void
+    {
+        $this->isOpen = !$this->isOpen;
+    }
+}
