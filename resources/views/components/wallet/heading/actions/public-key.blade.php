e71476657c18f2011e3a5b683c16f12d60ee64c7
ItsANameToo <itsanametoo@protonmail.com>
2020-10-30 14:44:44 +0100

    wip

--- resources/views/components/wallet/heading/actions/public-key.blade.php
+++ resources/views/components/wallet/heading/actions/public-key.blade.php
@@ -1,6 +1,6 @@
-<div x-data="{ publicKeyModalVisible: false }">
+<div x-data="{ publicKeyModalVisible: false }" class="relative">
     <button type="button" @click="publicKeyModalVisible = !publicKeyModalVisible"
-        class="relative flex items-center justify-center flex-1 w-16 px-3 rounded cursor-pointer bg-theme-secondary-800 hover:bg-theme-primary-600 transition-default lg:flex-none h-11">
+        class="flex items-center justify-center flex-1 w-16 px-3 rounded cursor-pointer bg-theme-secondary-800 hover:bg-theme-primary-600 transition-default lg:flex-none h-11">
         @svg('key', 'w-6 h-6')
     </button>
 
