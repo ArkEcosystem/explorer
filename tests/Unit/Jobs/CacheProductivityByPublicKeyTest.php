cec011f4b9d210201539f36d7e6a00dbab29d8c9
Brian Faust <hello@basecode.sh>
2020-10-31 04:07:19 +0200

    wip

---
+++ tests/Unit/Jobs/CacheProductivityByPublicKeyTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Jobs\CacheProductivityByPublicKey;
+use App\Models\Block;
+
+use Illuminate\Support\Facades\Cache;
+use function Tests\configureExplorerDatabase;
+
+it('should cache the productivity for the public key', function () {
+    configureExplorerDatabase();
+
+    $block = Block::factory()->create();
+
+    expect(Cache::has('productivity:'.$block->generator_public_key))->toBeFalse();
+
+    (new CacheProductivityByPublicKey($block->generator_public_key))->handle();
+
+    expect(Cache::has('productivity:'.$block->generator_public_key))->toBeTrue();
+    expect(Cache::get('productivity:'.$block->generator_public_key))->toBeFloat();
+});
