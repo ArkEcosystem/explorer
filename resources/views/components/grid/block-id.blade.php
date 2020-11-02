9aea14c00945c095b86388f5aede2352f05f3a20
Highjhacker <jolanbeer@gmail.com>
2020-11-02 11:29:16 +0100

    refactor: replaced contents with inline

--- resources/views/components/grid/block-id.blade.php
+++ resources/views/components/grid/block-id.blade.php
@@ -1,11 +1,11 @@
 <x-grid.generic :title="trans('general.transaction.block_id')" icon="app-block-id" :without-border="$withoutBorder ?? false">
     <span class="flex items-center">
 
-        <a href="{{ route('block', $model->blockId()) }}" class="sm:hidden md:contents lg:hidden font-semibold link">
+        <a href="{{ route('block', $model->blockId()) }}" class="sm:hidden md:inline lg:hidden font-semibold link">
             <x-truncate-middle :value="$model->blockId()" :length="10" />
         </a>
 
-        <a href="{{ route('block', $model->blockId()) }}" class="hidden sm:contents md:hidden lg:contents font-semibold link">
+        <a href="{{ route('block', $model->blockId()) }}" class="hidden sm:inline md:hidden lg:inline font-semibold link">
             <x-truncate-middle :value="$model->blockId()" :length="32" />
         </a>
 
