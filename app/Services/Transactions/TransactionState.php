993ecf2bfdeacf2940d80bc2bfa3ae1bb01407ac
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2020-11-02 11:08:03 -0300

    fix stan

--- app/Services/Transactions/TransactionState.php
+++ app/Services/Transactions/TransactionState.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services\Transactions;
 
 use App\Facades\Network;
+use App\Models\Block;
 use App\Models\Transaction;
 use App\Services\Cache\NetworkCache;
 
@@ -20,6 +21,7 @@ final class TransactionState
     public function isConfirmed(): bool
     {
         try {
+            /** @var Block $block */
             $block = $this->transaction->block;
 
             $confirmations = (new NetworkCache())->getHeight() - $block->height->toNumber();
