63b3cd83725a7a7864c1c8596e4f6cbb50362a17
Highjhacker <Highjhacker@users.noreply.github.com>
2020-11-04 20:47:02 +0000

    style: resolve style guide violations

--- resources/views/components/tables/rows/mobile/round-status-history.blade.php
+++ resources/views/components/tables/rows/mobile/round-status-history.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-row items-center sm:justify-end space-x-3 round-status-history">
+<div class="flex flex-row items-center space-x-3 sm:justify-end round-status-history">
     @foreach($model->performance() as $performed)
         @if($performed)
             <span class="text-theme-success-500">
