47420e56216cd7aa3962e61be57eaefc47ad74fb
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-04 08:02:21 +0200

    Restore app/Contracts/Network.php

---
+++ app/Contracts/Network.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Contracts;
+
+use Carbon\Carbon;
+
+interface Network
+{
+    public function name(): string;
+
+    public function alias(): string;
+
+    public function currency(): string;
+
+    public function currencySymbol(): string;
+
+    public function confirmations(): int;
+
+    public function knownWallets(): array;
+
+    public function canBeExchanged(): bool;
+
+    public function host(): string;
+
+    public function usesMarketsquare(): bool;
+
+    public function epoch(): Carbon;
+
+    public function delegateCount(): int;
+
+    public function blockTime(): int;
+
+    public function blockReward(): int;
+
+    public function config(): \BitWasp\Bitcoin\Network\Network;
+}
