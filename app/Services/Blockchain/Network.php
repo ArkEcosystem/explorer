8b1330a7edcf87b79e6099dd5d173876d48726e6
Brian Faust <hello@basecode.sh>
2020-11-04 07:59:56 +0200

    wip

--- app/Services/Blockchain/Network.php
+++ app/Services/Blockchain/Network.php
@@ -2,85 +2,88 @@
 
 declare(strict_types=1);
 
-namespace App\Services\Blockchain\Networks\ARK;
+namespace App\Services\Blockchain;
 
-use App\Contracts\Network;
 use App\Services\Cache\WalletCache;
-use ArkEcosystem\Crypto\Networks\Devnet;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 
-final class Development implements Network
+final class Network
 {
+    private array $config;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
     public function name(): string
     {
-        return 'ARK Development Network';
+        return $this->config['name'];
     }
 
     public function alias(): string
     {
-        return 'devnet';
+        return $this->config['alias'];
     }
 
     public function currency(): string
     {
-        return 'DARK';
+        return $this->config['currency'];
     }
 
     public function currencySymbol(): string
     {
-        return 'DѦ';
+        return $this->config['currencySymbol'];
     }
 
     public function confirmations(): int
     {
-        return 51;
+        return $this->config['confirmations'];
     }
 
     public function knownWallets(): array
     {
-        return (new WalletCache())->setKnown(
-            fn () => Http::get('https://raw.githubusercontent.com/ArkEcosystem/common/master/devnet/known-wallets-extended.json')->json()
-        );
+        return (new WalletCache())->setKnown(fn () => Http::get($this->config['knownWallets'])->json());
     }
 
     public function canBeExchanged(): bool
     {
-        return false;
+        return $this->config['canBeExchanged'];
     }
 
     public function host(): string
     {
-        return 'https://dwallets.ark.io/api';
+        return $this->config['host'];
     }
 
     public function usesMarketsquare(): bool
     {
-        return false;
+        return $this->config['usesMarketsquare'];
     }
 
     public function epoch(): Carbon
     {
-        return Carbon::parse(Devnet::new()->epoch());
+        return Carbon::parse($this->config['epoch']);
     }
 
     public function delegateCount(): int
     {
-        return 51;
+        return $this->config['delegateCount'];
     }
 
     public function blockTime(): int
     {
-        return 8;
+        return $this->config['blockTime'];
     }
 
     public function blockReward(): int
     {
-        return 2;
+        return $this->config['blockReward'];
     }
 
     public function config(): \BitWasp\Bitcoin\Network\Network
     {
-        return new Devnet();
+        return resolve($this->config['config']);
     }
 }
