58d8a2df9930b8f3676a19bd8db246a269f4eab8
Highjhacker <jolanbeer@gmail.com>
2020-11-04 16:03:41 +0100

    wip

--- resources/views/components/tables/rows/mobile/round-status-history.blade.php
+++ resources/views/components/tables/rows/mobile/round-status-history.blade.php
@@ -1,11 +1,11 @@
-<div class="flex flex-row items-center justify-end space-x-3 pl-14">
+<div class="flex flex-row items-center justify-end space-x-3 pl-14 round-status-history">
     @foreach($model->performance() as $performed)
         @if($performed)
-            <span class="text-theme-success-500 round-status-history">
+            <span class="text-theme-success-500">
                 <x-icon name="app-status-done" size="lg" />
             </span>
         @else
-            <span class="text-theme-danger-500 round-status-history">
+            <span class="text-theme-danger-500">
                 <x-icon name="app-status-undone" size="lg" />
             </span>
         @endif
