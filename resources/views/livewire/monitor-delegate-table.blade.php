583d938ce61ed76cd4fe489220f73fa41416c05b
Brian Faust <hello@basecode.sh>
2020-11-04 14:44:50 +0200

    wip

--- resources/views/livewire/monitor-delegate-table.blade.php
+++ resources/views/livewire/monitor-delegate-table.blade.php
@@ -1,8 +1,8 @@
 <div id="delegate-list" class="w-full">
     <x-loading.visible>
-        <span x-show="status === 'resigned'">
-            <x-tables.desktop.skeleton.monitor.resigned />
-            <x-tables.mobile.skeleton.monitor.resigned />
+        <span x-show="status === 'active'">
+            <x-tables.desktop.skeleton.monitor.active />
+            <x-tables.mobile.skeleton.monitor.active />
         </span>
 
         <span x-show="status === 'standby'">
@@ -10,32 +10,32 @@
             <x-tables.mobile.skeleton.monitor.standby />
         </span>
 
-        <span x-show="status === 'active'">
-            <x-tables.desktop.skeleton.monitor.active />
-            <x-tables.mobile.skeleton.monitor.active />
+        <span x-show="status === 'resigned'">
+            <x-tables.desktop.skeleton.monitor.resigned />
+            <x-tables.mobile.skeleton.monitor.resigned />
         </span>
     </x-loading.visible>
 
-    <span x-show="status === 'resigned'">
-        <x-loading.hidden>
-            <x-tables.desktop.monitor.resigned :delegates="$delegates" />
-            <x-tables.mobile.monitor.resigned :delegates="$delegates" />
-        </x-loading.hidden>
-    </span>
-
-    <span x-show="status === 'standby'">
-        <x-loading.hidden>
-            <x-tables.desktop.monitor.standby :delegates="$delegates" />
-            <x-tables.mobile.monitor.standby :delegates="$delegates" />
-        </x-loading.hidden>
-    </span>
-
-    <span x-show="status === 'active'">
+    @if($this->state['status'] === 'active')
         <x-loading.hidden>
             <x-tables.desktop.monitor.active :delegates="$delegates" />
             <x-tables.mobile.monitor.active :delegates="$delegates" />
         </x-loading.hidden>
-    </span>
+    @endif
+
+    @if($this->state['status'] === 'standby')
+        <x-loading.hidden>
+            <x-tables.desktop.monitor.standby :delegates="$delegates" />
+            <x-tables.mobile.monitor.standby :delegates="$delegates" />
+        </x-loading.hidden>
+    @endif
+
+    @if($this->state['status'] === 'resigned')
+        <x-loading.hidden>
+            <x-tables.desktop.monitor.resigned :delegates="$delegates" />
+            <x-tables.mobile.monitor.resigned :delegates="$delegates" />
+        </x-loading.hidden>
+    @endif
 
     <script>
         window.addEventListener('livewire:load', () => window.livewire.on('pageChanged', () => scrollToQuery('#transaction-list')));
