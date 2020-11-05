e535cbfb9be595c47bef06d4d478b0197b458dbd
Brian Faust <hello@basecode.sh>
2020-11-05 15:51:53 +0200

    wip

--- tests/Unit/Services/NumberFormatterTest.php
+++ tests/Unit/Services/NumberFormatterTest.php
@@ -24,6 +24,7 @@ it('should format a number with a currency symbol', function () {
 
 it('should format a number with a currency symbol if it has a [.]', function () {
     assertMatchesSnapshot(NumberFormatter::currency(123.456, 'ARK'));
+    assertMatchesSnapshot(NumberFormatter::currency(0.000003, 'ARK'));
 });
 
 it('should format a number with a currency symbol if it has a [,]', function () {
