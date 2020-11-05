488e5d27cb507b587fc45cc7a8053b396c8d78c5
Highjhacker <jolanbeer@gmail.com>
2020-11-05 21:33:12 +0100

    refactor: responsive version

--- resources/views/components/tables/mobile/delegates/monitor.blade.php
+++ resources/views/components/tables/mobile/delegates/monitor.blade.php
@@ -1,15 +1,42 @@
-<div class="space-y-8 divide-y table-list-mobile">
-    @foreach ($delegates as $delegate)
-        <div class="space-y-3 table-list-mobile-row">
-            <x-tables.rows.mobile.slot-id :model="$delegate" />
+<div class="w-full table-container md:hidden">
+    <table>
+        <thead>
+            <tr>
+                <x-tables.headers.mobile.number name="pages.delegates.order" alignment="text-left" />
+                <x-tables.headers.mobile.address name="pages.delegates.name" />
+                <x-tables.headers.mobile.text name="pages.delegates.forging_at" alignment="text-center" responsive/>
+                <x-tables.headers.mobile.status name="pages.delegates.status" alignment="text-left" />
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($delegates as $delegate)
+                <tr
+                    wire:key="$delegate->publicKey()"
+                    @if ($delegate->keepsMissing())
+                        class="bg-theme-danger-50"
+                    @elseif ($delegate->justMissed())
+                        class="bg-theme-warning-50"
+                    @endif
+                >
+                    <td>
+                        <x-tables.rows.mobile.slot-id :model="$delegate" />
+                    </td>
 
-            <x-tables.rows.mobile.username-with-avatar :model="$delegate" />
+                    {{-- Have to suffix it with a -2 to avoid issues with the other same wire:key for the desktop component --}}
+                    <td wire:key="{{ $delegate->publicKey() }}-username-2">
+                        <x-tables.rows.mobile.username-with-avatar :model="$delegate->wallet()" />
+                    </td>
 
-            <x-tables.rows.mobile.slot-time :model="$delegate" />
+                    <td class="hidden sm:table-cell">
+                        <x-tables.rows.mobile.slot-time :model="$delegate" />
+                    </td>
 
-            <x-tables.rows.mobile.round-status :model="$delegate" />
-
-            <x-tables.rows.mobile.wallet-last-block :model="$delegate" />
-        </div>
-    @endforeach
+                    {{-- Have to suffix it with a -2 to avoid issues with the other same wire:key for the desktop component --}}
+                    <td wire:key="{{ $delegate->publicKey() }}-round-status-{{ $delegate->status() }}-2">
+                        <x-tables.rows.mobile.round-status :model="$delegate" />
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
