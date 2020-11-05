78a21194c9e1fab499929df9b1e434bc348f2f1f
Brian Faust <hello@basecode.sh>
2020-11-05 09:57:47 +0200

    wip

--- resources/views/components/page-headers/transaction/icon-type.blade.php
+++ resources/views/components/page-headers/transaction/icon-type.blade.php
@@ -1,5 +1,7 @@
-<x-general.entity-header-item
-    :title="trans('pages.transaction.transaction_type')"
-    icon="app-transactions.{{ $model->iconType() }}"
-    :text="$model->typeLabel()"
-/>
+<x-general.entity-header-item :title="trans('pages.transaction.transaction_type')" icon="app-transactions.{{ $model->iconType() }}">
+    @isset($asEntity)
+        @lang('pages.transaction.'.$transaction->entityType())
+    @else
+        {{ $model->typeLabel() }}
+    @endisset
+</x-general.entity-header-item>
