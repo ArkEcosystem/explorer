c0c864ebe57f26079fa272377a8cd2db22d626a4
Brian Faust <hello@basecode.sh>
2020-11-05 13:34:57 +0200

    wip

--- resources/views/components/date-picker.blade.php
+++ resources/views/components/date-picker.blade.php
@@ -4,6 +4,7 @@
     x-init="new Pikaday({
         field: $refs.input,
         format: 'DD/MM/YYYY',
+        maxDate: new Date(),
         toString(date, format) {
             return date.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
         },
