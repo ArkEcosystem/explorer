30f2b2526d9a30e82a2bbfa37940ff61ff613df2
Brian Faust <hello@basecode.sh>
2020-11-05 09:59:30 +0200

    wip

--- resources/views/components/page-headers/transaction/icon-type.blade.php
+++ resources/views/components/page-headers/transaction/icon-type.blade.php
@@ -1,7 +1,7 @@
 <x-general.entity-header-item :title="trans('pages.transaction.transaction_type')" icon="app-transactions.{{ $model->iconType() }}">
     <x-slot name="text">
         @isset($asEntity)
-            @lang('pages.transaction.'.$transaction->entityType())
+            @lang('pages.transaction.'.$model->entityType())
         @else
             {{ $model->typeLabel() }}
         @endisset
