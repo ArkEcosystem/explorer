4ae90fff40fd67c3fb241e4a4d29a0c1beacafeb
Brian Faust <hello@basecode.sh>
2020-11-05 05:38:07 +0200

    wip

--- tests/Unit/Services/Blockchain/CustomNetworkTest.php
+++ tests/Unit/Services/Blockchain/CustomNetworkTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use App\Services\Blockchain\CustomNetwork;
 
-it('should handle ARK Production', function () {
+it('should have an address prefix and epoch', function () {
     $subject = new CustomNetwork(['base58Prefix' => 23, 'epoch' => '2017-03-21T13:00:00.000Z']);
 
     expect($subject->pubKeyHash())->toBe(17);
