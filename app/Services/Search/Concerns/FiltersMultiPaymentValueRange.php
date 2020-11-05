d57e4ce595a066306df7ef5ae9feffd8c37a0a0d
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2020-11-05 20:22:55 -0300

    fix stan

--- app/Services/Search/Concerns/FiltersMultiPaymentValueRange.php
+++ app/Services/Search/Concerns/FiltersMultiPaymentValueRange.php
@@ -5,19 +5,18 @@ declare(strict_types=1);
 namespace App\Services\Search\Concerns;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
 
 trait FiltersMultiPaymentValueRange
 {
     /**
-     * @param Builder         $query
-     * @param string|int|null $from
-     * @param string|int|null $to
-     * @param bool            $useSatoshi
+     * @param Builder  $query
+     * @param int|null $from
+     * @param int|null $to
+     * @param bool     $useSatoshi
      *
      * @return Builder
      */
-    private function queryMultiPaymentValueRange(Builder $query, $from, $to, bool $useSatoshi = true): Builder
+    private function queryMultiPaymentValueRange(Builder $query, ?int $from, ?int $to, bool $useSatoshi = true): Builder
     {
         if (is_null($from) && is_null($to)) {
             return $query;
@@ -25,19 +24,19 @@ trait FiltersMultiPaymentValueRange
 
         $query->where('amount', '=', 0);
 
-        $query->whereExists(function (\Illuminate\Database\Query\Builder $qq) use ($useSatoshi, $to, $from) {
+        $query->whereExists(function (\Illuminate\Database\Query\Builder $qq) use ($useSatoshi, $to, $from): void {
             $qq->selectRaw('i.id')
-               ->fromRaw(DB::raw("( SELECT id, (jsonb_array_elements(asset -> 'payments') ->> 'amount')::bigint am FROM transactions t WHERE t.id = id ) i"))
+               ->fromRaw("( SELECT id, (jsonb_array_elements(asset -> 'payments') ->> 'amount')::bigint am FROM transactions t WHERE t.id = id ) i")
                ->whereRaw('i.id = transactions.id')
                ->groupBy('i.id')
-               ->when(! is_null($from) && $from > 0, function ($q) use ($useSatoshi, $from) {
+               ->when(! is_null($from) && $from > 0, function ($q) use ($useSatoshi, $from): void {
                    $q->havingRaw('sum(am) >= ?', [
-                       $from * ($useSatoshi ? 1e8 : 1),
+                       (int) $from * ($useSatoshi ? 1e8 : 1),
                    ]);
                })
-               ->when(! is_null($to) && $to > 0, function ($q) use ($useSatoshi, $to) {
+               ->when(! is_null($to) && $to > 0, function ($q) use ($useSatoshi, $to): void {
                    $q->havingRaw('sum(am) <= ?', [
-                       $to * ($useSatoshi ? 1e8 : 1),
+                       (int) $to * ($useSatoshi ? 1e8 : 1),
                    ]);
                });
         });
