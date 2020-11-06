6778198d56828b822381b63f37c6ec757ae0788f
ItsANameToo <itsanametoo@protonmail.com>
2020-11-06 10:49:00 +0100

    wip

--- resources/views/components/tables/desktop/transactions.blade.php
+++ resources/views/components/tables/desktop/transactions.blade.php
@@ -4,7 +4,11 @@
             <tr>
                 <x-tables.headers.desktop.text name="general.transaction.id" />
                 <x-tables.headers.desktop.text name="general.transaction.timestamp" responsive />
-                <x-tables.headers.desktop.address name="general.transaction.sender" icon />
+                @isset($useDirection)
+                    <x-tables.headers.desktop.address name="general.transaction.sender" icon use-direction />
+                @else
+                    <x-tables.headers.desktop.address name="general.transaction.sender" icon />
+                @endif
                 <x-tables.headers.desktop.address name="general.transaction.recipient" />
                 <x-tables.headers.desktop.number name="general.transaction.amount" />
                 <x-tables.headers.desktop.number name="general.transaction.fee" responsive breakpoint="xl" />
--- resources/views/components/tables/headers/desktop/address.blade.php
+++ resources/views/components/tables/headers/desktop/address.blade.php
@@ -1,1 +1,1 @@
-<th><span @isset($icon) class="relative pl-24 left-2" @else class="pl-14" @endif>@lang($name)</span></th>
+<th><span @isset($useDirection) class="pl-26" @else class="pl-14" @endif>@lang($name)</span></th>
