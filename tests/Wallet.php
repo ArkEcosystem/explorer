4c46bc4b3a4a71f10cf2eaa364ec3bc478ed6481
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2020-11-09 15:00:16 -0300

    Faker not to repeat wallets during the span of a single test

--- tests/Wallet.php
+++ tests/Wallet.php
@@ -6,10 +6,12 @@ namespace Tests;
 
 use Faker\Generator;
 use Faker\Provider\Base;
+use Illuminate\Support\Collection;
 
 final class Wallet extends Base
 {
     private static $wallets;
+    private Collection $availableWallets;
 
     public function __construct(Generator $generator)
     {
@@ -18,10 +20,11 @@ final class Wallet extends Base
         if (self::$wallets === null) {
             self::$wallets = json_decode(file_get_contents(base_path('tests/fixtures/wallets.json')), true);
         }
+        $this->availableWallets = collect(self::$wallets)->shuffle();
     }
 
     public function wallet()
     {
-        return static::randomElement(self::$wallets);
+        return $this->availableWallets->shift();
     }
 }
