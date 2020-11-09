c5af1dda473b9d4cfadc45da2227b77f23582c2b
Highjhacker <jolanbeer@gmail.com>
2020-11-09 14:28:22 +0100

    refactor: requested changes

--- resources/views/components/grid/ipfs-hash.blade.php
+++ resources/views/components/grid/ipfs-hash.blade.php
@@ -1,15 +1,15 @@
 <x-grid.generic :title="trans('general.transaction.ipfs-hash')" icon="app-transactions.ipfs">
     <span class="flex items-center">
-        <a href="https://cloudflare-ipfs.com/ipfs/{{ $model->ipfsHash() }}" class="font-semibold sm:hidden md:inline lg:hidden link">
-            <x-truncate-middle :value="$model->ipfsHash()" :length="10" />
-        </a>
+        <x-ark-external-link url="https://cloudflare-ipfs.com/ipfs/{{ $model->ipfsHash() }}">
+            <x-slot name="text">
+                <span class="sm:hidden md:inline lg:hidden">
+                    <x-truncate-middle :value="$model->ipfsHash()" :length="10" />
+                </span>
 
-        <a href="https://cloudflare-ipfs.com/ipfs/{{ $model->ipfsHash() }}" class="hidden font-semibold sm:inline md:hidden lg:inline link">
-            <x-truncate-middle :value="$model->ipfsHash()" :length="30" />
-        </a>
-
-        <a href="https://cloudflare-ipfs.com/ipfs/{{ $model->ipfsHash() }}" class="ml-2 link">
-            @svg('link', 'h-4 w-4 link')
-        </a>
+                <span class="hidden sm:inline md:hidden lg:inline">
+                    <x-truncate-middle :value="$model->ipfsHash()" :length="30" />
+                </span>
+            </x-slot>
+        </x-ark-external-link>
     </span>
 </x-grid.generic>
