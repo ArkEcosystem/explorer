4f294e43e611b28ba2f29b1f5cd582cf0d13ff1f
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-09 14:19:40 +0000

    fix: close on click away

--- resources/views/livewire/wallet-qr-code.blade.php
+++ resources/views/livewire/wallet-qr-code.blade.php
@@ -1,6 +1,6 @@
 <div x-data="{ visible: {{ $isOpen ? 'true' : 'false' }}, options: false }" x-cloak>
     <div x-show="visible">
-        <x-ark-modal class="w-full" width-class="max-w-md" wire-close="$emit('toggleQrCode')" alpine-close="visible = false" title-class="header-2">
+        <x-ark-modal width-class="max-w-md" wire-close="$emit('toggleQrCode')" alpine-close="visible = false" title-class="header-2">
             @slot('title')
                 @lang('pages.wallet.qrcode.title')
             @endslot
