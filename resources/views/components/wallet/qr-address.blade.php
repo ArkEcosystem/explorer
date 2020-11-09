a6680280b0aeb7e00bbe62bfb1f0b7e414d19bbc
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-09 13:16:09 +0000

    feat: qr modal address component

---
+++ resources/views/components/wallet/qr-address.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <div class="flex {{ ($withoutReverse ?? false) ? 'space-x-3' : 'flex-row-reverse' }} items-center sm:justify-start">
+        <div class="hidden sm:block">
+            <x-general.avatar :identifier="$model->address()" no-shrink />
+        </div>
+
+        <div class="flex flex-col flex-1 justify-center mr-4 sm:mr-0">
+            <div class="font-semibold text-theme-secondary-500 text-sm">@lang('general.address')</div>
+
+            <div class="flex items-center justify-between sm:justify-start">
+                <div>
+                    <div class="hidden font-semibold text-theme-secondary-900 sm:flex">
+                        <x-truncate-middle :value="$model->address()" :length="19" />
+                    </div>
+
+                    <div class="font-semibold text-theme-secondary-900 sm:hidden">
+                        <x-truncate-middle :value="$model->address()" :length="15" />
+                    </div>
+                </div>
+
+                <x-clipboard :value="$model->address()" />
+            </div>
+        </div>
+    </div>
+</div>
