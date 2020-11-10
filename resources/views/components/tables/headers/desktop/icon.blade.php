c1414e828195d792c45997f25765f163a0ac8d88
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-10 12:38:03 +0000

    refactor: remove commented code

--- resources/views/components/tables/headers/desktop/icon.blade.php
+++ resources/views/components/tables/headers/desktop/icon.blade.php
@@ -1,5 +1,1 @@
-{{-- @isset($responsive)
-    <th class="hidden {{ $alignment ?? 'text-right' }} {{ $breakpoint ?? 'lg' }}:table-cell">@lang($name)</th>
-@else --}}
-    <th class="{{ $alignment ?? 'text-center' }}">@lang($name)</th>
-{{-- @endisset --}}
+<th class="{{ $alignment ?? 'text-center' }}">@lang($name)</th>
