028ae0494e4345e45329e7a08dc834c5039e472b
Brian Faust <hello@basecode.sh>
2020-11-10 17:44:38 +0200

    wip

---
+++ app/Actions/CacheNetworkHeight.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions;
+
+use App\Models\Block;
+use App\Models\Scopes\OrderByHeightScope;
+use App\Services\Cache\NetworkCache;
+
+final class CacheNetworkHeight
+{
+    public static function execute(): void
+    {
+        $block = Block::withScope(OrderByHeightScope::class)->first();
+
+        if (is_null($block)) {
+            $height = 0;
+        } else {
+            $height = $block->height->toNumber();
+        }
+
+        (new NetworkCache())->setHeight($height);
+    }
+}
