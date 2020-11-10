6ae5c7d10042c8412dd194280c4d2f2b3bdc86d9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-10 12:37:13 +0000

    feat: table icon column component

---
+++ resources/views/components/tables/headers/desktop/icon.blade.php
@@ -0,0 +1,5 @@
+{{-- @isset($responsive)
+    <th class="hidden {{ $alignment ?? 'text-right' }} {{ $breakpoint ?? 'lg' }}:table-cell">@lang($name)</th>
+@else --}}
+    <th class="{{ $alignment ?? 'text-center' }}">@lang($name)</th>
+{{-- @endisset --}}
