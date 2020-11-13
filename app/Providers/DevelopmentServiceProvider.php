d9185ca358f92c1c44f6df8e1bf7ebb4b32a92a8
marianogoldman <marianogoldman@users.noreply.github.com>
2020-11-13 12:10:17 +0000

    style: resolve style guide violations

--- app/Providers/DevelopmentServiceProvider.php
+++ app/Providers/DevelopmentServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
 use Faker\Factory;
@@ -8,7 +10,7 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Tests\Wallet;
 
-class DevelopmentServiceProvider extends ServiceProvider
+final class DevelopmentServiceProvider extends ServiceProvider
 {
     /**
      * Register services.
