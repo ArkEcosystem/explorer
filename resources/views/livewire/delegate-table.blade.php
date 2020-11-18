348dbba0705e89ab1bdcbfcf4b0f37e03bcf27a8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-11-18 17:25:04 +0000

    fix: remove unnecesary  from poll

--- resources/views/livewire/delegate-table.blade.php
+++ resources/views/livewire/delegate-table.blade.php
@@ -19,7 +19,7 @@
     </span>
 
     @if($this->state['status'] === 'active')
-        <div wire:poll.{{ Network::blockTime() }}s="$refresh" wire:key="poll_active_delegates_skeleton">
+        <div wire:poll.{{ Network::blockTime() }}s wire:key="poll_active_delegates_skeleton">
             @if (count($delegates) && $state['status'] === 'active')
                 <x-tables.desktop.delegates.active :delegates="$delegates" />
                 <x-tables.mobile.delegates.active :delegates="$delegates" />
