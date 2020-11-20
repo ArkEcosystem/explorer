38d087bb4d41c82c9fad754e39b251f892b045b9
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-11-20 14:31:59 -0600

    add dynamic truncate to address

--- resources/views/components/general/identity-iconless.blade.php
+++ resources/views/components/general/identity-iconless.blade.php
@@ -1,17 +1,17 @@
 <div class="flex items-center justify-between space-x-3">
-    <div class="flex items-center">
+    <div class="flex items-center max-w-full">
         @if ($prefix ?? false)
             {{ $prefix }}
         @endif
 
-        <a href="{{ route('wallet', $model->address()) }}" class="font-semibold link">
+        <a href="{{ route('wallet', $model->address()) }}" class="max-w-full font-semibold link">
             @if ($model->username())
                 {{ $model->username() }}
             @else
                 @isset($withoutTruncate)
                     {{ $model->address() }}
                 @else
-                    <x-truncate-middle :value="$model->address()" length="{{ $length ?? 8 }}"/>
+                    <x-truncate>{{ $model->address() }}</x-truncate>
                 @endisset
             @endif
         </a>
--- resources/views/components/grid/block-id.blade.php
+++ resources/views/components/grid/block-id.blade.php
@@ -1,7 +1,7 @@
 <x-grid.generic :title="trans('general.transaction.block_id')" icon="app-block-id">
     <span class="flex items-center flex-1 min-w-0">
 
-        <a href="{{ route('block', $model->blockId()) }}" class="w-full max-w-full min-w-0 font-semibold link">
+        <a href="{{ route('block', $model->blockId()) }}" class="max-w-full min-w-0 font-semibold link">
             <x-truncate>{{ $model->blockId() }}</x-truncate>
         </a>
 
