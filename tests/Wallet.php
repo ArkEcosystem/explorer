40b12cbc74eda0a6e6479c2b531feda5b4d100c5
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-11-20 15:55:13 -0600

    Create more realistic delegate usernames and public keys

--- tests/Wallet.php
+++ tests/Wallet.php
@@ -28,4 +28,16 @@ final class Wallet extends Base
     {
         return $this->availableWallets->shift();
     }
+
+    public function publicKey($length = 34): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
 }
