a3a6e49eb7cd20b6cdc8832722671d2321e109fa
Highjhacker <jolanbeer@gmail.com>
2020-12-02 12:57:40 +0100

    refactor: compact for latest records

--- resources/views/components/tables/desktop/blocks.blade.php
+++ resources/views/components/tables/desktop/blocks.blade.php
@@ -1,16 +1,24 @@
-<div class="hidden table-container md:block">
+<div class="hidden table-container @if(isset($compact))table-compact @endif md:block">
     <table>
         <thead>
             <tr>
-                <x-tables.headers.desktop.id name="general.block.id" />
-                <x-tables.headers.desktop.text name="general.block.timestamp" responsive />
+                <x-tables.headers.desktop.id name="general.block.id" responsive/>
+                @if (!isset($compact))
+                    <x-tables.headers.desktop.text name="general.block.timestamp" responsive />
+                @else
+                    <x-tables.headers.desktop.text name="general.block.timestamp" />
+                @endif
                 @if(!isset($withoutGenerator))
                     <x-tables.headers.desktop.address name="general.block.generated_by" />
                 @endif
                 <x-tables.headers.desktop.number name="general.block.height" />
                 <x-tables.headers.desktop.number name="general.block.transactions" />
                 <x-tables.headers.desktop.number name="general.block.amount" />
-                <x-tables.headers.desktop.number name="general.block.fee" responsive />
+                @if (!isset($compact))
+                    <x-tables.headers.desktop.number name="general.block.fee" responsive />
+                @else
+                    <x-tables.headers.desktop.number name="general.block.fee" />
+                @endif
             </tr>
         </thead>
         <tbody>
@@ -19,29 +27,47 @@
                     <x-ark-tables.cell wire:key="{{ $block->id() }}-id">
                         <x-tables.rows.desktop.block-id :model="$block" />
                     </x-ark-tables.cell>
-                    <x-ark-tables.cell responsive>
-                        <x-tables.rows.desktop.timestamp :model="$block" />
-                    </x-ark-tables.cell>
-                    @if(!isset($withoutGenerator))
-                        <x-ark-tables.cell wire:key="{{ $block->id() }}-forger">
-                            <x-tables.rows.desktop.block-forger :model="$block" />
+
+                    @if (!isset($compact))
+                        <x-ark-tables.cell responsive>
+                            <x-tables.rows.desktop.timestamp :model="$block" />
+                        </x-ark-tables.cell>
+                    @else
+                        <x-ark-tables.cell>
+                            <x-tables.rows.desktop.timestamp :model="$block" />
                         </x-ark-tables.cell>
                     @endif
+
+                    @if(!isset($withoutGenerator))
+                        <x-ark-tables.cell wire:key="{{ $block->id() }}-forger">
+                            <x-tables.rows.desktop.block-forger :model="$block" compact/>
+                        </x-ark-tables.cell>
+                    @endif
+
                     <x-ark-tables.cell class="text-right">
                         <x-tables.rows.desktop.block-height :model="$block" />
                     </x-ark-tables.cell>
+
                     <x-ark-tables.cell class="text-right">
                         <x-tables.rows.desktop.transaction-count :model="$block" />
                     </x-ark-tables.cell>
+
                     <x-ark-tables.cell
                         class="text-right"
                         last-on="lg"
                     >
                         <x-tables.rows.desktop.amount :model="$block" />
                     </x-ark-tables.cell>
-                    <x-ark-tables.cell class="text-right" responsive>
-                        <x-tables.rows.desktop.fee :model="$block" />
-                    </x-ark-tables.cell>
+
+                    @if (!isset($compact))
+                        <x-ark-tables.cell class="text-right" responsive>
+                            <x-tables.rows.desktop.fee :model="$block" />
+                        </x-ark-tables.cell>
+                    @else
+                        <x-ark-tables.cell class="text-right">
+                            <x-tables.rows.desktop.fee :model="$block" />
+                        </x-ark-tables.cell>
+                    @endif
                 </x-ark-tables.row>
             @endforeach
         </tbody>
--- resources/views/components/tables/desktop/transactions.blade.php
+++ resources/views/components/tables/desktop/transactions.blade.php
@@ -1,19 +1,37 @@
-<div class="hidden table-container md:block">
+<div class="hidden table-container @if(isset($compact))table-compact @endif md:block">
     <table>
         <thead>
             <tr>
                 <x-tables.headers.desktop.id name="general.transaction.id" />
-                <x-tables.headers.desktop.text name="general.transaction.timestamp" responsive />
+
+                @if (!isset($compact))
+                    <x-tables.headers.desktop.text name="general.transaction.timestamp" responsive />
+                @else
+                    <x-tables.headers.desktop.text name="general.transaction.timestamp" />
+                @endif
+
                 @isset($useDirection)
                     <x-tables.headers.desktop.address name="general.transaction.sender" icon use-direction />
                 @else
                     <x-tables.headers.desktop.address name="general.transaction.sender" icon />
                 @endif
+
                 <x-tables.headers.desktop.address name="general.transaction.recipient" />
+
                 <x-tables.headers.desktop.number name="general.transaction.amount" />
-                <x-tables.headers.desktop.number name="general.transaction.fee" responsive breakpoint="xl" />
+
+                @if (!isset($compact))
+                    <x-tables.headers.desktop.number name="general.transaction.fee" responsive breakpoint="xl" />
+                @else
+                    <x-tables.headers.desktop.number name="general.transaction.fee" />
+                @endif
+
                 @isset($useConfirmations)
-                    <x-tables.headers.desktop.number name="general.transaction.confirmations" responsive breakpoint="xl" />
+                    @if(!isset($compact))
+                        <x-tables.headers.desktop.number name="general.transaction.confirmations" responsive breakpoint="xl" />
+                    @else
+                        <x-tables.headers.desktop.number name="general.transaction.confirmations" />
+                    @endif
                 @endisset
             </tr>
         </thead>
@@ -23,18 +41,29 @@
                     <x-ark-tables.cell wire:key="{{ $transaction->id() }}-id">
                         <x-tables.rows.desktop.transaction-id :model="$transaction" />
                     </x-ark-tables.cell>
-                    <x-ark-tables.cell responsive>
-                        <x-tables.rows.desktop.timestamp :model="$transaction" shortened />
-                    </x-ark-tables.cell>
-                    <x-ark-tables.cell wire:key="{{ $transaction->id() }}-sender">                        @isset($useDirection)
+
+                    @if(!isset($compact))
+                        <x-ark-tables.cell responsive>
+                            <x-tables.rows.desktop.timestamp :model="$transaction" shortened />
+                        </x-ark-tables.cell>
+                    @else
+                        <x-ark-tables.cell>
+                            <x-tables.rows.desktop.timestamp :model="$transaction" shortened />
+                        </x-ark-tables.cell>
+                    @endif
+
+                    <x-ark-tables.cell wire:key="{{ $transaction->id() }}-sender">
+                        @isset($useDirection)
                             <x-tables.rows.desktop.sender-with-direction :model="$transaction" :wallet="$wallet" />
                         @else
                             <x-tables.rows.desktop.sender :model="$transaction" />
                         @endif
                     </x-ark-tables.cell>
+
                     <x-ark-tables.cell wire:key="{{ $transaction->id() }}-recipient">
-                        <x-tables.rows.desktop.recipient :model="$transaction" />
+                        <x-tables.rows.desktop.recipient :model="$transaction" :compact="$compact" />
                     </x-ark-tables.cell>
+
                     <x-ark-tables.cell
                         class="text-right"
                         last-on="xl"
@@ -49,22 +78,41 @@
                             <x-tables.rows.desktop.amount :model="$transaction" />
                         @endisset
                     </x-ark-tables.cell>
-                    <x-ark-tables.cell
-                        class="text-right"
-                        responsive
-                        breakpoint="xl"
-                    >
-                        <x-tables.rows.desktop.fee :model="$transaction" />
-                    </x-ark-tables.cell>
+
+                    @if(!isset($compact))
+                        <x-ark-tables.cell
+                            class="text-right"
+                            responsive
+                            breakpoint="xl"
+                        >
+                            <x-tables.rows.desktop.fee :model="$transaction" />
+                        </x-ark-tables.cell>
+                    @else
+                        <x-ark-tables.cell
+                            class="text-right"
+                        >
+                            <x-tables.rows.desktop.fee :model="$transaction" />
+                        </x-ark-tables.cell>
+                    @endif
+
                     @isset($useConfirmations)
-                    <x-ark-tables.cell
-                        class="text-right"
-                        responsive
-                        breakpoint="xl"
-                        wire:key="{{ $transaction->id() }}-confirmations"
-                    >
-                        <x-tables.rows.desktop.confirmations :model="$transaction" />
-                    </x-ark-tables.cell>
+                        @if(!isset($compact))
+                            <x-ark-tables.cell
+                                class="text-right"
+                                responsive
+                                breakpoint="xl"
+                                wire:key="{{ $transaction->id() }}-confirmations"
+                            >
+                                <x-tables.rows.desktop.confirmations :model="$transaction" />
+                            </x-ark-tables.cell>
+                        @else
+                            <x-ark-tables.cell
+                                class="text-right"
+                                wire:key="{{ $transaction->id() }}-confirmations"
+                            >
+                                <x-tables.rows.desktop.confirmations :model="$transaction" />
+                            </x-ark-tables.cell>
+                        @endif
                     @endisset
                 </x-ark-tables.row>
             @endforeach
