5cfca9771f6893790445d40b679b866c9690c59f
ItsANameToo <ItsANameToo@users.noreply.github.com>
2020-12-04 11:22:31 +0000

    style: resolve style guide violations

--- resources/views/livewire/wallet-qr-code.blade.php
+++ resources/views/livewire/wallet-qr-code.blade.php
@@ -76,7 +76,7 @@
     <button
         wire:click="toggleQrCode"
         type="button"
-        class="flex items-center justify-center w-full flex-1 lg:w-16 rounded cursor-pointer bg-theme-primary-600 hover:bg-theme-primary-700 transition-default h-11 lg:flex-none lg:px-3"
+        class="flex items-center justify-center flex-1 w-full rounded cursor-pointer lg:w-16 bg-theme-primary-600 hover:bg-theme-primary-700 transition-default h-11 lg:flex-none lg:px-3"
     >
         <x-ark-icon name="app-qr-code" size="md" />
     </button>
