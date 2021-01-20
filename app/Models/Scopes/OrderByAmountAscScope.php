081792db6a2603645f8573d5388c1d2d31a20ccd
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-01-20 11:08:25 -0300

    Order by amount (correct but slow)

--- app/Models/Scopes/OrderByAmountAscScope.php
+++ app/Models/Scopes/OrderByAmountAscScope.php
@@ -7,11 +7,18 @@ namespace App\Models\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\DB;
 
 final class OrderByAmountAscScope implements Scope
 {
     public function apply(Builder $builder, Model $model)
     {
-        $builder->orderBy('amount', 'asc');
+        $builder->fromRaw('transactions t');
+        $builder->selectRaw(DB::raw("t.*, d.list, COALESCE(d.list, t.amount) AS calc_amount"));
+        $builder->crossJoin(DB::raw("LATERAL (
+    SELECT sum((d->>'amount')::bigint) AS list
+    FROM jsonb_array_elements(t.asset -> 'payments') AS d(elem)) d"));
+
+        $builder->orderByRaw('COALESCE(d.list, t.amount) asc');
     }
 }
