32251c6bc7cf1bb955ee1e1b1c27a2a83bb8f987
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-01-20 11:38:25 -0300

    DRY

---
+++ app/Models/Concerns/QueryWithCalculatedAmount.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Concerns;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
+
+trait QueryWithCalculatedAmount
+{
+    public function baseCalculatedAmountQuery(Builder $builder, $direction = 'asc'): void
+    {
+        $builder->fromRaw('transactions t');
+        $builder->selectRaw(DB::raw('t.*, d.list, COALESCE(d.list, t.amount) AS calc_amount'));
+        $builder->crossJoin(DB::raw("LATERAL (
+            SELECT sum((d->>'amount')::bigint) AS list
+            FROM jsonb_array_elements(t.asset -> 'payments') AS d(elem)) d")
+        );
+
+        $builder->orderByRaw("COALESCE(d.list, t.amount) {$direction}");
+    }
+}
--- app/Models/Scopes/OrderByAmountAscScope.php
+++ app/Models/Scopes/OrderByAmountAscScope.php
@@ -4,21 +4,17 @@ declare(strict_types=1);
 
 namespace App\Models\Scopes;
 
+use App\Models\Concerns\QueryWithCalculatedAmount;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
-use Illuminate\Support\Facades\DB;
 
 final class OrderByAmountAscScope implements Scope
 {
+    use QueryWithCalculatedAmount;
+
     public function apply(Builder $builder, Model $model)
     {
-        $builder->fromRaw('transactions t');
-        $builder->selectRaw(DB::raw('t.*, d.list, COALESCE(d.list, t.amount) AS calc_amount'));
-        $builder->crossJoin(DB::raw("LATERAL (
-    SELECT sum((d->>'amount')::bigint) AS list
-    FROM jsonb_array_elements(t.asset -> 'payments') AS d(elem)) d"));
-
-        $builder->orderByRaw('COALESCE(d.list, t.amount) asc');
+        $this->baseCalculatedAmountQuery($builder, 'asc');
     }
 }
