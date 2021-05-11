6d24e0c2527d2844385d4b2ab8be3d7dc0b467ca
Highjhacker <jolanbeer@gmail.com>
2021-05-11 16:32:29 +0200

    wip

--- app/Http/Livewire/Concerns/Ordering.php
+++ app/Http/Livewire/Concerns/Ordering.php
@@ -5,12 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Livewire\Concerns;
 
 use App\Enums\OrderingDirectionEnum;
-use App\Models\Scopes\OrderByAddressAscScope;
-use App\Models\Scopes\OrderByAddressDescScope;
-use App\Models\Scopes\OrderByBalanceAscScope;
-use App\Models\Scopes\OrderByBalanceDescScope;
-use App\Models\Scopes\OrderBySupplyAscScope;
-use App\Models\Scopes\OrderBySupplyDescScope;
 
 trait Ordering
 {
@@ -28,15 +22,4 @@ trait Ordering
 
         return 'chevron-down';
     }
-
-    /*private function getOrderingScope(): string
-    {
-        $scopes = [
-            'address' => ['asc' => OrderByAddressAscScope::class, 'desc' => OrderByAddressDescScope::class],
-            'balance' => ['asc' => OrderByBalanceAscScope::class, 'desc' => OrderByBalanceDescScope::class],
-            'supply'  => ['asc' => OrderBySupplyAscScope::class, 'desc' => OrderBySupplyDescScope::class],
-        ];
-
-        return $scopes[$this->walletsOrdering][$this->walletsOrderingDirection];
-    }*/
 }
