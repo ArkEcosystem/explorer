17b7762d02dbdd62d6879f4756a1c185de13e5c5
alfonsobries <alfonsobries@users.noreply.github.com>
2021-05-13 22:12:46 +0000

    style: resolve style guide violations

--- resources/views/components/tables/rows/desktop/skeletons/status.blade.php
+++ resources/views/components/tables/rows/desktop/skeletons/status.blade.php
@@ -12,7 +12,7 @@
     :first-on="$firstOn"
     :last-on="$lastOn"
 >
-    <div class="flex flex-row-reverse items-center justify-between md:flex-row md:space-x-3 md:justify-start">
+    <div class="flex flex-row-reverse justify-between items-center md:flex-row md:space-x-3 md:justify-start">
         <div class="w-6 h-6 rounded-full md:w-11 md:h-11 loading-state"></div>
         <div class="w-6 h-6 rounded-full md:w-11 md:h-11 loading-state"></div>
         <div class="w-6 h-6 rounded-full md:w-11 md:h-11 loading-state"></div>
