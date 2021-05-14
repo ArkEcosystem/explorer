73a4967b466a26d7f2cbfad9a37b2f6ea92f6129
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-05-14 16:32:36 +0100

    fix paddings

--- resources/views/livewire/search-page.blade.php
+++ resources/views/livewire/search-page.blade.php
@@ -17,29 +17,27 @@
         </div>
     </div>
 
-    <div class="bg-white border-t-20 border-theme-secondary-100 dark:border-black dark:bg-theme-secondary-900" id="results-list">
-        <x-ark-container>
-            <h1 class="header-2">@lang('pages.search_results.title')</h1>
+    <x-ark-container>
+        <h1 class="header-2">@lang('pages.search_results.title')</h1>
 
-            @if($results && $results->count())
-                <div>
-                    @if ($state['type'] === 'block')
-                        <x-tables.blocks :blocks="$results" />
-                    @endif
+        @if($results && $results->count())
+            <div>
+                @if ($state['type'] === 'block')
+                    <x-tables.blocks :blocks="$results" />
+                @endif
 
-                    @if ($state['type'] === 'transaction')
-                        <x-tables.transactions :transactions="$results" />
-                    @endif
+                @if ($state['type'] === 'transaction')
+                    <x-tables.transactions :transactions="$results" />
+                @endif
 
-                    @if ($state['type'] === 'wallet')
-                        <x-tables.wallets :wallets="$results" />
-                    @endif
-                </div>
-            @else
-                <x-general.no-results :text="trans('pages.search_results.no_results')" />
-            @endif
-        </x-ark-container>
-    </div>
+                @if ($state['type'] === 'wallet')
+                    <x-tables.wallets :wallets="$results" />
+                @endif
+            </div>
+        @else
+            <x-general.no-results :text="trans('pages.search_results.no_results')" />
+        @endif
+    </x-ark-container>
 
     <script>
         window.addEventListener('livewire:load', () => window.livewire.on('pageChanged', () => scrollToQuery('#results-list')));
