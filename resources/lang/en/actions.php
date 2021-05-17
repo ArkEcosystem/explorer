b18c366a359d70eaf196c0b501beb48ec77dfd19
alfonsobries <alfonsobries@users.noreply.github.com>
2021-05-17 15:03:48 +0000

    style: resolve style guide violations

--- resources/lang/en/actions.php
+++ resources/lang/en/actions.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 return [
-    'advanced_search' => 'Advanced Search',
+    'advanced_search'   => 'Advanced Search',
     'hide_advanced'     => 'Hide Advanced',
-    'find_it'         => 'Find It',
-    'view_all'        => 'View All',
+    'find_it'           => 'Find It',
+    'view_all'          => 'View All',
 ];
