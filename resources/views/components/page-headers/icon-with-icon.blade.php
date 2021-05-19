e00d91f4c91992031f94c96d39435d9ecb2e983c
Highjhacker <Highjhacker@users.noreply.github.com>
2021-05-19 17:26:03 +0000

    style: resolve style guide violations

--- resources/views/components/page-headers/icon-with-icon.blade.php
+++ resources/views/components/page-headers/icon-with-icon.blade.php
@@ -4,7 +4,7 @@
     </div>
 </div>
 
-<div class="hidden md:flex border-4 rounded-full border-theme-secondary-100 dark:border-theme-secondary-900">
+<div class="hidden rounded-full border-4 md:flex border-theme-secondary-100 dark:border-theme-secondary-900">
     <div class="rounded-full circled-icon {{ $secondIconColors ?? 'text-theme-success-600 border-theme-success-600' }} bg-theme-secondary-100 dark:bg-theme-secondary-900">
         <x-ark-icon :name="$secondIcon" />
     </div>
