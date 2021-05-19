7a662e6d3be2c0425e4d8e1de8781765858f8fc2
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-19 15:58:59 -0500

    typehints

--- app/Http/Livewire/WalletBalance.php
+++ app/Http/Livewire/WalletBalance.php
@@ -6,6 +6,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Wallet;
 use App\ViewModels\ViewModelFactory;
+use App\ViewModels\WalletViewModel;
 use Livewire\Component;
 
 final class WalletBalance extends Component
@@ -17,12 +18,12 @@ final class WalletBalance extends Component
         $this->wallet = $wallet;
     }
 
-    public function getWalletView()
+    public function getWalletView(): WalletViewModel
     {
         return ViewModelFactory::make($this->wallet);
     }
 
-    public function render()
+    public function render(): void
     {
         return view('livewire.wallet-balance', [
             'balance' => $this->getWalletView()->balanceFiat(),
