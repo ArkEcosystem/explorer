222b138a9a3cf83aadf28e6df59e4ec84e1f9f2a
alfonsobries <alfonsobries@users.noreply.github.com>
2021-05-20 18:08:34 +0000

    style: resolve style guide violations

--- resources/views/livewire/network-status-block.blade.php
+++ resources/views/livewire/network-status-block.blade.php
@@ -2,7 +2,7 @@
     <div class="py-10 content-container-full-width">
         <div class="px-8 md:w-full">
             <div
-                class="flex w-full grid-cols-2 gap-3 md:grid xl:space-x-3 xl:gap-0 xl:flex"
+                class="flex grid-cols-2 gap-3 w-full md:grid xl:space-x-3 xl:gap-0 xl:flex"
                 wire:poll.60s
             >
                 <x-stats.stat :label="trans('general.height')" icon="app-block_height">
@@ -17,7 +17,7 @@
                     {{ $marketCap }}
                 </x-stats.stat>
 
-                <x-stats.stat class="justify-between flex-grow" :label="trans('general.price')" icon="app-price" :disabled="! Network::canBeExchanged()">
+                <x-stats.stat class="flex-grow justify-between" :label="trans('general.price')" icon="app-price" :disabled="! Network::canBeExchanged()">
                     {{ $price }}
 
                     <x-slot name="side">
