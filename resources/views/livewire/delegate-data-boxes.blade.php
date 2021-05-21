9872c148a62084c4b8893e266203d09b5a794e35
Highjhacker <Highjhacker@users.noreply.github.com>
2021-05-21 15:08:23 +0000

    style: resolve style guide violations

--- resources/views/livewire/delegate-data-boxes.blade.php
+++ resources/views/livewire/delegate-data-boxes.blade.php
@@ -1,29 +1,29 @@
 @if(! count($statistics))
     <div wire:poll="pollStatistics" wire:key="poll_statistics_skeleton">
         <div class="flex w-full md:flex-col xl:flex-row md:space-y-4 xl:space-y-0 xl:space-x-4">
-            <div class="flex flex-row py-3 px-6 bg-white dark:dark:bg-theme-secondary-900 rounded-xl">
+            <div class="flex flex-row py-3 px-6 bg-white rounded-xl dark:dark:bg-theme-secondary-900">
                 <div class="flex lg:w-1/2 xl:w-full space-x-4">
                     <div class="flex flex-grow rounded-xl dark:bg-theme-secondary-900">
-                        <div class="circled-icon h-11 rounded-xl border-theme-secondary-300 bg-theme-secondary-100"></div>
-                        <div class="flex flex-col ml-4 justify-between">
-                            <div class="h-2 w-24 bg-theme-secondary-100"></div>
-                            <div class="h-2 w-24 bg-theme-secondary-100"></div>
+                        <div class="h-11 rounded-xl circled-icon border-theme-secondary-300 bg-theme-secondary-100"></div>
+                        <div class="flex flex-col justify-between ml-4">
+                            <div class="w-24 h-2 bg-theme-secondary-100"></div>
+                            <div class="w-24 h-2 bg-theme-secondary-100"></div>
                         </div>
                     </div>
 
                     <div class="flex flex-grow rounded-xl dark:bg-theme-secondary-900">
-                        <div class="circled-icon h-11 rounded-xl border-theme-secondary-300 bg-theme-secondary-100"></div>
-                        <div class="flex flex-col ml-4 justify-between">
-                            <div class="h-2 w-24 bg-theme-secondary-100"></div>
-                            <div class="h-2 w-24 bg-theme-secondary-100"></div>
+                        <div class="h-11 rounded-xl circled-icon border-theme-secondary-300 bg-theme-secondary-100"></div>
+                        <div class="flex flex-col justify-between ml-4">
+                            <div class="w-24 h-2 bg-theme-secondary-100"></div>
+                            <div class="w-24 h-2 bg-theme-secondary-100"></div>
                         </div>
                     </div>
 
                     <div class="flex flex-grow rounded-xl dark:bg-theme-secondary-900">
-                        <div class="circled-icon h-11 rounded-xl border-theme-secondary-300 bg-theme-secondary-100"></div>
-                        <div class="flex flex-col ml-4 justify-between">
-                            <div class="h-2 w-24 bg-theme-secondary-100"></div>
-                            <div class="h-2 w-24 bg-theme-secondary-100"></div>
+                        <div class="h-11 rounded-xl circled-icon border-theme-secondary-300 bg-theme-secondary-100"></div>
+                        <div class="flex flex-col justify-between ml-4">
+                            <div class="w-24 h-2 bg-theme-secondary-100"></div>
+                            <div class="w-24 h-2 bg-theme-secondary-100"></div>
                         </div>
                     </div>
                 </div>
@@ -31,18 +31,18 @@
 
             <div class="flex flex-row space-x-4 w-full">
                 <div class="flex flex-grow py-3 px-6 bg-white rounded-xl dark:bg-theme-secondary-900">
-                    <div class="circled-icon h-11 rounded-xl border-theme-secondary-300 bg-theme-secondary-100"></div>
-                    <div class="flex flex-col ml-4 justify-between">
-                        <div class="h-2 w-24 bg-theme-secondary-100"></div>
-                        <div class="h-2 w-24 bg-theme-secondary-100"></div>
+                    <div class="h-11 rounded-xl circled-icon border-theme-secondary-300 bg-theme-secondary-100"></div>
+                    <div class="flex flex-col justify-between ml-4">
+                        <div class="w-24 h-2 bg-theme-secondary-100"></div>
+                        <div class="w-24 h-2 bg-theme-secondary-100"></div>
                     </div>
                 </div>
 
                 <div class="flex flex-grow py-3 px-6 bg-white rounded-xl dark:bg-theme-secondary-900">
-                    <div class="circled-icon h-11 rounded-xl border-theme-secondary-300 bg-theme-secondary-100"></div>
-                    <div class="flex flex-col ml-4 justify-between">
-                        <div class="h-2 w-24 bg-theme-secondary-100"></div>
-                        <div class="h-2 w-24 bg-theme-secondary-100"></div>
+                    <div class="h-11 rounded-xl circled-icon border-theme-secondary-300 bg-theme-secondary-100"></div>
+                    <div class="flex flex-col justify-between ml-4">
+                        <div class="w-24 h-2 bg-theme-secondary-100"></div>
+                        <div class="w-24 h-2 bg-theme-secondary-100"></div>
                     </div>
                 </div>
             </div>
@@ -52,7 +52,7 @@
     <div id="statistics-list" class="w-full" wire:poll.{{ Network::blockTime() }}s="pollStatistics" wire:key="poll_statistics_real">
         <div class="flex w-full md:flex-col xl:flex-row md:space-y-4 xl:space-y-0 xl:space-x-4">
             {{--TODO: Replace the text and progress of the 3 boxes below with real data once implemented--}}
-            <div class="flex flex-row py-3 px-6 bg-white dark:bg-theme-secondary-900 rounded-xl">
+            <div class="flex flex-row py-3 px-6 bg-white rounded-xl dark:bg-theme-secondary-900">
                 <div class="flex lg:w-1/2 xl:w-full">
                     <x-general.entity-header-item
                         title="Forging"
