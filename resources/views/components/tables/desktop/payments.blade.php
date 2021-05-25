ea77f5226e4b3f44f80102e570a05dd37b853499
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-25 09:32:05 -0500

    Use new ui table component

--- resources/views/components/tables/desktop/payments.blade.php
+++ resources/views/components/tables/desktop/payments.blade.php
@@ -1,36 +1,34 @@
-<div class="hidden w-full table-container md:block">
-    <table class="sticky-headers">
-        <thead>
-            <tr>
-                <x-tables.headers.desktop.address
-                    name="general.transaction.recipient"
+<x-ark-tables.table sticky class="hidden w-full md:block">
+    <thead>
+        <tr>
+            <x-tables.headers.desktop.address
+                name="general.transaction.recipient"
+                last-on="md"
+            />
+            <x-tables.headers.desktop.number
+                name="general.transaction.amount"
+                responsive
+                breakpoint="md"
+            />
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($payments as $payment)
+            <x-ark-tables.row>
+                <x-ark-tables.cell
+                    wire:key="{{ $payment->address() }}-address"
                     last-on="md"
-                />
-                <x-tables.headers.desktop.number
-                    name="general.transaction.amount"
+                >
+                    <x-general.identity :model="$payment" without-truncate />
+                </x-ark-tables.cell>
+                <x-ark-tables.cell
                     responsive
                     breakpoint="md"
-                />
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($payments as $payment)
-                <x-ark-tables.row>
-                    <x-ark-tables.cell
-                        wire:key="{{ $payment->address() }}-address"
-                        last-on="md"
-                    >
-                        <x-general.identity :model="$payment" without-truncate />
-                    </x-ark-tables.cell>
-                    <x-ark-tables.cell
-                        responsive
-                        breakpoint="md"
-                        class="text-right"
-                    >
-                        <x-tables.rows.desktop.amount :model="$payment" />
-                    </x-ark-tables.cell>
-                </x-ark-tables.row>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+                    class="text-right"
+                >
+                    <x-tables.rows.desktop.amount :model="$payment" />
+                </x-ark-tables.cell>
+            </x-ark-tables.row>
+        @endforeach
+    </tbody>
+</x-ark-tables.table>
--- resources/views/components/tables/skeletons/desktop.blade.php
+++ resources/views/components/tables/skeletons/desktop.blade.php
@@ -1,33 +1,31 @@
-<div class="hidden w-full table-container md:block">
-    <table class="sticky-headers">
-        <thead>
-            <tr>
-                @foreach($headers as $name => $header)
+<x-ark-tables.table sticky class="hidden w-full md:block">
+    <thead>
+        <tr>
+            @foreach($headers as $name => $header)
+                <x-dynamic-component
+                    :component="$header['component']"
+                    :name="$name"
+                    :responsive="Arr::get($header, 'responsive', false)"
+                    :breakpoint="Arr::get($header, 'breakpoint', 'lg')"
+                    :first-on="Arr::get($header, 'firstOn', null)"
+                    :last-on="Arr::get($header, 'lastOn', null)"
+                />
+            @endforeach
+        </tr>
+    </thead>
+    <tbody>
+        <x-skeleton>
+            <x-ark-tables.row>
+                @foreach($rows as $row)
                     <x-dynamic-component
-                        :component="$header['component']"
-                        :name="$name"
-                        :responsive="Arr::get($header, 'responsive', false)"
-                        :breakpoint="Arr::get($header, 'breakpoint', 'lg')"
-                        :first-on="Arr::get($header, 'firstOn', null)"
-                        :last-on="Arr::get($header, 'lastOn', null)"
+                        :component="$row['component']"
+                        :responsive="Arr::get($row, 'responsive', false)"
+                        :breakpoint="Arr::get($row, 'breakpoint', 'lg')"
+                        :first-on="Arr::get($row, 'firstOn', null)"
+                        :last-on="Arr::get($row, 'lastOn', null)"
                     />
                 @endforeach
-            </tr>
-        </thead>
-        <tbody>
-            <x-skeleton>
-                <x-ark-tables.row>
-                    @foreach($rows as $row)
-                        <x-dynamic-component
-                            :component="$row['component']"
-                            :responsive="Arr::get($row, 'responsive', false)"
-                            :breakpoint="Arr::get($row, 'breakpoint', 'lg')"
-                            :first-on="Arr::get($row, 'firstOn', null)"
-                            :last-on="Arr::get($row, 'lastOn', null)"
-                        />
-                    @endforeach
-                </x-ark-tables.row>
-            </x-skeleton>
-        </tbody>
-    </table>
-</div>
+            </x-ark-tables.row>
+        </x-skeleton>
+    </tbody>
+</x-ark-tables.table>
