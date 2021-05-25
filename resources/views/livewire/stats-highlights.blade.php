1522a2a439846c2ec03d4f7cf9b161f7e1db0ae0
faustbrian <faustbrian@users.noreply.github.com>
2021-05-25 15:28:32 +0000

    style: resolve style guide violations

--- resources/views/livewire/stats-highlights.blade.php
+++ resources/views/livewire/stats-highlights.blade.php
@@ -1,4 +1,4 @@
-<div wire:poll.60s class="w-full gap-5 flex overflow-x-auto md:overflow-x-hidden md:grid md:grid-cols-2 md:grid-rows-2 xl:grid-cols-4 xl:grid-rows-1">
+<div wire:poll.60s class="flex overflow-x-auto gap-5 w-full md:overflow-x-hidden md:grid md:grid-cols-2 md:grid-rows-2 xl:grid-cols-4 xl:grid-rows-1">
     <x-stats.highlight icon="app.stats-total-supply" :title="trans('pages.statistics.highlights.total-supply')" :value="$totalSupply" />
     <x-stats.highlight icon="app.stats-voting" :title="trans('pages.statistics.highlights.voting', ['percent' => $votingPercent])" :value="$votingValue" />
     <x-stats.highlight icon="app.stats-delegates" :title="trans('pages.statistics.highlights.registered-delegates')" :value="$delegates" />
