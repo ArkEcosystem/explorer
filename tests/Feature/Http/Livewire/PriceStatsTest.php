daf52c83eb0ee417f4258053a2176dd755f8a495
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-25 11:48:06 -0500

    Test coverage

--- tests/Feature/Http/Livewire/PriceStatsTest.php
+++ tests/Feature/Http/Livewire/PriceStatsTest.php
@@ -23,3 +23,16 @@ it('should render the values', function () {
     Livewire::test(PriceStats::class, ['placeholder' => true])
         ->assertSee('[4,5,2,2,2,3,5,1,4,5,6,5,3,3,4,5,6,4,4,4,5,8,8,10]');
 });
+
+
+it('handle price change when price is zero', function () {
+    $response = json_decode(file_get_contents(base_path('tests/fixtures/cryptocompare/histohour.json')), true);
+    // Force 0 price
+    $response['Data'] = collect($response['Data'])->map(fn ($item) => array_merge($item, ['close' => 0]))->toArray();
+
+    Http::fake([
+        'cryptocompare.com/*' => Http::response($response),
+    ]);
+
+    Livewire::test(PriceStats::class)->assertSee('0.00%');
+});
