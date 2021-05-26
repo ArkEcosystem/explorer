bcc483c1c8101ff79ccfac6d358b69dcef6c9381
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-26 15:24:37 -0500

    Adjust text color

--- resources/views/components/grid/generic.blade.php
+++ resources/views/components/grid/generic.blade.php
@@ -6,7 +6,7 @@
 ])
 
 <div {{ $attributes->merge(['class' => 'flex justify-between pb-4 space-x-4'])->only('class') }}>
-    <div class="flex flex-col flex-1 space-y-2 min-w-0 min-h-0">
+    <div class="flex flex-col flex-1 min-w-0 min-h-0 space-y-2">
         <span class="text-sm font-semibold text-theme-secondary-500 dark:text-theme-secondary-700">
             {{ $title }}
         </span>
@@ -16,13 +16,13 @@
                 @lang('generic.not_specified')
             </span>
         @else
-            <span class="font-semibold break-words text-theme-secondary-700 dark:text-theme-secondary-200">
+            <span class="font-semibold break-words text-theme-secondary-900 dark:text-theme-secondary-200">
                 {{ $slot }}
             </span>
         @endif
     </div>
 
-    <div class="flex justify-center mt-1 w-12 h-12">
+    <div class="flex justify-center w-12 h-12 mt-1">
         <div class="circled-icon text-theme-secondary-900 border-theme-secondary-900 dark:text-theme-secondary-600 dark:border-theme-secondary-600">
             <x-ark-icon
                 :name="$icon"
