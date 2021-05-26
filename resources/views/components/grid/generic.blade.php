c3b50d14eb0a353db67091efdf02ce16cc8ee6d8
faustbrian <faustbrian@users.noreply.github.com>
2021-05-26 20:25:31 +0000

    style: resolve style guide violations

--- resources/views/components/grid/generic.blade.php
+++ resources/views/components/grid/generic.blade.php
@@ -6,7 +6,7 @@
 ])
 
 <div {{ $attributes->merge(['class' => 'flex justify-between pb-4 space-x-4'])->only('class') }}>
-    <div class="flex flex-col flex-1 min-w-0 min-h-0 space-y-2">
+    <div class="flex flex-col flex-1 space-y-2 min-w-0 min-h-0">
         <span class="text-sm font-semibold text-theme-secondary-500 dark:text-theme-secondary-700">
             {{ $title }}
         </span>
@@ -22,7 +22,7 @@
         @endif
     </div>
 
-    <div class="flex justify-center w-12 h-12 mt-1">
+    <div class="flex justify-center mt-1 w-12 h-12">
         <div class="circled-icon text-theme-secondary-900 border-theme-secondary-900 dark:text-theme-secondary-600 dark:border-theme-secondary-600">
             <x-ark-icon
                 :name="$icon"
