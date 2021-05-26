ac77d6cc1d1f696ee8d9550edec22d24f579fc3d
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-26 15:10:46 -0500

    Update icons and other

--- resources/views/components/transaction/extension/multi-payment.blade.php
+++ resources/views/components/transaction/extension/multi-payment.blade.php
@@ -1,7 +1,7 @@
-<div class="bg-white border-t-20 border-theme-secondary-100 dark:border-black dark:bg-theme-secondary-900">
+<div class="bg-white border-t border-theme-secondary-300 dark:border-theme-secondary-800 dark:bg-theme-secondary-900">
     <x-ark-container>
         <div class="w-full">
-            <div class="flex relative justify-between items-end mb-5">
+            <div class="relative flex items-end justify-between mb-5">
                 <h4>@lang('pages.transaction.recipient_list')</h4>
             </div>
 
