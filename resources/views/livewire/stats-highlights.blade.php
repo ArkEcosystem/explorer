147c77bd034c40720a06557756054a40ae492275
leMaur <leMaur@users.noreply.github.com>
2021-05-27 08:44:07 +0000

    style: resolve style guide violations

--- resources/views/livewire/stats-highlights.blade.php
+++ resources/views/livewire/stats-highlights.blade.php
@@ -1,4 +1,4 @@
-<div wire:poll.60s class="flex w-full gap-5 md:grid md:grid-cols-2 md:grid-rows-2 xl:grid-cols-4 xl:grid-rows-1">
+<div wire:poll.60s class="flex gap-5 w-full md:grid md:grid-cols-2 md:grid-rows-2 xl:grid-cols-4 xl:grid-rows-1">
     <x-stats.highlight icon="stacked-coins" :title="trans('pages.statistics.highlights.total-supply')" :value="$totalSupply" />
     <x-stats.highlight icon="checkmark-box" :title="trans('pages.statistics.highlights.voting', ['percent' => $votingPercent])" :value="$votingValue" />
     <x-stats.highlight icon="delegate_registration" :title="trans('pages.statistics.highlights.registered-delegates')" :value="$delegates" />
