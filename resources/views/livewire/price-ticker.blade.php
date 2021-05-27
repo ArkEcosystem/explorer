a4fd0cf9a2ef7f83d2a9c69ec4c2bf90efe21758
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-27 13:51:32 -0500

    Add opacity while changing currency

--- resources/views/livewire/price-ticker.blade.php
+++ resources/views/livewire/price-ticker.blade.php
@@ -1,3 +1,8 @@
-<div wire:poll.60s class="uppercase">
-    {{ $from }}/{{ $to }}: {{ $price }}
+<div
+    wire:poll.60s
+    class="relative uppercase"
+    x-data="{ to: '{{ $to }}', busy: false }"
+    x-init="livewire.on('currencyChanged', () => busy = true);"
+>
+    <span :class="{ 'opacity-50': busy }">{{ $from }}/{{ $to }}: {{ $price }}</span>
 </div>
