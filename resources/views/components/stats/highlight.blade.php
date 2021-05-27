307fac5f931410061b7a773b344a40276a82e3c9
Maurizio <maurizio.lepora@gmail.com>
2021-05-27 10:40:52 +0200

    wip

--- resources/views/components/stats/highlight.blade.php
+++ resources/views/components/stats/highlight.blade.php
@@ -2,7 +2,9 @@
 
 <x-general.card class="flex flex-shrink-0 items-center space-x-3">
     <div class="flex items-center space-x-3">
-        <x-ark-icon :name="$icon" size="xl" class="text-theme-secondary-900 dark:text-theme-secondary-600" />
+        <div class="border-2 border-theme-secondary-900 rounded-full p-2.5">
+            <x-ark-icon :name="$icon" class="text-theme-secondary-900 dark:text-theme-secondary-600" />
+        </div>
 
         <div class="flex flex-col">
             <h2 class="text-sm font-semibold text-theme-secondary-500 dark:text-theme-secondary-700 whitespace-nowrap">{{ $title }}</h2>
