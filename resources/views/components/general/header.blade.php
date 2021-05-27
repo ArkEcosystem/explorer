424c628e3dc5ac4de12c8326ae20e64b4d31af5a
Maurizio <maurizio.lepora@gmail.com>
2021-05-27 10:07:44 +0200

    wip

--- resources/views/components/general/header.blade.php
+++ resources/views/components/general/header.blade.php
@@ -1,5 +1,3 @@
-<div class="bg-theme-secondary-100 dark:bg-black">
-    <div class="py-8 px-8 content-container-full-width md:px-10">
-        {{ $slot }}
-    </div>
+<div {{ $attributes->class('py-8 content-container-full-width bg-theme-secondary-100 dark:bg-black') }}>
+    {{ $slot }}
 </div>
