fdb48e5c46cf314df7a36a44ffacab6742587506
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-28 13:52:51 -0500

    Refresh stats blocks & price chart independently

--- resources/views/livewire/network-status-block.blade.php
+++ resources/views/livewire/network-status-block.blade.php
@@ -2,7 +2,7 @@
     <div class="py-8 content-container-full-width">
         <div class="px-8 md:w-full">
             <div
-                class="flex grid-cols-2 gap-3 w-full md:grid xl:space-x-3 xl:gap-0 xl:flex"
+                class="flex w-full grid-cols-2 gap-3 md:grid xl:space-x-3 xl:gap-0 xl:flex"
                 wire:poll.{{ Network::blockTime() }}s
             >
                 <x-stats.stat :label="trans('general.height')" icon="app-block_height">
@@ -17,7 +17,9 @@
                     {{ $marketCap }}
                 </x-stats.stat>
 
-                <livewire:price-stats />
+                <div wire:ignore>
+                    <livewire:price-stats />
+                </div>
             </div>
         </div>
     </div>
--- resources/views/livewire/price-stats.blade.php
+++ resources/views/livewire/price-stats.blade.php
@@ -1,77 +1,74 @@
+<div class="justify-between flex-grow" wire:poll.60s>
+    <x-stats.stat
+        icon="app-price"
+        :disabled="! Network::canBeExchanged()"
+    >
+        <x-slot name="body">
+            <div class="flex flex-grow">
+                <div class="flex flex-col justify-between h-full">
+                    <div class="flex">
+                        <span class="text-sm font-semibold leading-none whitespace-nowrap dark:text-theme-secondary-600 text-theme-secondary-500">@lang('general.price')</span>
+                        @if (Network::canBeExchanged())
+                            <a class="pl-3 ml-3 text-sm font-semibold leading-none border-l whitespace-nowrap link border-theme-secondary-300 dark:border-theme-secondary-800" href="{{ route('statistics') }}">
+                                @lang('actions.view_statistics')
+                            </a>
+                        @endif
+                    </div>
 
-<x-stats.stat
-    class="flex-grow justify-between"
-    icon="app-price"
-    wire:poll.60s
-    :disabled="! Network::canBeExchanged()"
->
-    <x-slot name="body">
-        <div class="flex flex-grow">
-            <div class="flex flex-col justify-between h-full">
-                <div class="flex">
-                    <span class="text-sm font-semibold leading-none whitespace-nowrap dark:text-theme-secondary-600 text-theme-secondary-500">@lang('general.price')</span>
                     @if (Network::canBeExchanged())
-                        <a class="pl-3 ml-3 text-sm font-semibold leading-none whitespace-nowrap border-l link border-theme-secondary-300 dark:border-theme-secondary-800" href="{{ route('statistics') }}">
-                            @lang('actions.view_statistics')
-                        </a>
+                        <div class="flex space-x-3">
+                            <span class="font-semibold leading-none whitespace-nowrap text-theme-secondary-900 dark:text-white">
+                                {{ $price }}
+                            </span>
+
+                            @if ($priceChange < 0)
+                                <span class="flex items-center space-x-1 text-sm font-semibold leading-none text-theme-danger-400">
+                                    <span>
+                                        <x-ark-icon name="triangle-down" size="2xs" />
+                                    </span>
+                                    <span>
+                                        <x-percentage>{{ $priceChange * 100 * -1 }}</x-percentage>
+                                    </span>
+                                </span>
+                            @else
+                                <span class="flex items-center space-x-1 text-sm font-semibold leading-none text-theme-success-600">
+                                    <span>
+                                        <x-ark-icon name="triangle-up" size="2xs" />
+                                    </span>
+                                    <span>
+                                        <x-percentage>{{ $priceChange * 100 }}</x-percentage>
+                                    </span>
+                                </span>
+                            @endif
+                        </div>
+                    @else
+                        <span class="font-semibold leading-none whitespace-nowrap dark:text-theme-secondary-600 text-theme-secondary-500">
+                            @lang('general.not_available')
+                        </span>
                     @endif
                 </div>
 
-                @if (Network::canBeExchanged())
-                    <div class="flex space-x-3">
-                        <span class="font-semibold leading-none whitespace-nowrap text-theme-secondary-900 dark:text-white">
-                            {{ $price }}
-                        </span>
-
-                        @if ($priceChange < 0)
-                            <span class="flex items-center space-x-1 text-sm font-semibold leading-none text-theme-danger-400">
-                                <span>
-                                    <x-ark-icon name="triangle-down" size="2xs" />
-                                </span>
-                                <span>
-                                    <x-percentage>{{ $priceChange * 100 * -1 }}</x-percentage>
-                                </span>
-                            </span>
-                        @else
-                            <span class="flex items-center space-x-1 text-sm font-semibold leading-none text-theme-success-600">
-                                <span>
-                                    <x-ark-icon name="triangle-up" size="2xs" />
-                                </span>
-                                <span>
-                                    <x-percentage>{{ $priceChange * 100 }}</x-percentage>
-                                </span>
-                            </span>
-                        @endif
-                    </div>
-                @else
-                    <span class="font-semibold leading-none whitespace-nowrap dark:text-theme-secondary-600 text-theme-secondary-500">
-                        @lang('general.not_available')
-                    </span>
-                @endif
-            </div>
-
-            <div class="hidden flex-grow justify-end lg:flex" >
-                <div
-                    wire:key="{{ Settings::currency() }}"
-                    class="ml-6"
-                    x-data="PriceChart(
-                        {{ $historical->values()->toJson() }},
-                        {{ $historical->keys()->toJson() }},
-                        {{ $priceChange === null ? 0 : $priceChange }},
-                        {{ ! Network::canBeExchanged() ? 'true' : 'false' }},
-                        {{ Settings::usesDarkTheme() ? 'true' : 'false' }},
-                        '{{ time() }}'
-                    )"
-                    x-init="init"
-                    @toggle-dark-mode.window="toggleDarkMode"
-                >
-                    <div class="block" wire:ignore>
-                        <canvas x-ref="chart" class="w-full h-full" width="120" height="40" ></canvas>
+                <div class="justify-end flex-grow hidden lg:flex" >
+                    <div
+                        wire:key="{{ Settings::currency() }}"
+                        class="ml-6"
+                        x-data="PriceChart(
+                            {{ $historical->values()->toJson() }},
+                            {{ $historical->keys()->toJson() }},
+                            {{ $priceChange === null ? 0 : $priceChange }},
+                            {{ ! Network::canBeExchanged() ? 'true' : 'false' }},
+                            {{ Settings::usesDarkTheme() ? 'true' : 'false' }},
+                            '{{ time() }}'
+                        )"
+                        x-init="init"
+                        @toggle-dark-mode.window="toggleDarkMode"
+                    >
+                        <div class="block" wire:ignore>
+                            <canvas x-ref="chart" class="w-full h-full" width="120" height="40" ></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </x-slot>
-</x-stats.stat>
-
-
+        </x-slot>
+    </x-stats.stat>
+</div>
