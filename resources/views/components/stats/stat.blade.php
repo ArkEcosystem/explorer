8f0fcd2f9445e414fc12a6b198f91fdd72cdf9ca
alfonsobries <alfonsobries@users.noreply.github.com>
2021-05-28 15:42:26 +0000

    style: resolve style guide violations

--- resources/views/components/stats/stat.blade.php
+++ resources/views/components/stats/stat.blade.php
@@ -5,13 +5,13 @@
 ])
 
 <div {{ $attributes->merge(['class' => 'flex py-4 bg-white dark:bg-theme-secondary-900 rounded-xl px-7']) }}>
-    <div class="flex items-center flex-grow space-x-4">
+    <div class="flex flex-grow items-center space-x-4">
         <span
             class="flex items-center justify-center w-10 h-10 border-2 rounded-full @if($disabled) dark:border-theme-secondary-600 border-theme-secondary-500 dark:text-theme-secondary-600 text-theme-secondary-500 @else border-theme-secondary-900 text-theme-secondary-900 dark:text-theme-secondary-700 dark:border-theme-secondary-700 @endif"
         >
             <x-ark-icon :name="$icon" />
         </span>
-        <span class="flex flex-col justify-between flex-grow h-full">
+        <span class="flex flex-col flex-grow justify-between h-full">
             @isset($body)
                 {{ $body }}
             @else
