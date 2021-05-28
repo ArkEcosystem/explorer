b2acd2483e6fecb7f923e761aec4cda67b94488a
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-28 09:17:15 -0500

    Use spinner on mobile search

--- resources/views/components/general/search/search-input.blade.php
+++ resources/views/components/general/search/search-input.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center py-6 px-8">
+<div class="flex items-center px-8 py-6">
     <div class="flex-1 mr-8">
         <input
             x-ref="input"
@@ -22,7 +22,7 @@
 
     <button
         type="button"
-        class="hidden py-2 px-4 mr-8 font-normal text-center rounded text-theme-primary-500 transition-default hover:bg-theme-primary-100 dark:hover:bg-theme-secondary-800 dark:text-theme-secondary-600 md:block"
+        class="hidden px-4 py-2 mr-8 font-normal text-center rounded text-theme-primary-500 transition-default hover:bg-theme-primary-100 dark:hover:bg-theme-secondary-800 dark:text-theme-secondary-600 md:block"
         @click="showAdvanced = !showAdvanced;"
     >
         <span x-show="!showAdvanced">@lang('actions.advanced_search')</span>
@@ -31,21 +31,24 @@
 
     <button
         type="button"
-        class="hidden relative button-primary md:block"
+        class="relative hidden button-primary md:block"
         :class="{ 'pointer-events-none' : searching }"
         wire:click="performSearch"
         @click="searching = true"
     >
-        <span x-show="searching" class="flex absolute right-0 left-0 justify-center items-center" x-cloak>
+        <span x-show="searching" class="absolute left-0 right-0 flex items-center justify-center" x-cloak>
             <x-ark-spinner-icon />
         </span>
         <span :class="{ 'invisible': searching }">@lang('actions.find_it')</span>
     </button>
 
-    <div
+    <button
+        type="button"
         class="cursor-pointer text-theme-secondary-700 hover:text-theme-primary-600 dark:text-theme-secondary-500 dark:hover:text-theme-secondary-400 md:hidden"
         wire:click="performSearch"
+        @click="searching = true"
     >
-        <x-ark-icon name="search" />
-    </div>
+        <span x-show="searching"><x-ark-spinner-icon /></span>
+        <span x-show="!searching"><x-ark-icon name="search" /></span>
+    </button>
 </div>
