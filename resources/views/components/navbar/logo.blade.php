16091c2d0349dcf15faa3c6b9e7d639bc1699f26
alfonsobries <alfonsobries@users.noreply.github.com>
2021-05-31 17:14:48 +0000

    style: resolve style guide violations

--- resources/views/components/navbar/logo.blade.php
+++ resources/views/components/navbar/logo.blade.php
@@ -1,4 +1,4 @@
-<div class="relative flex items-center">
+<div class="flex relative items-center">
     <img src="/images/logo.svg" class="h-10 lg:h-11" />
 
     <span class="hidden ml-4 sm:flex text-theme-secondary-900 dark:text-theme-secondary-200 sm:items-center sm:text-xl">
