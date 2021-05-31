35d4504be29cf2cfe54dcb97a86348b81f997c76
Highjhacker <Highjhacker@users.noreply.github.com>
2021-05-31 14:05:08 +0000

    style: resolve style guide violations

--- resources/views/components/page-headers/wallet/actions/public-key.blade.php
+++ resources/views/components/page-headers/wallet/actions/public-key.blade.php
@@ -1,10 +1,10 @@
-<div x-data="{ publicKeyModalVisible: false }" class="flex-1 w-full sm:w-auto mt-4 sm:mt-0">
+<div x-data="{ publicKeyModalVisible: false }" class="flex-1 mt-4 w-full sm:w-auto sm:mt-0">
     <button type="button" @click="publicKeyModalVisible = !publicKeyModalVisible"
         class="flex justify-center items-center px-3 w-full h-11 rounded cursor-pointer lg:w-16 bg-theme-secondary-800 hover:bg-theme-primary-600 transition-default lg:flex-none dark:text-theme-secondary-600 dark:hover:text-theme-secondary-200">
         <x-ark-icon name="key" />
     </button>
 
-    <div @click.away="publicKeyModalVisible = false" x-show="publicKeyModalVisible" class="absolute left-0 mx-8 lg:mx-0 lg:left-auto right-0 lg:mr-36 w-auto z-10 bg-white rounded-lg shadow-lg dark:shadow-lg-dark dark:bg-theme-secondary-900" x-cloak>
+    <div @click.away="publicKeyModalVisible = false" x-show="publicKeyModalVisible" class="absolute right-0 left-0 z-10 mx-8 w-auto bg-white rounded-lg shadow-lg lg:mx-0 lg:left-auto lg:mr-36 dark:shadow-lg-dark dark:bg-theme-secondary-900" x-cloak>
         <div class="flex flex-col p-6 space-y-2 leading-tight">
             <span class="text-sm font-semibold text-theme-secondary-400 dark:text-theme-secondary-700">@lang('pages.wallet.public_key.title')</span>
             <span class="flex font-semibold link">
