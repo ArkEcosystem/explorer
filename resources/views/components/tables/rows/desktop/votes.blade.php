ab0bf5e53237ae9aca231da30f0254bb522fbff6
ItsANameToo <itsanametoo@protonmail.com>
2021-05-31 15:01:50 +0200

    wip

--- resources/views/components/tables/rows/desktop/votes.blade.php
+++ resources/views/components/tables/rows/desktop/votes.blade.php
@@ -1,6 +1,5 @@
 <div class="flex justify-end">
-    <x-currency :currency="Network::currency()">{{ App\Services\NumberFormatter::number($model->votes()) }}</x-currency>
-
+    <span>{{ App\Services\NumberFormatter::number($model->votes()) }} {{ Network::currency() }}</span>
     <span class="vote-percentage">
         <x-percentage>{{ $model->votesPercentage() }}</x-percentage>
     </span>
