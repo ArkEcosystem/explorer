23febb6ab3b64f44bc195446ec098593fe968cbf
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-05-31 11:11:19 -0500

    Remove beta badge

--- resources/views/components/navbar/logo.blade.php
+++ resources/views/components/navbar/logo.blade.php
@@ -1,4 +1,4 @@
-<div class="flex relative items-center">
+<div class="relative flex items-center">
     <img src="/images/logo.svg" class="h-10 lg:h-12" />
 
     <span class="hidden ml-4 sm:flex text-theme-secondary-900 dark:text-theme-secondary-200 sm:items-center sm:text-2xl">
@@ -6,8 +6,4 @@
             {{ Network::explorerTitle() }}
         </span>
     </span>
-
-    <div class="navbar-beta-badge">
-        @lang('general.beta_uppercase')
-    </div>
 </div>
