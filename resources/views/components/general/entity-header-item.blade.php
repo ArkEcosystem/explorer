6ff1c785570dd22c67c2f55784bd80174faa701b
alfonsobries <alfonsobries@users.noreply.github.com>
2021-05-31 18:55:38 +0000

    style: resolve style guide violations

--- resources/views/components/general/entity-header-item.blade.php
+++ resources/views/components/general/entity-header-item.blade.php
@@ -1,6 +1,6 @@
 <div class="entity-header-item {{ $wrapperClass ?? false }}">
     @unless($withoutIcon ?? false)
-        <div class="items-center hidden lg:flex">
+        <div class="hidden items-center lg:flex">
             <div class="circled-icon text-theme-secondary-900 border-theme-secondary-900 dark:text-theme-secondary-600 dark:border-theme-secondary-600">
                 @if ($icon ?? false)
                     @if ($iconSize ?? false)
