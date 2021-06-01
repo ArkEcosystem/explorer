774e5b1b987913554353e935342074564abfdf25
alfonsobries <alfonsobries@users.noreply.github.com>
2021-06-01 22:55:23 +0000

    style: resolve style guide violations

--- resources/js/tabs.js
+++ resources/js/tabs.js
@@ -1,4 +1,4 @@
-const Tabs = (defaultSelected = '', extraData = {}, onSelected = () => {}) => {
+const Tabs = (defaultSelected = "", extraData = {}, onSelected = () => {}) => {
     return {
         selected: defaultSelected,
         select(name) {
@@ -7,33 +7,33 @@ const Tabs = (defaultSelected = '', extraData = {}, onSelected = () => {}) => {
         },
         selectPrevTab(e) {
             const el = e.target;
-            const wrapper = el.closest('[role=tablist]');
+            const wrapper = el.closest("[role=tablist]");
             let prevTab = el;
             do {
                 prevTab = prevTab.previousElementSibling;
                 if (prevTab === null) {
-                    prevTab = wrapper.querySelector('[role=tab]:last-child');
+                    prevTab = wrapper.querySelector("[role=tab]:last-child");
                 }
-            } while(! prevTab.matches('[role=tab]'));
+            } while (!prevTab.matches("[role=tab]"));
             prevTab.focus();
         },
         selectNextTab(e) {
             const el = e.target;
-            const wrapper = el.closest('[role=tablist]');
+            const wrapper = el.closest("[role=tablist]");
             let nextTab = el;
 
             do {
                 nextTab = nextTab.nextElementSibling;
                 if (nextTab === null) {
-                    nextTab = wrapper.querySelector('[role=tab]');
+                    nextTab = wrapper.querySelector("[role=tab]");
                 }
-            } while(! nextTab.matches('[role=tab]'));
+            } while (!nextTab.matches("[role=tab]"));
 
             nextTab.focus();
         },
         onSelected: onSelected,
-        ...extraData
-    }
+        ...extraData,
+    };
 };
 
 export default Tabs;
