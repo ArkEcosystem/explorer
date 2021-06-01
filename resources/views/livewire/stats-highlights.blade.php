99546f8ce1229dc0ecbdbf9db66b7acb5b9d0c6e
Highjhacker <jolanbeer@gmail.com>
2021-06-01 17:29:42 +0200

    fix: failing tests

--- resources/views/livewire/stats-highlights.blade.php
+++ resources/views/livewire/stats-highlights.blade.php
@@ -3,19 +3,19 @@
         <div class="px-10 md:w-full">
             <div wire:poll.{{ $refreshInterval }}s class="flex gap-3 w-full md:grid md:grid-cols-2 md:grid-rows-2 xl:grid-cols-4 xl:grid-rows-1">
                 <x-stats.stat :label="trans('pages.statistics.highlights.total-supply')" icon="stacked-coins">
-                    <x-number>{{ $totalSupply }}</x-number>
+                    <x-currency :currency="Network::currency()">{{ $totalSupply }}</x-currency>
                 </x-stats.stat>
 
                 <x-stats.stat :label="trans('pages.statistics.highlights.voting', ['percent' => $votingPercent])" icon="checkmark-box">
-                    <x-number>{{ $votingValue }}</x-number>
+                    <x-currency :currency="Network::currency()">{{ $votingValue }}</x-currency>
                 </x-stats.stat>
 
                 <x-stats.stat :label="trans('pages.statistics.highlights.registered-delegates')" icon="delegate_registration">
-                    <x-number>{{ $delegates }}</x-number>
+                    {{ $delegates }}
                 </x-stats.stat>
 
                 <x-stats.stat :label="trans('pages.statistics.highlights.wallets')" icon="wallet">
-                    <x-number>{{ $wallets }}</x-number>
+                    {{ $wallets }}
                 </x-stats.stat>
             </div>
         </div>
