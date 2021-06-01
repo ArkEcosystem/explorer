54442d25e5a6f3187d5822a7bdbd81e82f052630
Highjhacker <jolanbeer@gmail.com>
2021-06-01 17:09:59 +0200

    refactor: tweaks for statistics boxes

--- resources/views/components/general/card.blade.php
+++ resources/views/components/general/card.blade.php
@@ -1,8 +1,8 @@
 @props(['withBorder' => false])
 
 <div {{ $attributes->class([
-        'rounded-lg',
-        'px-8 py-4 bg-white dark:bg-theme-secondary-900' => ! $withBorder,
+        'rounded-xl',
+        'px-7 py-4 bg-white dark:bg-theme-secondary-900' => ! $withBorder,
         'p-8 border border-theme-secondary-300 dark:border-theme-secondary-800' => $withBorder,
     ]) }}>
     {{ $slot }}
