d046164a5d91ed055ccf579a16e36cb49d2d2b3f
Highjhacker <jolanbeer@gmail.com>
2021-06-02 09:20:39 +0200

    refactor: requested changes

--- resources/views/livewire/stats-highlights.blade.php
+++ resources/views/livewire/stats-highlights.blade.php
@@ -3,11 +3,11 @@
         <div class="px-10 md:w-full">
             <div wire:poll.{{ $refreshInterval }}s class="flex gap-3 w-full md:grid md:grid-cols-2 md:grid-rows-2 xl:grid-cols-4 xl:grid-rows-1">
                 <x-stats.stat :label="trans('pages.statistics.highlights.total-supply')" icon="stacked-coins">
-                    <x-currency :currency="Network::currency()">{{ $totalSupply }}</x-currency>
+                    {{ $totalSupply }}
                 </x-stats.stat>
 
                 <x-stats.stat :label="trans('pages.statistics.highlights.voting', ['percent' => $votingPercent])" icon="checkmark-box">
-                    <x-currency :currency="Network::currency()">{{ $votingValue }}</x-currency>
+                   {{ $votingValue }}
                 </x-stats.stat>
 
                 <x-stats.stat :label="trans('pages.statistics.highlights.registered-delegates')" icon="delegate_registration">
