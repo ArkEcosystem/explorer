2913b82fc3d742994cf995d81e4e937bf9e44065
alfonsobries <alfonsobries@users.noreply.github.com>
2021-06-03 14:07:21 +0000

    style: resolve style guide violations

--- resources/views/components/general/entity-header-item.blade.php
+++ resources/views/components/general/entity-header-item.blade.php
@@ -1,6 +1,6 @@
 <div class="entity-header-item {{ $wrapperClass ?? false }}">
     @unless($withoutIcon ?? false)
-        <div class="items-center hidden md:flex">
+        <div class="hidden items-center md:flex">
             <div class="circled-icon text-theme-secondary-900 border-theme-secondary-900 dark:text-theme-secondary-600 dark:border-theme-secondary-600">
                 @if ($icon ?? false)
                     @if ($iconSize ?? false)
--- resources/views/components/general/entity-header.blade.php
+++ resources/views/components/general/entity-header.blade.php
@@ -1,16 +1,16 @@
-<div class="flex flex-col border-2 rounded-xl border-theme-secondary-300 dark:border-theme-secondary-800">
+<div class="flex flex-col rounded-xl border-2 border-theme-secondary-300 dark:border-theme-secondary-800">
     <div class="{{ $padding ?? 'px-8 py-6' }} bg-black dark:bg-theme-secondary-900 @if (isset($bottom)) rounded-t-xl @else rounded-xl @endif lg:relative">
-        <div class="flex flex-col justify-between space-y-8 overflow-auto lg:flex-row lg:space-y-0 ">
+        <div class="flex overflow-auto flex-col justify-between space-y-8 lg:flex-row lg:space-y-0 ">
             <div class="flex overflow-auto md:space-x-4">
-                <div class="items-center hidden md:flex">
+                <div class="hidden items-center md:flex">
                     {!! $logo !!}
                 </div>
 
-                <div class="flex flex-col justify-between flex-1 min-w-0 space-y-4 overflow-auto font-semibold lg:ml-4 md:space-y-0">
+                <div class="flex overflow-auto flex-col flex-1 justify-between space-y-4 min-w-0 font-semibold lg:ml-4 md:space-y-0">
                     <div class="flex text-sm leading-tight text-theme-secondary-600 dark:text-theme-secondary-700">{{ $title }}</div>
 
                     <div class="flex items-center space-x-2 leading-tight">
-                        <span class="flex max-w-full pr-1 text-theme-secondary-400 dark:text-theme-secondary-200">
+                        <span class="flex pr-1 max-w-full text-theme-secondary-400 dark:text-theme-secondary-200">
                             <x-truncate-dynamic>{{ $value}}</x-truncate-dynamic>
 
                             <x-clipboard :value="$value" colors="text-theme-secondary-600 hover:text-theme-secondary-400" />
@@ -32,7 +32,7 @@
     </div>
 
     @isset($bottom)
-        <div class="px-8 py-4 border-t rounded-b-xl bg-theme-secondary-100 border-theme-secondary-300 dark:border-theme-secondary-800 dark:bg-theme-secondary-900">
+        <div class="py-4 px-8 rounded-b-xl border-t bg-theme-secondary-100 border-theme-secondary-300 dark:border-theme-secondary-800 dark:bg-theme-secondary-900">
             {{ $bottom }}
         </div>
     @endisset
