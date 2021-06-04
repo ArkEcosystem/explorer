8541eecc2290797b4ec4e45a6fbdaccaca72685c
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-04 15:36:16 -0500

    Update SearchPageTest.php

--- tests/Feature/Http/Livewire/SearchPageTest.php
+++ tests/Feature/Http/Livewire/SearchPageTest.php
@@ -617,7 +617,7 @@ it('should search for transactions by block with an ID', function () {
     Transaction::factory(10)->create();
 
     $tx = Transaction::factory()->create([
-        'block_id' => 'blockid',
+        'block_id' => 'ffff273321907d20bda3278ade259e6364ec2091ecd5993398a2ef2402725a31',
     ]);
 
     Livewire::test(SearchPage::class)
