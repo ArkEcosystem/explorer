4f796a447a1cca9ef3d412b6f0203c2235147213
Highjhacker <jolanbeer@gmail.com>
2021-06-04 19:52:03 +0200

    refactor: multisignature icon

--- resources/views/components/grid/multi-signature-address.blade.php
+++ resources/views/components/grid/multi-signature-address.blade.php
@@ -2,4 +2,7 @@
     :title="trans('general.transaction.multi_signature_address')"
     :transaction="$model"
     :model="$model->multiSignatureWallet()"
-    icon="app-volume" />
+    icon="app.transactions-multi-signature"
+    icon-size="sm"
+    icon-class="ml-2 text-theme-secondary-500 dark:text-theme-secondary-700"
+/>
