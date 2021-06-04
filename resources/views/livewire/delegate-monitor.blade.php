8bd35b937d01f02bf7bd10f51fc177563af6a3c4
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-04 09:31:33 -0500

    Proper skeleton for monitor

--- resources/views/livewire/delegate-monitor.blade.php
+++ resources/views/livewire/delegate-monitor.blade.php
@@ -2,12 +2,10 @@
     @if(! count($delegates))
         <div wire:poll="pollDelegates" wire:key="poll_delegates_skeleton">
             <x-tables.desktop.skeleton.delegates.monitor />
-
-            <x-tables.mobile.skeleton.delegates.monitor />
         </div>
     @else
         <div id="network-list" class="w-full" wire:poll.{{ Network::blockTime() }}s="pollDelegates" wire:key="poll_delegates_real">
-            <div class="flex overflow-hidden flex-col mb-8 rounded-lg border border-theme-secondary-300 dark:border-theme-secondary-800">
+            <div class="flex flex-col mb-8 overflow-hidden border rounded-lg border-theme-secondary-300 dark:border-theme-secondary-800">
                 <div class="p-8 bg-theme-secondary-100 border-theme-secondary-300 dark:border-theme-secondary-800 dark:bg-theme-secondary-900">
                     <div class="grid grid-cols-1 gap-y-8 sm:grid-cols-2 xl:grid-cols-4">
                         <x-general.entity-header-item
@@ -43,8 +41,6 @@
             </div>
 
             <x-tables.desktop.delegates.monitor :delegates="$delegates" :round="$round" />
-
-            <x-tables.mobile.delegates.monitor :delegates="$delegates" />
         </div>
     @endif
 </div>
