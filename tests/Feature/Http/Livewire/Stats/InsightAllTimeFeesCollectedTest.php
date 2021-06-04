ed6df5188af02e1254591f93aacb9cbb593a0e61
Maurizio <maurizio.lepora@gmail.com>
2021-06-04 16:11:52 +0200

    fix: tests

--- tests/Feature/Http/Livewire/Stats/InsightAllTimeFeesCollectedTest.php
+++ tests/Feature/Http/Livewire/Stats/InsightAllTimeFeesCollectedTest.php
@@ -5,18 +5,17 @@ declare(strict_types=1);
 use App\Http\Livewire\Stats\InsightAllTimeFeesCollected;
 use App\Models\Transaction;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Artisan;
 use Livewire\Livewire;
 use function Tests\configureExplorerDatabase;
 
-beforeEach(function (): void {
-    configureExplorerDatabase();
-
-    Carbon::setTestNow('2020-06-15 00:00:00');
-});
+beforeEach(fn () => configureExplorerDatabase());
 
 it('should render the component', function () {
     Transaction::factory(30)->create(['fee' => 12345678910, 'timestamp' => Carbon::createFromTimestamp(Carbon::now()->unix() - 1490101200)->sub('2 years')->unix()]);
     Transaction::factory(30)->create(['fee' => 9234567890, 'timestamp' => Carbon::createFromTimestamp(Carbon::now()->unix() - 1490101200)->unix()]);
+
+    Artisan::call('explorer:cache-fees');
 
     Livewire::test(InsightAllTimeFeesCollected::class)
         ->set('period', 'day')
@@ -24,7 +23,7 @@ it('should render the component', function () {
         ->assertSee('6,474.07 DARK')
         ->assertSee(trans('pages.statistics.insights.fees'))
         ->assertSee('2,770.37 DARK')
-        ->assertSee('["2770.3703670000000000"]');
+        ->assertSee('[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2770.370367]');
 });
 
 it('should filter by year', function () {
@@ -32,11 +31,13 @@ it('should filter by year', function () {
     Transaction::factory(30)->create(['fee' => 12345678910, 'timestamp' => Carbon::createFromTimestamp(Carbon::now()->unix() - 1490101200)->sub('11 months')->unix()]);
     Transaction::factory(30)->create(['fee' => 9234567890, 'timestamp' => Carbon::createFromTimestamp(Carbon::now()->unix() - 1490101200)->unix()]);
 
+    Artisan::call('explorer:cache-fees');
+
     Livewire::test(InsightAllTimeFeesCollected::class)
         ->set('period', 'year')
         ->assertSee(trans('pages.statistics.insights.all-time-fees-collected'))
         ->assertSee('6,544.48 DARK')
         ->assertSee(trans('pages.statistics.insights.fees'))
         ->assertSee('6,474.07 DARK')
-        ->assertSee('["2770.3703670000000000","3703.7036730000000000"]');
+        ->assertSee('[3703.703673,0,0,0,0,0,0,0,0,0,0,2770.370367]');
 });
--- tests/Feature/Http/Livewire/Stats/InsightAllTimeTransactionsTest.php
+++ tests/Feature/Http/Livewire/Stats/InsightAllTimeTransactionsTest.php
@@ -5,18 +5,17 @@ declare(strict_types=1);
 use App\Http\Livewire\Stats\InsightAllTimeTransactions;
 use App\Models\Transaction;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Artisan;
 use Livewire\Livewire;
 use function Tests\configureExplorerDatabase;
 
-beforeEach(function (): void {
-    configureExplorerDatabase();
-
-    Carbon::setTestNow('2021-01-01 00:00:00');
-});
+beforeEach(fn () => configureExplorerDatabase());
 
 it('should render the component', function () {
     Transaction::factory(30)->create(['timestamp' => Carbon::createFromTimestamp(Carbon::now()->unix() - 1490101200)->sub('2 years')->unix()]);
     Transaction::factory(30)->create(['timestamp' => Carbon::createFromTimestamp(Carbon::now()->unix() - 1490101200)->unix()]);
+
+    Artisan::call('explorer:cache-transactions');
 
     Livewire::test(InsightAllTimeTransactions::class)
         ->set('period', 'day')
@@ -24,7 +23,7 @@ it('should render the component', function () {
         ->assertSee('60')
         ->assertSee(trans('pages.statistics.insights.transactions'))
         ->assertSee('30')
-        ->assertSee('[30]');
+        ->assertSee('[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30]');
 });
 
 it('it should filter by year', function () {
@@ -32,11 +31,13 @@ it('it should filter by year', function () {
     Transaction::factory(40)->create(['timestamp' => Carbon::createFromTimestamp(Carbon::now()->unix() - 1490101200)->sub('11 months')->unix()]);
     Transaction::factory(50)->create(['timestamp' => Carbon::createFromTimestamp(Carbon::now()->unix() - 1490101200)->unix()]);
 
+    Artisan::call('explorer:cache-transactions');
+
     Livewire::test(InsightAllTimeTransactions::class)
         ->set('period', 'year')
         ->assertSee(trans('pages.statistics.insights.all-time-transactions'))
         ->assertSee('120')
         ->assertSee(trans('pages.statistics.insights.transactions'))
         ->assertSee('90')
-        ->assertSee('[50,40]');
+        ->assertSee('[40,0,0,0,0,0,0,0,0,0,0,50]');
 });
