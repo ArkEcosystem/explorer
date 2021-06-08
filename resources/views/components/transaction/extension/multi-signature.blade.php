f9b101abd24182a105b907968d53b6bb9af17d05
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-06-08 13:56:03 +0100

    refactor: use h3 instead of all 24px

--- resources/views/components/transaction/extension/multi-signature.blade.php
+++ resources/views/components/transaction/extension/multi-signature.blade.php
@@ -2,9 +2,7 @@
     <x-ark-container>
         <div class="w-full">
             <div class="flex relative justify-between items-end">
-                <h2 class="text-2xl">
-                    @lang('pages.transaction.participants')
-                </h2>
+                <h3>@lang('pages.transaction.participants')</h3>
             </div>
 
             <x-ark-tables.table class="hidden mt-5 md:block">
--- resources/views/livewire/wallet-voter-table.blade.php
+++ resources/views/livewire/wallet-voter-table.blade.php
@@ -1,9 +1,7 @@
 <div id="block-list" class="w-full">
     @if($wallets->isNotEmpty())
         <div class="flex relative justify-between items-center">
-            <h4 class="text-2xl">
-                @lang('pages.voters_by_wallet.subtitle')
-            </h4>
+            <h3>@lang('pages.voters_by_wallet.subtitle')</h3>
         </div>
 
         <x-skeletons.wallets>
