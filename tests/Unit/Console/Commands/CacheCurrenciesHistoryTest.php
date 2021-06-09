b45f59af88980d4227a01874241df95ba97816ad
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-09 12:56:55 -0500

    Tests for the job

--- tests/Unit/Console/Commands/CacheCurrenciesHistoryTest.php
+++ tests/Unit/Console/Commands/CacheCurrenciesHistoryTest.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Bus;
 use App\Jobs\CacheCurrenciesHistory as CacheCurrenciesHistoryJob;
 
-it('should execute the command', function () {
+it('should execute the job', function () {
     Bus::fake();
 
     Config::set('currencies', [
@@ -21,4 +21,38 @@ it('should execute the command', function () {
     $this->artisan('explorer:cache-currencies-history');
 
     Bus::assertDispatched(CacheCurrenciesHistoryJob::class, fn($job) => $job->source === 'DARK' && $job->currency === 'USD');
+});
+
+it('should execute the command with no delay command', function () {
+    Bus::fake();
+
+    Config::set('currencies', [
+        'usd' => [
+            'currency' => 'USD',
+            'locale'   => 'en_US',
+        ],
+    ]);
+
+    Config::set('explorer.networks.development.canBeExchanged', true);
+
+    $this->artisan('explorer:cache-currencies-history --no-delay');
+
+    Bus::assertDispatched(CacheCurrenciesHistoryJob::class, fn($job) => $job->source === 'DARK' && $job->currency === 'USD');
+});
+
+it('should not execute the job if cannot be exchanged', function () {
+    Bus::fake();
+
+    Config::set('currencies', [
+        'usd' => [
+            'currency' => 'USD',
+            'locale'   => 'en_US',
+        ],
+    ]);
+
+    Config::set('explorer.networks.development.canBeExchanged', false);
+
+    $this->artisan('explorer:cache-currencies-history');
+
+    Bus::assertNotDispatched(CacheCurrenciesHistoryJob::class);
 });
