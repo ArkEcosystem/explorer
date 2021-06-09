49b6d7f32da819ad2b29bb7e92cdf1d26ef799a6
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-09 13:32:05 -0500

    Fix test condition

--- tests/Feature/Http/Livewire/PriceStatsTest.php
+++ tests/Feature/Http/Livewire/PriceStatsTest.php
@@ -19,9 +19,9 @@ it('should render the values', function () {
 });
 
 it('should render the placeholder values when no price cached yet', function () {
-    Config::set('explorer.networks.development.canBeExchanged', false);
+    Config::set('explorer.networks.development.canBeExchanged', true);
 
-    (new NetworkStatusBlockCache())->setPrice('DARK', 'USD', 1);
+    (new NetworkStatusBlockCache())->setPrice('DARK', 'USD', null);
     (new NetworkStatusBlockCache())->setHistoricalHourly('DARK', 'USD', collect(['1.898', '1.904', '1.967', '1.941', '2.013', '2.213', '2.414', '2.369', '2.469', '2.374', '2.228', '2.211', '2.266', '2.364', '2.341', '2.269', '1.981', '1.889', '1.275', '1.471', '1.498', '1.518', '1.61', '1.638']));
 
     Livewire::test(PriceStats::class)
@@ -29,7 +29,7 @@ it('should render the placeholder values when no price cached yet', function () 
 });
 
 it('should render the placeholder values when no historial data yet', function () {
-    Config::set('explorer.networks.development.canBeExchanged', false);
+    Config::set('explorer.networks.development.canBeExchanged', true);
 
     (new NetworkStatusBlockCache())->setPrice('DARK', 'USD', 1);
     (new NetworkStatusBlockCache())->setHistoricalHourly('DARK', 'USD', null);
