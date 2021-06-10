5cd5b30b655a0707f148999c9ff5ec9b5f3e5e6b
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-10 16:32:37 -0500

    Adjust tabs between padding

--- resources/views/components/tabs/tab.blade.php
+++ resources/views/components/tabs/tab.blade.php
@@ -4,7 +4,7 @@
 
 <button
     type="button"
-    class="relative px-2 ml-14 cursor-pointer explorer-tab transition-default hover:text-theme-secondary-900 dark:hover:text-theme-secondary-200"
+    class="relative px-2 ml-8 cursor-pointer explorer-tab transition-default hover:text-theme-secondary-900 dark:hover:text-theme-secondary-200"
     @click="select('{{ $name }}')"
     @keydown.enter="select('{{ $name }}')"
     @keydown.space.prevent="select('{{ $name }}')"
@@ -19,7 +19,7 @@
     {{ $attributes }}
 >
     <span
-        class="block pt-4 pb-3 w-full h-full font-semibold whitespace-nowrap border-b-4"
+        class="block w-full h-full pt-4 pb-3 font-semibold border-b-4 whitespace-nowrap"
         :class="{
             'border-transparent dark:text-theme-secondary-500 ': selected !== '{{ $name }}',
             'text-theme-secondary-900 border-theme-primary-600 dark:text-theme-secondary-200': selected === '{{ $name }}',
