866b2efed9cbc28d1e288a00df2c398b8fe67d40
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-18 10:53:56 -0500

    Rename to generateHashId

--- app/Services/Helpers.php
+++ app/Services/Helpers.php
@@ -10,7 +10,7 @@ final class Helpers
      * Blends multiple parameters and creates an unique 10 chars string.
      * Useful for wire:keys.
      */
-    public static function blend(int | bool | string | null ...$params): string
+    public static function generateHashId(int | bool | string | null ...$params): string
     {
         return substr(md5(implode('*', $params)), 0, 10);
     }
