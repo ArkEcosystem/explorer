3c7f908de06709a24a886a29e0d46d0e35cc63f1
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-06-18 11:33:32 -0500

    typo

--- resources/views/components/tables/mobile/transactions.blade.php
+++ resources/views/components/tables/mobile/transactions.blade.php
@@ -11,7 +11,7 @@
 
 <div class="divide-y table-list-mobile">
     @foreach ($transactions as $transaction)
-        <div class="table-list-mobile-row" wire:key="{{ Helpers::generateId('mobile', $transaction->id(), Settings::curency(), ...$params) }}">
+        <div class="table-list-mobile-row" wire:key="{{ Helpers::generateId('mobile', $transaction->id(), Settings::currency(), ...$params) }}">
             <x-tables.rows.mobile.transaction-id :model="$transaction" />
 
             <x-tables.rows.mobile.timestamp :model="$transaction" />
