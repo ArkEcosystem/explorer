c087cc4cd1f23a72d2ccdc0ac3884d1470e1afcc
leMaur <leMaur@users.noreply.github.com>
2021-06-23 14:03:39 +0000

    style: resolve style guide violations

--- tests/Feature/Http/Livewire/Stats/InsightAllTimeFeesCollectedTest.php
+++ tests/Feature/Http/Livewire/Stats/InsightAllTimeFeesCollectedTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use App\Http\Livewire\Stats\InsightAllTimeFeesCollected;
 use App\Models\Transaction;
 use App\Services\Timestamp;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Artisan;
 use Livewire\Livewire;
 use function Tests\configureExplorerDatabase;
