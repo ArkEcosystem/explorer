a1b3f0d486d0eed68f283ebdb5e9d11a8e44e25f
alfonsobries <alfonsobries@users.noreply.github.com>
2021-06-28 23:17:07 +0000

    style: resolve style guide violations

--- resources/views/livewire/transaction-table.blade.php
+++ resources/views/livewire/transaction-table.blade.php
@@ -1,10 +1,10 @@
 <div>
     @isset($showTitle)
-        <div class="w-full mb-3">
-            <div class="relative flex flex-col justify-between md:flex-row md:items-end">
+        <div class="mb-3 w-full">
+            <div class="flex relative flex-col justify-between md:flex-row md:items-end">
                 <h1 class="mb-8 md:mb-0">@lang('pages.transactions.title')</h1>
 
-                <div class="mb-2 -my-3 md:-mr-8 md:mb-0">
+                <div class="-my-3 mb-2 md:mb-0 md:-mr-8">
                     <x-transaction-table-filter />
                 </div>
             </div>
