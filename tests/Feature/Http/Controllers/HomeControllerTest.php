1ebad82b58cc94b8bcfe7d74c7ece3591ef4b6b7
ItsANameToo <itsanametoo@protonmail.com>
2021-06-29 16:50:40 +0200

    wip

--- tests/Feature/Http/Controllers/HomeControllerTest.php
+++ tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+use function Tests\configureExplorerDatabase;
+
 it('should render the page without any errors', function () {
+    configureExplorerDatabase();
     $this
         ->get(route('home'))
         ->assertOk();
