7e1ed6be3dd0b84986d8340ae4ebd4922f5324ac
Maurizio <maurizio.lepora@gmail.com>
2021-06-29 16:50:42 +0200

    fix: test

--- tests/Feature/Http/Controllers/HomeControllerTest.php
+++ tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
+use function Tests\configureExplorerDatabase;
+
 it('should render the page without any errors', function () {
+    configureExplorerDatabase();
+
     $this
         ->get(route('home'))
         ->assertOk();
