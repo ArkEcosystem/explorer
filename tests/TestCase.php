a76c52af22779c68b17f03a2abeeb2d25624ff39
alfonsobries <alfonsobries@gmail.com>
2021-07-23 13:01:23 -0500

    remove configureExplorerDatabase in favour of regular database refreshing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,45 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    /**
+     * The parameters that should be used when running "migrate:fresh".
+     *
+     * @return array
+     */
+    protected function migrateFreshUsing()
+    {
+        $seeder = $this->seeder();
+
+        return array_merge(
+            [
+                '--database' => 'explorer',
+                '--path'     => 'tests/migrations',
+                '--drop-views' => $this->shouldDropViews(),
+                '--drop-types' => $this->shouldDropTypes(),
+            ],
+            $seeder ? ['--seeder' => $seeder] : ['--seed' => $this->shouldSeed()]
+        );
+    }
+
+    /**
+     * The parameters that should be used when running "migrate".
+     *
+     * @return array
+     */
+    protected function migrateUsing()
+    {
+        return [
+            '--database' => 'explorer',
+            '--path'     => 'tests/migrations',
+            '--seed' => $this->shouldSeed(),
+        ];
+    }
 }
