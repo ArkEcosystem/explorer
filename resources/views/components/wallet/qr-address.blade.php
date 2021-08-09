5d9376550107cb769d8e5339d87dbfe7065567b6
alfonsobries <alfonsobries@gmail.com>
2021-08-09 12:25:34 -0500

    Increase length for some truncate middle components

--- resources/views/components/wallet/qr-address.blade.php
+++ resources/views/components/wallet/qr-address.blade.php
@@ -4,19 +4,19 @@
             <x-general.avatar :identifier="$model->address()" no-shrink />
         </div>
 
-        <div class="flex flex-col flex-1 justify-center mr-4 sm:mr-0">
+        <div class="flex flex-col justify-center flex-1 mr-4 sm:mr-0">
             <div class="text-sm font-semibold text-theme-secondary-500">@lang('general.address')</div>
 
-            <div class="flex justify-between items-center sm:justify-start">
+            <div class="flex items-center justify-between sm:justify-start">
                 <div>
                     <div class="hidden font-semibold sm:flex text-theme-secondary-900 dark:text-theme-secondary-200">
-                        <x-truncate-middle :length="19">
+                        <x-truncate-middle :length="21">
                             {{ $model->address() }}
                         </x-truncate-middle>
                     </div>
 
                     <div class="font-semibold sm:hidden text-theme-secondary-900 dark:text-theme-secondary-200">
-                        <x-truncate-middle :length="15">
+                        <x-truncate-middle :length="17">
                             {{ $model->address() }}
                         </x-truncate-middle>
                     </div>
