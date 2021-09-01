0bd46e5ef1814ce05e3f8b1d9914754f1ead83ed
alfonsobries <alfonsobries@gmail.com>
2021-09-01 10:19:45 -0500

    Add border to the inner elements

--- resources/views/components/general/entity-header.blade.php
+++ resources/views/components/general/entity-header.blade.php
@@ -1,16 +1,16 @@
-<div class="flex overflow-hidden flex-col rounded-xl border-2 border-theme-secondary-300 dark:border-theme-secondary-800">
-    <div class="{{ $padding ?? 'px-8 py-6' }} bg-theme-secondary-900 lg:relative">
-        <div class="flex overflow-auto flex-col justify-between space-y-8 lg:flex-row lg:space-y-0">
+<div class="flex flex-col rounded-xl ">
+    <div class="{{ $padding ?? 'px-8 py-6' }} bg-theme-secondary-900 dark:border-theme-secondary-800 border-theme-secondary-300 @if (isset($bottom)) rounded-t-xl border-t-2 border-l-2 border-r-2 @else rounded-xl border-2  @endif lg:relative">
+        <div class="flex flex-col justify-between space-y-8 overflow-auto lg:flex-row lg:space-y-0">
             <div class="flex overflow-auto md:space-x-4">
-                <div class="hidden items-center md:flex">
+                <div class="items-center hidden md:flex">
                     {!! $logo !!}
                 </div>
 
-                <div class="flex overflow-auto flex-col flex-1 justify-center space-y-2 min-w-0 font-semibold lg:ml-4">
+                <div class="flex flex-col justify-center flex-1 min-w-0 space-y-2 overflow-auto font-semibold lg:ml-4">
                     <div class="flex text-sm leading-tight text-theme-secondary-600 dark:text-theme-secondary-700">{{ $title }}</div>
 
                     <div class="flex items-center space-x-2 leading-tight">
-                        <span class="flex pr-1 max-w-full text-theme-secondary-400 dark:text-theme-secondary-200">
+                        <span class="flex max-w-full pr-1 text-theme-secondary-400 dark:text-theme-secondary-200">
                             <x-truncate-dynamic>{{ $value}}</x-truncate-dynamic>
 
                             <x-clipboard :value="$value" colors="text-theme-secondary-600 hover:text-theme-secondary-400" />
@@ -32,7 +32,7 @@
     </div>
 
     @isset($bottom)
-        <div class="py-4 px-8 rounded-b-xl border-t bg-theme-secondary-100 border-theme-secondary-300 dark:border-theme-secondary-800 dark:bg-theme-secondary-900">
+        <div class="px-8 py-4 border-t border-b-2 border-l-2 border-r-2 rounded-b-xl dark:border-theme-secondary-800 border-theme-secondary-300 bg-theme-secondary-100 dark:bg-theme-secondary-900">
             {{ $bottom }}
         </div>
     @endisset
--- resources/views/components/page-headers/wallet/actions/public-key.blade.php
+++ resources/views/components/page-headers/wallet/actions/public-key.blade.php
@@ -1,10 +1,10 @@
-<div x-data="{ publicKeyModalVisible: false }" class="flex-1 mt-4 w-full sm:mt-0 sm:w-auto">
+<div x-data="{ publicKeyModalVisible: false }" class="flex-1 w-full mt-4 sm:mt-0 sm:w-auto">
     <button type="button" @click="publicKeyModalVisible = !publicKeyModalVisible"
-        class="flex justify-center items-center px-3 w-full h-11 rounded cursor-pointer lg:flex-none lg:w-16 bg-theme-secondary-800 transition-default dark:text-theme-secondary-600 dark:hover:text-theme-secondary-200 hover:bg-theme-primary-600">
+        class="flex items-center justify-center w-full px-3 rounded cursor-pointer h-11 lg:flex-none lg:w-16 bg-theme-secondary-800 transition-default dark:text-theme-secondary-600 dark:hover:text-theme-secondary-200 hover:bg-theme-primary-600">
         <x-ark-icon name="key" />
     </button>
 
-    <div @click.away="publicKeyModalVisible = false" x-show="publicKeyModalVisible" class="absolute right-0 left-0 mx-8 mt-4 w-auto bg-white rounded-xl shadow-lg lg:left-auto lg:mx-0 lg:mt-1 lg:mr-32 z-15 dark:shadow-lg-dark dark:bg-theme-secondary-900" x-cloak>
+    <div @click.away="publicKeyModalVisible = false" x-show="publicKeyModalVisible" class="absolute left-0 right-0 w-auto mx-8 mt-4 bg-white shadow-lg rounded-xl lg:left-auto lg:mx-0 lg:mt-1 lg:mr-32 z-15 dark:shadow-lg-dark dark:bg-theme-secondary-900" x-cloak>
         <div class="flex flex-col p-6 space-y-2 leading-tight">
             <span class="text-sm font-semibold text-theme-secondary-400 dark:text-theme-secondary-700">@lang('pages.wallet.public_key.title')</span>
             <span class="flex font-semibold link">
