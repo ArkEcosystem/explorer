3d4581799bacd05acee056f6c2a1e425eb76a5ad
alfonsobries <alfonsobries@gmail.com>
2021-09-09 11:00:10 -0500

    Rename test WIP

--- tests/Unit/Services/CryptoCompareTest.php
+++ tests/Unit/Services/CryptoCompareTest.php
@@ -11,7 +11,7 @@ use function Tests\fakeCryptoCompare;
 it('should fetch the price data for the given collection', function () {
     fakeCryptoCompare();
 
-    expect(CryptoCompare::getCurrenciesData('ARK', collect(['USD'])))->toEqual(collect([
+    expect((new CryptoCompare)->getCurrenciesData('ARK', collect(['USD'])))->toEqual(collect([
         'USD' => [
             'priceChange' => 0.14989143413680925,
             'price'       => 1.2219981765,
@@ -24,7 +24,7 @@ it('should fetch the historical prices for the given pair', function () {
         'cryptocompare.com/*' => Http::response(json_decode(file_get_contents(base_path('tests/fixtures/cryptocompare/historical.json')), true)),
     ]);
 
-    assertMatchesSnapshot(CryptoCompare::historical('ARK', 'USD'));
+    assertMatchesSnapshot((new CryptoCompare)->historical('ARK', 'USD'));
 });
 
 it('should fetch the historical prices per hour for the given pair', function () {
@@ -32,5 +32,5 @@ it('should fetch the historical prices per hour for the given pair', function ()
         'cryptocompare.com/*' => Http::response(json_decode(file_get_contents(base_path('tests/fixtures/cryptocompare/histohour.json')), true)),
     ]);
 
-    assertMatchesSnapshot(CryptoCompare::historicalHourly('ARK', 'USD'));
+    assertMatchesSnapshot((new CryptoCompare)->historicalHourly('ARK', 'USD'));
 });
