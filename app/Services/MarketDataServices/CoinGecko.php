d915ae47e34454903288442e6e1ee911b7a724c2
alfonsobries <alfonsobries@gmail.com>
2021-09-10 15:59:45 -0500

    No need for coingecko library

--- app/Services/MarketDataServices/CoinGecko.php
+++ app/Services/MarketDataServices/CoinGecko.php
@@ -8,9 +8,9 @@ use App\Contracts\MarketDataService;
 use App\Facades\Network;
 use App\Services\Cache\CryptoDataCache;
 use Carbon\Carbon;
-use Codenixsv\CoinGeckoApi\CoinGeckoClient;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
 final class CoinGecko implements MarketDataService
@@ -18,15 +18,16 @@ final class CoinGecko implements MarketDataService
     public function historical(string $source, string $target, string $format = 'Y-m-d'): Collection
     {
         return (new CryptoDataCache())->setHistorical($source, $target, $format, function () use ($source, $target, $format): Collection {
-            $client = new CoinGeckoClient();
-
             $params = [
                 'vs_currency' => Str::lower($target),
                 'days'        => Network::epoch()->diffInDays(),
                 'interval'    => 'daily',
             ];
 
-            $data = $client->coins()->get('/coins/'.Str::lower($source).'/market_chart', $params);
+            $data = Http::get(
+                'https://api.coingecko.com/api/v3/coins/'.Str::lower($source).'/market_chart',
+                $params
+            )->json();
 
             return collect($data['prices'])
                 ->mapWithKeys(fn ($item) => [Carbon::createFromTimestampMs($item[0])->format($format) => $item[1]]);
@@ -36,13 +37,15 @@ final class CoinGecko implements MarketDataService
     public function historicalHourly(string $source, string $target, int $limit = 23, string $format = 'Y-m-d H:i:s'): Collection
     {
         return (new CryptoDataCache())->setHistoricalHourly($source, $target, $format, $limit, function () use ($source, $target, $format, $limit): Collection {
-            $client = new CoinGeckoClient();
+            $params = [
+                'vs_currency' => Str::lower($target),
+                'days'        => strval(ceil($limit / 24)),
+            ];
 
-            $data = $client->coins()->getMarketChart(
-                Str::lower($source),
-                Str::lower($target),
-                strval(ceil($limit / 24))
-            );
+            $data = Http::get(
+                'https://api.coingecko.com/api/v3/coins/'.Str::lower($source).'/market_chart',
+                $params
+            )->json();
 
             return collect($data['prices'])
                 ->groupBy(fn ($item) => Carbon::createFromTimestampMsUTC($item[0])->format('Y-m-d H:').'00:00')
@@ -57,9 +60,7 @@ final class CoinGecko implements MarketDataService
 
     public function getCurrenciesData(string $source, Collection $targets): Collection
     {
-        $client = new CoinGeckoClient();
-
-        $data = $client->coins()->getCoin(Str::lower($source));
+        $data = Http::get('https://api.coingecko.com/api/v3/coins/'.Str::lower($source))->json();
 
         return $targets->mapWithKeys(fn (string $currency) => [strtoupper($currency) => [
             'price' => Arr::get($data, 'market_data.current_price.' . Str::lower($currency)),
