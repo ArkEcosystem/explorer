5cbaeeb7779326e61d6d6beb86e3b9980f2fab29
ItsANameToo <itsanametoo@protonmail.com>
2021-09-16 11:48:19 +0200

    wip

--- tests/Unit/Services/MarketDataProviders/CoinGeckoTest.php
+++ tests/Unit/Services/MarketDataProviders/CoinGeckoTest.php
@@ -8,7 +8,7 @@ use function Spatie\Snapshots\assertMatchesSnapshot;
 
 it('should fetch the price data for the given collection', function () {
     Http::fake([
-        'api.coingecko.com/*'     => Http::response(json_decode(file_get_contents(base_path('tests/fixtures/coingecko/coin.json')), true), 200),
+        'api.coingecko.com/*' => Http::response(json_decode(file_get_contents(base_path('tests/fixtures/coingecko/coin.json')), true), 200),
     ]);
 
     $dto = (new CoinGecko())->priceAndPriceChange('ARK', collect(['USD']))->get('USD');
@@ -18,7 +18,7 @@ it('should fetch the price data for the given collection', function () {
 
 it('should fetch the historical prices for the given pair', function () {
     Http::fake([
-        'api.coingecko.com/*'     => Http::response(json_decode(file_get_contents(base_path('tests/fixtures/coingecko/market_data.json')), true), 200),
+        'api.coingecko.com/*' => Http::response(json_decode(file_get_contents(base_path('tests/fixtures/coingecko/market_data.json')), true), 200),
     ]);
 
     assertMatchesSnapshot((new CoinGecko())->historical('ARK', 'USD'));
@@ -26,7 +26,7 @@ it('should fetch the historical prices for the given pair', function () {
 
 it('should fetch the historical prices per hour for the given pair', function () {
     Http::fake([
-        'api.coingecko.com/*'     => Http::response(json_decode(file_get_contents(base_path('tests/fixtures/coingecko/market_data_1_day.json')), true), 200),
+        'api.coingecko.com/*' => Http::response(json_decode(file_get_contents(base_path('tests/fixtures/coingecko/market_data_1_day.json')), true), 200),
     ]);
 
     assertMatchesSnapshot((new CoinGecko())->historicalHourly('ARK', 'USD'));
