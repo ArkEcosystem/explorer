a6ae8a4259bde904ad409bda2de9dd1365e037f6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-09-21 20:39:11 +0100

    method to check tx view model

--- app/ViewModels/Concerns/Transaction/HasDirection.php
+++ app/ViewModels/Concerns/Transaction/HasDirection.php
@@ -4,11 +4,27 @@ declare(strict_types=1);
 
 namespace App\ViewModels\Concerns\Transaction;
 
+use Illuminate\Support\Arr;
+
 trait HasDirection
 {
     public function isSent(string $address): bool
     {
         return $this->direction->isSent($address);
+    }
+
+    public function isSentToSelf(): bool
+    {
+        if ($this->isTransfer() && $this->sender()->address === $this->recipient()->address) {
+            return true;
+        }
+
+        return collect(Arr::get($this->transaction->asset ?? [], 'payments', []))
+            ->some(function ($payment): bool {
+                $sender = $this->sender();
+
+                return $sender !== null && $sender->address === $payment['recipientId'];
+            });
     }
 
     public function isReceived(string $address): bool
