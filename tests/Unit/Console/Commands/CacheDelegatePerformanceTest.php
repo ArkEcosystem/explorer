f6e6875d1310cd1b80c62225f83c959fe0b55971
alfonsobries <alfonsobries@gmail.com>
2021-09-27 12:50:56 -0500

    Add test that used to be part of the job

--- tests/Unit/Console/Commands/CacheDelegatePerformanceTest.php
+++ tests/Unit/Console/Commands/CacheDelegatePerformanceTest.php
@@ -3,30 +3,20 @@
 declare(strict_types=1);
 
 use App\Console\Commands\CacheDelegatePerformance;
-// use App\Jobs\CachePastRoundPerformanceByPublicKey;
 use App\Models\Round;
-use App\Models\Wallet;
-use Illuminate\Support\Facades\Queue;
 use App\Facades\Network;
 use App\Models\Block;
 use App\Services\Cache\WalletCache;
 use Illuminate\Support\Facades\Cache;
 
-// it('should execute the command', function () {
-//     Queue::fake();
-
-//     Wallet::factory(51)->create()->each(function () {
-//         Round::factory()->create(['round' => '112168']);
-//     });
-
-//     (new CacheDelegatePerformance())->handle();
-
-//     Queue::assertPushed(CachePastRoundPerformanceByPublicKey::class, 51);
-// });
-
-
 it('should cache the past performance for a public key', function () {
     $publicKey = 'generator';
+
+    Round::factory()->create([
+        'round' => '16',
+        'public_key' => $publicKey,
+    ]);
+
     foreach (range(10, 14) as $round) {
         Block::factory()->create([
             'generator_public_key' => $publicKey,
@@ -39,8 +29,6 @@ it('should cache the past performance for a public key', function () {
 
     (new CacheDelegatePerformance())->handle();
 
-    // (new CachePastRoundPerformanceByPublicKey(16, $publicKey))->handle();
-
     expect(Cache::tags('wallet')->has(md5("performance/$publicKey")))->toBeTrue();
     expect((new WalletCache())->getPerformance($publicKey))->toBe([
         true,
@@ -51,26 +39,32 @@ it('should cache the past performance for a public key', function () {
     ]);
 });
 
-// it('should cache end of a round missed blocks for a public key ', function () {
-//     $publicKey = 'generator';
-//     foreach (range(10, 13) as $round) {
-//         Block::factory()->create([
-//             'generator_public_key' => $publicKey,
-//             'height'               => $round * Network::delegateCount(),
-//         ]);
-//     }
+it('should cache end of a round missed blocks for a public key ', function () {
+    $publicKey = 'generator';
 
-//     expect(Block::whereGeneratorPublicKey($publicKey)->count())->toBe(4);
-//     expect(Cache::tags('wallet')->has(md5("performance/$publicKey")))->toBeFalse();
+    Round::factory()->create([
+        'round' => '16',
+        'public_key' => $publicKey,
+    ]);
 
-//     (new CachePastRoundPerformanceByPublicKey(16, $publicKey))->handle();
+    foreach (range(10, 13) as $round) {
+        Block::factory()->create([
+            'generator_public_key' => $publicKey,
+            'height'               => $round * Network::delegateCount(),
+        ]);
+    }
 
-//     expect(Cache::tags('wallet')->has(md5("performance/$publicKey")))->toBeTrue();
-//     expect((new WalletCache())->getPerformance($publicKey))->toBe([
-//         true,
-//         true,
-//         true,
-//         true,
-//         false,
-//     ]);
-// });
+    expect(Block::whereGeneratorPublicKey($publicKey)->count())->toBe(4);
+    expect(Cache::tags('wallet')->has(md5("performance/$publicKey")))->toBeFalse();
+
+    (new CacheDelegatePerformance())->handle();
+
+    expect(Cache::tags('wallet')->has(md5("performance/$publicKey")))->toBeTrue();
+    expect((new WalletCache())->getPerformance($publicKey))->toBe([
+        true,
+        true,
+        true,
+        true,
+        false,
+    ]);
+});
