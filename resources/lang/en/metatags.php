611838bd35c4897923f1a7414ee88e3b83b648f2
Maurizio <maurizio.lepora@gmail.com>
2021-09-28 10:22:19 +0200

    fix

--- resources/lang/en/metatags.php
+++ resources/lang/en/metatags.php
@@ -30,7 +30,7 @@ return [
 
     'transaction' => [
         'title'       => ':txid Transaction Details - :name Blockchain Explorer',
-        'description' => 'View information and details for transaction with ID :txid on :name Blockchain.',
+        'description' => 'View information and details for transaction with ID :txid on the :name Blockchain.',
         'image'       => asset('images/metadata/explorer-preview.png'),
     ],
 
@@ -42,7 +42,7 @@ return [
 
     'block' => [
         'title'       => ':blockID Block Details - :name Blockchain Explorer',
-        'description' => 'View information and details for block with ID :blockID on :name Blockchain.',
+        'description' => 'View information and details for block with ID :blockID on the :name Blockchain.',
         'image'       => asset('images/metadata/explorer-preview.png'),
     ],
 
@@ -54,25 +54,25 @@ return [
 
     'wallet' => [
         'title'       => ':address Wallet Details - :name Blockchain Explorer',
-        'description' => 'View balance, transaction history, and other details for :address on :name Blockchain.',
+        'description' => 'View balance, transaction history, and other details for :address on the :name Blockchain.',
         'image'       => asset('images/metadata/explorer-preview.png'),
     ],
 
     'wallet-voters' => [
         'title'       => 'Voters of :address Wallet - :name Blockchain Explorer',
-        'description' => 'View all voters of :address wallet on :name Blockchain.',
+        'description' => 'View all voters of :address wallet on the :name Blockchain.',
         'image'       => asset('images/metadata/explorer-preview.png'),
     ],
 
     'wallet-blocks' => [
         'title'       => 'Blocks Validated by :address Wallet - :name Blockchain Explorer',
-        'description' => 'View blocks validated by :address wallet on :name Blockchain.',
+        'description' => 'View blocks validated by :address wallet on the :name Blockchain.',
         'image'       => asset('images/metadata/explorer-preview.png'),
     ],
 
     'search' => [
         'title'       => 'Search Results for :searchTerm - :name Blockchain Explorer',
-        'description' => 'View search results for :searchTerm on :name Blockchain.',
+        'description' => 'View search results for :searchTerm on the :name Blockchain.',
         'image'       => asset('images/metadata/explorer-preview.png'),
     ],
 
