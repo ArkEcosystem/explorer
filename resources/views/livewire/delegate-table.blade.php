fb5d603b18b3bccd2b9b0a8fef28e98eebfc85fd
Maurizio <maurizio.lepora@gmail.com>
2021-10-01 12:14:49 +0200

    fix loading state

--- resources/views/livewire/delegate-table.blade.php
+++ resources/views/livewire/delegate-table.blade.php
@@ -1,27 +1,53 @@
 <div id="delegate-list" class="w-full">
+    <span x-show="selected !== 'active'" x-cloak>
+        <x-loading.visible>
+            <span x-show="selected === 'active'" x-cloak>
+                <x-tables.desktop.skeleton.delegates.active />
+            </span>
 
-    <x-delegates.table-loading-state />
+            <span x-show="selected === 'standby'" x-cloak>
+                <x-tables.desktop.skeleton.delegates.standby />
+            </span>
 
-    <x-loading.hidden>
-        @switch($this->state['status'])
-            @case('standby')
-                <x-tables.desktop.delegates.standby :delegates="$delegates" />
-                <x-general.pagination :results="$delegates" class="mt-8" />
-                @break
+            <span x-show="selected === 'resigned'" x-cloak>
+                <x-tables.desktop.skeleton.delegates.resigned />
+            </span>
+        </x-loading.visible>
+    </span>
 
-            @case('resigned')
-                <x-tables.desktop.delegates.resigned :delegates="$delegates" />
-                <x-general.pagination :results="$delegates" class="mt-8" />
-                @break
-
-            @default
-                @if($delegates->isEmpty())
-                    <x-tables.desktop.skeleton.delegates.active />
-                @else
+    @if($this->state['status'] === 'active')
+        <div wire:poll.{{ Network::blockTime() }}s wire:key="poll_active_delegates_skeleton">
+            @if (count($delegates) && $state['status'] === 'active')
+                <span x-show="selected === 'active'">
                     <x-tables.desktop.delegates.active :delegates="$delegates" />
-                @endif
-        @endswitch
-    </x-loading.hidden>
+                </span>
+            @else
+                <x-loading.hidden>
+                    <x-tables.desktop.delegates.active :delegates="$delegates" />
+                </x-loading.hidden>
+            @endif
+        </div>
+    @elseif (! count($delegates) || $state['status'] !== 'active')
+        <span x-show="selected === 'active'" x-cloak>
+            <x-tables.desktop.skeleton.delegates.active />
+        </span>
+    @endif
+
+    @if($this->state['status'] === 'standby')
+        <x-loading.hidden>
+            <x-tables.desktop.delegates.standby :delegates="$delegates" />
+
+            <x-general.pagination :results="$delegates" class="mt-8" />
+        </x-loading.hidden>
+    @endif
+
+    @if($this->state['status'] === 'resigned')
+        <x-loading.hidden>
+            <x-tables.desktop.delegates.resigned :delegates="$delegates" />
+
+            <x-general.pagination :results="$delegates" class="mt-8" />
+        </x-loading.hidden>
+    @endif
 
     <script>
         window.addEventListener('livewire:load', () => window.livewire.on('pageChanged', () => scrollToQuery('#delegate-list')));
