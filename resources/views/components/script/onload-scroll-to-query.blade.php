c0ee2ca99295050456591f4289b5203d4f5cb1f5
Maurizio <maurizio.lepora@gmail.com>
2021-10-06 10:27:53 +0200

    wip

--- resources/views/components/script/onload-scroll-to-query.blade.php
+++ resources/views/components/script/onload-scroll-to-query.blade.php
@@ -1,3 +1,5 @@
+@props(['selector'])
+
 <script type="text/javascript">
     document.addEventListener('livewire:load', () => window.livewire.on('pageChanged', () => scrollToQuery($selector)));
 </script>
