565ce874b1d4afb9dd8f7cb574cabae7c92b9ea3
Maurizio <maurizio.lepora@gmail.com>
2021-10-14 17:14:23 +0200

    wip

--- resources/views/livewire/delegate-active-table.blade.php
+++ resources/views/livewire/delegate-active-table.blade.php
@@ -1,11 +1,9 @@
 <div class="w-full">
-    <x-loading.hidden>
-        @if (! count($delegates))
-            <x-tables.desktop.skeleton.delegates.active />
-        @else
-            <div wire:poll.{{ Network::blockTime() }}s wire:key="poll_active_delegates">
-                <x-tables.desktop.delegates.active :delegates="$delegates" />
-            </div>
-        @endif
-    </x-loading.hidden>
+    @if (! count($delegates))
+        <x-tables.desktop.skeleton.delegates.active />
+    @else
+        <div wire:poll.{{ Network::blockTime() }}s wire:key="poll_active_delegates">
+            <x-tables.desktop.delegates.active :delegates="$delegates" />
+        </div>
+    @endif
 </div>
