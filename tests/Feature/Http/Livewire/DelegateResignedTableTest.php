fe5770db9b183bc024eb53feb480aca8490ceba2
Maurizio <maurizio.lepora@gmail.com>
2021-10-14 16:44:07 +0200

    fix tests

---
+++ tests/Feature/Http/Livewire/DelegateResignedTableTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Livewire\DelegateResignedTable;
+use App\Models\Wallet;
+use Livewire\Livewire;
+
+it('should render the component without data', function (): void {
+    Livewire::test(DelegateResignedTable::class)
+        ->assertSet('load', false)
+        ->emit('tabFiltered', 'active')
+        ->assertSet('load', false);
+});
+
+it('should render the component with data', function (): void {
+    Livewire::test(DelegateResignedTable::class)
+        ->assertSet('load', false)
+        ->emit('tabFiltered', 'resigned')
+        ->assertSet('load', true);
+});
