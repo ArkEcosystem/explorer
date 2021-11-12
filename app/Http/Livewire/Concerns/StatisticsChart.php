374c2be899353e42d57d1181a4df93d6f0859f31
Highjhacker <jolanbeer@gmail.com>
2021-11-12 16:24:16 +0100

    chore: phpstan

--- app/Http/Livewire/Concerns/StatisticsChart.php
+++ app/Http/Livewire/Concerns/StatisticsChart.php
@@ -36,7 +36,9 @@ trait StatisticsChart
 
     private function transactionsPerPeriod(string $cache, string $period): Collection
     {
-        /* @var FeeCache|TransactionCache $cache */
+        /**
+         * @var FeeCache|TransactionCache $cache
+         */
         return collect((new $cache())->getHistorical($period));
     }
 }
