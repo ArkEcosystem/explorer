0a390b2872479f0781667fcb2626b0661e4cddb9
Maurizio <maurizio.lepora@gmail.com>
2021-11-15 10:48:10 +0100

    fix InteractsWithEntities.php

--- app/ViewModels/Concerns/Transaction/InteractsWithEntities.php
+++ app/ViewModels/Concerns/Transaction/InteractsWithEntities.php
@@ -10,27 +10,27 @@ use Illuminate\Support\Arr;
 
 trait InteractsWithEntities
 {
-    public function entityType(): ?string
+    public function entityType(): string|null
     {
         return (new TransactionTypeSlug($this->transaction))->exact();
     }
 
-    public function entityName(): ?string
+    public function entityName(): string|null
     {
         return $this->entityProperty('asset.data.name');
     }
 
-    public function entityCategory(): ?string
+    public function entityCategory(): string|null
     {
         return null;
     }
 
-    public function entityHash(): ?string
+    public function entityHash(): string|null
     {
         return $this->entityProperty('asset.data.ipfsData');
     }
 
-    private function entityProperty(string $property): ?string
+    private function entityProperty(string $property): string|null
     {
         $transaction = $this->transaction;
 
