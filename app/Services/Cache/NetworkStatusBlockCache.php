a3aca3374d965d877701d3ebefd3b5b5504fdb2d
Maurizio <maurizio.lepora@gmail.com>
2021-11-15 10:49:17 +0100

    fix NetworkStatusBlockCache.php

--- app/Services/Cache/NetworkStatusBlockCache.php
+++ app/Services/Cache/NetworkStatusBlockCache.php
@@ -14,40 +14,40 @@ final class NetworkStatusBlockCache implements Contract
 {
     use ManagesCache;
 
-    public function getHistoricalHourly(string $source, string $target): ?Collection
+    public function getHistoricalHourly(string $source, string $target): Collection|null
     {
         return $this->get(sprintf('historical-hourly/%s/%s', $source, $target));
     }
 
-    public function setHistoricalHourly(string $source, string $target, ?Collection $historical): ?Collection
+    public function setHistoricalHourly(string $source, string $target, ?Collection $historical): Collection|null
     {
         $this->put(sprintf('historical-hourly/%s/%s', $source, $target), $historical);
 
         return $historical;
     }
 
-    public function getPrice(string $source, string $target): ?float
+    public function getPrice(string $source, string $target): float|null
     {
         $price = $this->get(sprintf('price/%s/%s', $source, $target));
 
         return $price === null ? null : (float) $price;
     }
 
-    public function setPrice(string $source, string $target, ?float $price): ?float
+    public function setPrice(string $source, string $target, ?float $price): float|null
     {
         $this->put(sprintf('price/%s/%s', $source, $target), $price);
 
         return $price;
     }
 
-    public function getPriceChange(string $source, string $target): ?float
+    public function getPriceChange(string $source, string $target): float|null
     {
         $priceChange = $this->get(sprintf('pricechange/%s/%s', $source, $target));
 
         return $priceChange === null ? null : (float) $priceChange;
     }
 
-    public function setPriceChange(string $source, string $target, ?float $priceChange): ?float
+    public function setPriceChange(string $source, string $target, ?float $priceChange): float|null
     {
         $this->put(sprintf('pricechange/%s/%s', $source, $target), $priceChange);
 
