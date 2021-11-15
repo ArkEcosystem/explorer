d1f4622582655c9a13864b23a7979bb3c08d5951
Maurizio <maurizio.lepora@gmail.com>
2021-11-15 10:48:02 +0100

    fix InteractsWithDelegateRegistration.php

--- app/ViewModels/Concerns/Transaction/InteractsWithDelegateRegistration.php
+++ app/ViewModels/Concerns/Transaction/InteractsWithDelegateRegistration.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Arr;
 
 trait InteractsWithDelegateRegistration
 {
-    public function delegateUsername(): ?string
+    public function delegateUsername(): string|null
     {
         if (! $this->isDelegateRegistration()) {
             return null;
