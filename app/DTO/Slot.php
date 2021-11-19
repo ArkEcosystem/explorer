ce4406c48f1a0b99c5f3999d4d269342499dcd25
leMaur <leMaur@users.noreply.github.com>
2021-11-19 10:04:28 +0000

    style: resolve style guide violations

--- app/DTO/Slot.php
+++ app/DTO/Slot.php
@@ -34,8 +34,7 @@ final class Slot
         private string $status,
         private Collection $roundBlocks,
         private int $roundNumber
-    )
-    {
+    ) {
         $this->currentRoundBlocks = $this->roundBlocks
             ->where('generator_public_key', $this->publicKey)
             ->count();
