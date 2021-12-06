2b1cccf17eb5eb5641f78460a912ac7d8bf98f10
Maurizio <maurizio.lepora@gmail.com>
2021-12-06 17:07:00 +0100

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
                             target="_blank"
                         >
                             <x-ark-icon
-                                name="coin-gecko"
+                                name="app-coin-gecko"
                                 class="inline-block -mt-1"
                             />
                         </a>
