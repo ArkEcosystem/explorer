aef4f8a6560222dc18c4a58477bfe9dcaeaff6d9
Maurizio <maurizio.lepora@gmail.com>
2022-01-06 17:22:55 +0100

    update AvailablePeriods.php

--- app/Http/Livewire/Concerns/AvailablePeriods.php
+++ app/Http/Livewire/Concerns/AvailablePeriods.php
@@ -10,18 +10,18 @@ trait AvailablePeriods
 {
     private function defaultPeriod(): string
     {
-        return StatsPeriods::WEEK;
+        return StatsPeriods::WEEK->value;
     }
 
     private function availablePeriods(): array
     {
         return [
-            StatsPeriods::DAY      => trans('forms.statistics.periods.day'),
-            StatsPeriods::WEEK     => trans('forms.statistics.periods.week'),
-            StatsPeriods::MONTH    => trans('forms.statistics.periods.month'),
-            StatsPeriods::QUARTER  => trans('forms.statistics.periods.quarter'),
-            StatsPeriods::YEAR     => trans('forms.statistics.periods.year'),
-            StatsPeriods::ALL      => trans('forms.statistics.periods.all'),
+            StatsPeriods::DAY->value      => trans('forms.statistics.periods.day'),
+            StatsPeriods::WEEK->value     => trans('forms.statistics.periods.week'),
+            StatsPeriods::MONTH->value    => trans('forms.statistics.periods.month'),
+            StatsPeriods::QUARTER->value  => trans('forms.statistics.periods.quarter'),
+            StatsPeriods::YEAR->value     => trans('forms.statistics.periods.year'),
+            StatsPeriods::ALL->value      => trans('forms.statistics.periods.all'),
         ];
     }
 }
