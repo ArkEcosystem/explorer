cacbae1bde69632b4443f8a714a727b4a4637aa5
Maurizio <maurizio.lepora@gmail.com>
2022-01-06 18:33:01 +0100

    update TimelockScope.php

--- app/Models/Scopes/TimelockScope.php
+++ app/Models/Scopes/TimelockScope.php
@@ -14,7 +14,7 @@ final class TimelockScope implements Scope
 {
     public function apply(Builder $builder, Model $model)
     {
-        $builder->where('type_group', TransactionTypeGroupEnum::CORE);
-        $builder->where('type', CoreTransactionTypeEnum::TIMELOCK);
+        $builder->where('type_group', TransactionTypeGroupEnum::CORE->value);
+        $builder->where('type', CoreTransactionTypeEnum::TIMELOCK->value);
     }
 }
