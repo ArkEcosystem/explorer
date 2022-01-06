6b22a1baf94ee010219cf7d1219cdac013c95dc4
Maurizio <maurizio.lepora@gmail.com>
2022-01-06 17:21:26 +0100

    update CoreTransactionTypeEnum.php

--- app/Enums/CoreTransactionTypeEnum.php
+++ app/Enums/CoreTransactionTypeEnum.php
@@ -4,27 +4,27 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
-final class CoreTransactionTypeEnum
+enum CoreTransactionTypeEnum: int
 {
-    public const TRANSFER = 0;
+    case TRANSFER = 0;
 
-    public const SECOND_SIGNATURE = 1;
+    case SECOND_SIGNATURE = 1;
 
-    public const DELEGATE_REGISTRATION = 2;
+    case DELEGATE_REGISTRATION = 2;
 
-    public const VOTE = 3;
+    case VOTE = 3;
 
-    public const MULTI_SIGNATURE = 4;
+    case MULTI_SIGNATURE = 4;
 
-    public const IPFS = 5;
+    case IPFS = 5;
 
-    public const MULTI_PAYMENT = 6;
+    case MULTI_PAYMENT = 6;
 
-    public const DELEGATE_RESIGNATION = 7;
+    case DELEGATE_RESIGNATION = 7;
 
-    public const TIMELOCK = 8;
+    case TIMELOCK = 8;
 
-    public const TIMELOCK_CLAIM = 9;
+    case TIMELOCK_CLAIM = 9;
 
-    public const TIMELOCK_REFUND = 10;
+    case TIMELOCK_REFUND = 10;
 }
