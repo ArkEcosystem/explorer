454ed1fb1a02effc9f4057c2492145db633498ec
alfonsobries <alfonsobries@gmail.com>
2022-01-17 11:13:50 -0600

    Only clear views that are older than one hour

---
+++ app/Console/Commands/ClearExpiredViews.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Config;
+use RuntimeException;
+
+class ClearExpiredViews extends Command
+{
+    const EXPIRES_MINUTES = 60;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'view:clear-expired {time?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Removes old compiled views files';
+
+    /**
+     * Create a new config clear command instance.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem  $files
+     * @return void
+     */
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+
+        $this->files = $files;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $path = Config::get('view.compiled');
+
+        if (! $path) {
+            throw new RuntimeException('View path not found.');
+        }
+
+        $expiresMinutes = (int) $this->argument('time') ?: self::EXPIRES_MINUTES;
+
+        collect($this->files->glob("{$path}/*"))
+            ->filter(fn (string $view) => $this->files->lastModified($view) < Carbon::now()->subMinutes($expiresMinutes)->getTimestamp())
+            ->each(fn (string $view) => $this->files->delete($view));
+
+        $this->info(sprintf('Compiled views that are older than %s minute(s) cleared!', $expiresMinutes));
+    }
+}
